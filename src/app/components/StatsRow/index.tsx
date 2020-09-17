e981372efa26f9d370c1caa280fd3439728a8e0a
BetsyBraddock <k.shall0507+mutant@gmail.com>
2020-09-17 14:55:27 +0100

    Basic stats page

--- src/app/components/StatsRow/index.tsx
+++ src/app/components/StatsRow/index.tsx
@@ -3,40 +3,86 @@
  * StatsRow
  *
  */
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components/macro';
+import React from 'react';
 import { Asset } from 'types/asset';
+import { AssetsDictionary } from '../../../utils/blockchain/assets-dictionary';
+import { useCacheCallWithValue } from '../../hooks/useCacheCallWithValue';
+import {
+  getLendingContractName,
+  getTokenContractName,
+} from '../../../utils/blockchain/contract-helpers';
+import { fromWei } from 'web3-utils';
+import { weiTo4 } from '../../../utils/blockchain/math-helpers';
 
 interface Props {
   asset: Asset;
-  contract: any;
 }
 
 export function StatsRow(props: Props) {
-  const [data, setData] = useState();
-  useEffect(() => {
-    setData(data);
-  }, [data]);
+  const logo = AssetsDictionary.get(props.asset).logoSvg;
+  const lendingContract = getLendingContractName(props.asset);
+  const tokenContract = getTokenContractName(props.asset);
 
-  const supplyAPR = 1;
-  const TVL = 1;
-  const totalUSD = 1;
-  const totalAsset = 1;
-  const totalBorrowed = 1;
-  const totalAvailable = 1;
-  const borrowAPR = 1;
+  const { value: totalAssetSupply } = useCacheCallWithValue(
+    lendingContract,
+    'totalAssetSupply',
+    '0',
+  );
+
+  const { value: supplyInterestRate } = useCacheCallWithValue(
+    lendingContract,
+    'supplyInterestRate',
+    '0',
+  );
+
+  const { value: borrowInterestRate } = useCacheCallWithValue(
+    lendingContract,
+    'borrowInterestRate',
+    '0',
+  );
+
+  const { value: totalAssetBorrowed } = useCacheCallWithValue(
+    lendingContract,
+    'totalAssetBorrow',
+    '0',
+  );
+
+  const { value: liquidity } = useCacheCallWithValue(
+    lendingContract,
+    'marketLiquidity',
+    'Loading',
+  );
+
   return (
-    <tr>
-      <td>{props.asset}</td>
-      <td>{TVL}</td>
-      <td>{totalUSD}</td>
-      <td>{totalAsset}</td>
-      <td>{totalBorrowed}</td>
-      <td>{totalAvailable}</td>
-      <td>{supplyAPR}</td>
-      <td>{borrowAPR}</td>
-    </tr>
+    <>
+      <tr>
+        <td>
+          <img
+            className="d-inline"
+            style={{ height: '40px' }}
+            src={logo}
+            alt=""
+          />{' '}
+          {props.asset}
+        </td>
+        <td>
+          {parseFloat(weiTo4(totalAssetSupply)).toLocaleString('en')}{' '}
+          {props.asset}
+        </td>
+        <td>
+          {parseFloat(weiTo4(totalAssetBorrowed)).toLocaleString('en')}{' '}
+          {props.asset}
+        </td>
+        <td>
+          {(
+            parseFloat(fromWei(totalAssetSupply)) -
+            parseFloat(fromWei(totalAssetBorrowed))
+          ).toLocaleString('en', { minimumFractionDigits: 4 })}{' '}
+          {props.asset}
+        </td>
+        <td>{weiTo4(supplyInterestRate)}%</td>
+        <td>{weiTo4(borrowInterestRate)}%</td>
+      </tr>
+    </>
   );
 }
-
-const Div = styled.div``;
--- src/app/containers/StatsPage/index.tsx
+++ src/app/containers/StatsPage/index.tsx
@@ -12,37 +12,20 @@ import { StatsRow } from '../../components/StatsRow';
 
 import { AssetsDictionary } from 'utils/blockchain/assets-dictionary';
 
-import { useDrizzle } from '../../hooks/useDrizzle';
-import { useDrizzleState } from '../../hooks/useDrizzleState';
-
 export function StatsPage() {
   const assets = AssetsDictionary.assetList();
-  const [btcContractData, setBtcContractData] = useState();
-  const [usdContractData, setUsdContractData] = useState();
-  const initialized = useDrizzleState(state => state.drizzleStatus.initialized);
-  const drizzle = useDrizzle();
-
-  useEffect(() => {
-    if (initialized) {
-      setBtcContractData(drizzle.contracts['Bitcoin_lending']);
-      setUsdContractData(drizzle.contracts['USD_lending']);
-    }
-  }, [initialized, drizzle]);
 
   return (
     <>
       <Header />
       <main>
         <div className="container">
-          <h1>Stats</h1>
-          <table className="table table-border text-white">
+          <table className="table table-dark text-white mt-5">
             <thead>
               <tr>
                 <th>Asset</th>
-                <th>Total Value Lent</th>
-                <th>Total Supply (USD)</th>
-                <th>Total Supply (Asset)</th>
-                <th>Total Borrowed</th>
+                <th>Total Asset Supplied</th>
+                <th>Total Asset Borrowed</th>
                 <th>Total Available</th>
                 <th>Supply APR</th>
                 <th>Borrow APR</th>
@@ -50,11 +33,7 @@ export function StatsPage() {
             </thead>
             <tbody>
               {assets.map(asset => (
-                <StatsRow
-                  asset={asset}
-                  key={asset}
-                  contract={asset === 'BTC' ? btcContractData : usdContractData}
-                />
+                <StatsRow asset={asset} key={asset} />
               ))}
             </tbody>
           </table>
