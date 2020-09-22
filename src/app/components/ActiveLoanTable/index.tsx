da7e609cb0551ac8be3593e1059c3b278f73bb85
BetsyBraddock <k.shall0507+mutant@gmail.com>
2020-09-22 10:17:59 +0100

    Loan Table using blueprintjs table

--- src/app/components/ActiveLoanTable/index.tsx
+++ src/app/components/ActiveLoanTable/index.tsx
@@ -4,10 +4,15 @@
  *
  */
 import React from 'react';
-import DataTable from 'react-data-table-component';
-import { weiTo4, weiTo2 } from '../../../utils/blockchain/math-helpers';
+import { Cell, Column, Table } from '@blueprintjs/table';
+import { weiTo2 } from '../../../utils/blockchain/math-helpers';
 import { symbolByTokenAddress } from '../../../utils/blockchain/contract-helpers';
 import { useBorrowInterestRate } from '../../hooks/trading/useBorrowInterestRate';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowCircleDown,
+  faArrowCircleUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   data: any;
@@ -18,82 +23,81 @@ export function ActiveLoanTable(props: Props) {
     new Date(Number(timestamp) * 1e3).toLocaleString('en-GB', {
       timeZone: 'GMT',
     });
-  const data = props.data.map(item => {
-    console.log(item);
-    return {
-      icon: 'todo',
-      positionSize: `${parseFloat(weiTo2(item.collateral)).toLocaleString(
-        'en',
-      )} ${symbolByTokenAddress(item.collateralToken)}`,
-      currentMargin: `${weiTo2(item.currentMargin)}%`,
-      interestAPR: 'todo',
-      startPrice: 'todo',
-      endTime: `${date(item.endTimestamp)} GMT`,
-    };
-  });
 
-  const columns = [
-    {
-      name: '',
-      selector: 'icon',
-      sortable: false,
-      right: true,
-    },
-    {
-      name: 'Position Size',
-      selector: 'positionSize',
-      sortable: true,
-      right: true,
-    },
-    {
-      name: 'Current Margin',
-      selector: 'currentMargin',
-      sortable: true,
-      right: true,
-    },
-    {
-      name: 'Interest APR',
-      selector: 'interestAPR',
-      sortable: false,
-      right: true,
-    },
-    {
-      name: 'Start Price',
-      selector: 'startPrice',
-      sortable: false,
-      right: true,
-    },
-    {
-      name: 'End Date & Time',
-      selector: 'endTime',
-      sortable: true,
-      right: true,
-    },
-  ];
+  const positionSize = () =>
+    props.data.map(item => (
+      <Cell>
+        {parseFloat(weiTo2(item.collateral)).toLocaleString('en')}{' '}
+        {symbolByTokenAddress(item.collateralToken)}
+      </Cell>
+    ));
 
-  const customStyles = {
-    cells: {
-      style: {
-        backgroundColor: 'yellow',
-      },
-    },
-    headCells: {
-      style: {
-        border: '5px solid green',
-        fontSize: '14px',
-        color: '#586c86',
-      },
-    },
+  const iconRenderer = () =>
+    props.data.map(item => (
+      <Cell>
+        <FontAwesomeIcon
+          className="d-inline-block h-100 ml-2 pb-1 text-customOrange"
+          icon={faArrowCircleDown}
+        />
+      </Cell>
+    ));
+
+  const currentMargin = (rowIndex: number) => {
+    return <Cell>{weiTo2(props.data[rowIndex].currentMargin)}%</Cell>;
   };
 
+  const interestAPR = () => props.data.map(item => <Cell>todo</Cell>);
+
+  const startPrice = () => props.data.map(item => <Cell>todo</Cell>);
+
+  const endDate = () =>
+    props.data.map(item => <Cell>{date(item.endTimestamp)} GMT</Cell>);
+
+  const testCell = () => <Cell>hello</Cell>;
+
   return (
-    <DataTable
-      className="dataTable"
-      title="Active Loans"
-      columns={columns}
-      data={data}
-      striped={true}
-      customStyles={customStyles}
-    />
+    <>
+      {props.data && (
+        <Table numRows={props.data}>
+          <Column
+            name="Position Size"
+            cellRenderer={(rowIndex: number) => {
+              return (
+                <Cell>
+                  {parseFloat(
+                    weiTo2(props.data[rowIndex].collateral),
+                  ).toLocaleString('en')}{' '}
+                  {symbolByTokenAddress(props.data[rowIndex].collateralToken)}
+                </Cell>
+              );
+            }}
+          />
+          <Column
+            name="Current Margin"
+            cellRenderer={(rowIndex: number) => {
+              return <Cell>{weiTo2(props.data[rowIndex].currentMargin)}%</Cell>;
+            }}
+          />
+          <Column
+            name="Interest APR"
+            cellRenderer={(rowIndex: number) => {
+              return <Cell>todo</Cell>;
+            }}
+          />
+          <Column
+            name="Start Price"
+            cellRenderer={(rowIndex: number) => {
+              return <Cell>todo</Cell>;
+            }}
+          />
+          <Column
+            name="End Date/Time"
+            cellRenderer={(rowIndex: number) => {
+              return <Cell>{date(props.data[rowIndex].endTimestamp)} GMT</Cell>;
+            }}
+          />
+        </Table>
+      )}
+    </>
   );
 }
