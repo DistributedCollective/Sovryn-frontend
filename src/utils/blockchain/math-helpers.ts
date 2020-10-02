7db354b345d98b0924d113f1c8a906b7d2a7327d
Victor Creed <rytis.grincevicius+cv@gmail.com>
2020-10-02 17:27:36 +0300

    fixes wei rounding issues

--- src/utils/blockchain/math-helpers.ts
+++ src/utils/blockchain/math-helpers.ts
@@ -5,10 +5,17 @@ export const weiToBn = (amount: any) =>
   bignumber(fromWei(String(amount || '0'), 'ether'));
 
 export const weiToFixed = (amount: any, decimals: number = 0) =>
-  weiToBn(amount).toFixed(decimals);
+  roundToSmaller(Number(weiToBn(amount)), decimals);
 
 export const weiTo18 = (amount: any) => weiToFixed(amount, 18);
 
 export const weiTo4 = (amount: any) => weiToFixed(amount, 4);
 
 export const weiTo2 = (amount: any) => weiToFixed(amount, 2);
+
+export const roundToSmaller = (amount: number, decimals: number) => {
+  return (
+    amount.toString().match('^-?\\d+(?:\\.\\d{0,' + decimals + '})?')?.[0] ||
+    '0'
+  );
+};
