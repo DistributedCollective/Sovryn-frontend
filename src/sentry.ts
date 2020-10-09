7cd7d04d92596cd64244f24db8231059a4cae552
Victor Creed <rytis.grincevicius+cv@gmail.com>
2020-10-09 14:33:49 +0300

    fixes wei rounding issues

--- src/sentry.ts
+++ src/sentry.ts
@@ -2,7 +2,6 @@ import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 
 export default function sentryInit() {
-  console.log('Sentry error tracing is enabled.');
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     environment: process.env.REACT_APP_NETWORK,
@@ -10,6 +9,6 @@ export default function sentryInit() {
 
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
   });
 }
