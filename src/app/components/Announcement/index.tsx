6f9d82717055060c59abe51dac50980db22a84d0
BetsyBraddock <k.shall0507+mutant@gmail.com>
2020-10-25 16:18:37 +0000

    Removed date from Announcement

--- src/app/components/Announcement/index.tsx
+++ src/app/components/Announcement/index.tsx
@@ -15,8 +15,8 @@ export function Announcement() {
         <>
           As Sovryn has only recently launched, there is currently a{' '}
           <span className="font-weight-bold">$21 limit </span>
-          on all transactions on the platform. On Friday October 30th we will be
-          raising this limit to $210. Thank you for being an early adopter!
+          on all transactions on the platform. We will be raising this limit to
+          $210 soon. Thank you for being an early adopter!
         </>
       }
       localStorageRef="txLimitInfo"
