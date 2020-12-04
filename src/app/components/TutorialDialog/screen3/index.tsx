b51745a0392d1d3981ba09675630b85723410ca3
BetsyBraddock <k.shall0507+mutant@gmail.com>
2020-12-04 17:27:52 +0000

    Mobile wallet speech bubble

--- src/app/components/TutorialDialog/screen3/index.tsx
+++ src/app/components/TutorialDialog/screen3/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { translations } from 'locales/i18n';
 import { useTranslation } from 'react-i18next';
 import badgerQr from 'assets/images/tutorial/badger_qr.svg';
+import speech2 from 'assets/images/tutorial/speech2.svg';
 import { Sovryn } from 'utils/sovryn';
 import { SovrynNetwork } from 'utils/sovryn/sovryn-network';
 
@@ -13,8 +14,8 @@ export function Screen3() {
   }
 
   function closeModal() {
-    const modal = document.getElementById('walletconnect-wrapper');
-    modal?.classList.add('d-none');
+    const walletConectModal = document.getElementById('walletconnect-wrapper');
+    walletConectModal?.classList.add('d-none');
   }
 
   useEffect(() => {
@@ -25,12 +26,10 @@ export function Screen3() {
     <>
       <div>
         <div className="badger-qr position-absolute">
-          <img
-            src={badgerQr}
-            alt=""
-            className="h-100 w-100"
-            onClick={() => closeModal()}
-          />
+          <img src={badgerQr} alt="" className="h-100 w-100" />
+        </div>
+        <div className="speech2 position-absolute">
+          <img src={speech2} alt="" className="h-100 w-100" />
         </div>
       </div>
     </>
