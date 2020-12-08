545075be7cb12e0f381b93429a2b42e51b4ee777
BetsyBraddock <k.shall0507+mutant@gmail.com>
2020-12-08 22:46:43 +0000

    Design and transitions for FastBTC Tutorial

--- src/app/components/FastBTCTutorial/component/index.tsx
+++ src/app/components/FastBTCTutorial/component/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from 'assets/images/sovryn-logo-white-inline.svg';
 import { useSelector, useDispatch } from 'react-redux';
-import { Icon, Spinner } from '@blueprintjs/core';
+import { Icon } from '@blueprintjs/core';
 import { Screen1 } from '../screen1';
 import { Screen2 } from '../screen2';
 import { Screen3 } from '../screen3';
@@ -13,21 +13,15 @@ import {
 } from '../../../containers/FastBtcForm/slice';
 import { selectFastBtcForm } from '../../../containers/FastBtcForm/selectors';
 import { fastBtcFormSaga } from '../../../containers/FastBtcForm/saga';
-import { FieldGroup } from '../../../components/FieldGroup';
-import { InputField } from '../../../components/InputField';
 import { useAccount, useIsConnected } from '../../../hooks/useAccount';
-import { DummyField } from '../../../components/DummyField';
 import { SkeletonRow } from '../../../components/Skeleton/SkeletonRow';
-import { weiTo4, weiToFixed } from 'utils/blockchain/math-helpers';
-import { LinkToExplorer } from '../../../components/LinkToExplorer';
-import { TradeButton } from '../../../components/TradeButton';
 
 import { translations } from 'locales/i18n';
 import { useTranslation } from 'react-i18next';
 
 export function FastBTCTutorialComponent(props) {
   const { t } = useTranslation();
-  const [screen, setScreen] = useState(3);
+  const [screen, setScreen] = useState(1);
 
   useInjectReducer({ key: sliceKey, reducer: reducer });
   useInjectSaga({ key: sliceKey, saga: fastBtcFormSaga });
@@ -52,12 +46,9 @@ export function FastBTCTutorialComponent(props) {
     }
   }, [state.depositTx]);
 
-  const handleInputChange = useCallback(
-    e => {
-      dispatch(actions.changeReceiverAddress(e.currentTarget.value));
-    },
-    [dispatch],
-  );
+  function changeScreen(num) {
+    setScreen(num);
+  }
 
   return (
     <>
@@ -67,7 +58,7 @@ export function FastBTCTutorialComponent(props) {
             screen !== 1 && 'wide'
           }`}
         >
-          <div className="position-absolute text-white close">
+          <div className="position-absolute text-white close cursor-pointer">
             <Icon icon="cross" />
           </div>
           <div className="logo text-center mt-2">
@@ -76,7 +67,7 @@ export function FastBTCTutorialComponent(props) {
           {screen === 1 && (
             <Screen1
               btcAddress={state.depositAddress}
-              changeScreen={setScreen}
+              changeScreen={changeScreen}
             />
           )}
           {screen === 2 && (
@@ -85,7 +76,14 @@ export function FastBTCTutorialComponent(props) {
               transactionDetected={transactionDetected}
             />
           )}
-          {screen === 3 && <Screen3 />}
+          {screen === 3 && (
+            <Screen3
+              depositTxHash={state.depositTx ? state.depositTx.txHash : false}
+              transferTxHash={
+                state.transferTx ? state.transferTx.txHash : false
+              }
+            />
+          )}
         </div>
       </div>
     </>
--- src/app/components/FastBTCTutorial/screen2/index.tsx
+++ src/app/components/FastBTCTutorial/screen2/index.tsx
@@ -1,78 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import QRCode from 'qrcode.react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Icon } from '@blueprintjs/core';
 
 export function Screen2(props) {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 1000);
+  });
+
   return (
     <>
-      <p className="pt-3">
-        Your unique BTC address has been created, transfer BTC to receive RBTC
-        in your RSK wallet
-      </p>
-      <div className="row">
-        <div className="col-6">
-          <div className="qr-code">
-            <div className="text-center">
-              <QRCode
-                value={props.btcAddress}
-                renderAs="svg"
-                bgColor="var(--white)"
-                fgColor="var(--primary)"
-                includeMargin={true}
-                size={258}
-                className="rounded"
-              />
+      {show && (
+        <>
+          <p className="pt-3 text-center">
+            Your unique BTC address has been created, transfer BTC to receive
+            RBTC in your RSK wallet
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <div className="qr-code">
+                <div className="text-center">
+                  <QRCode
+                    value={props.btcAddress}
+                    renderAs="svg"
+                    bgColor="var(--white)"
+                    fgColor="var(--primary)"
+                    includeMargin={true}
+                    size={258}
+                    className="rounded"
+                  />
+                </div>
+              </div>
+              <div className="btcAddress--screen2 bg-secondary py-1 px-3 mt-3 mx-4 rounded cursor-pointer">
+                <CopyToClipboard text={props.btcAddress}>
+                  <div className="d-inline">{`${props.btcAddress.substring(
+                    0,
+                    24,
+                  )}...`}</div>
+                </CopyToClipboard>
+                <div className="d-inline">
+                  <Icon icon="duplicate" />
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <p className="mb-2">
+                <strong className="mb-2 d-block">
+                  <u>Deposit requirements:</u>
+                </strong>
+                <ul>
+                  <li>min: 0.001 BTC</li>
+                  <li>max: 0.01632829 BTC</li>
+                  <li>
+                    Fee 1.00 USD*{' '}
+                    <span className="small">
+                      <em>Deducted from transaction amount</em>
+                    </span>
+                  </li>
+                </ul>
+              </p>
+              <p>
+                <strong className="mb-2 d-block">
+                  <u>Important:</u>
+                </strong>
+                <ul>
+                  <li>
+                    Do not deposit any non - BTC assets to this address,
+                    otherwise your assets will be lost permanently
+                  </li>
+                  <li>
+                    Please allow upto 15 mins for the transaction to process
+                  </li>
+                </ul>
+              </p>
+              <p>
+                For support please join us on{' '}
+                <a href="discord.com/invite/J22WS6z">discord.</a>
+              </p>
             </div>
           </div>
-          <div className="btcAddress--screen2 bg-secondary py-1 px-3 mt-3 mx-4 rounded cursor-pointer">
-            <CopyToClipboard text={props.btcAddress}>
-              <div className="d-inline">{`${props.btcAddress.substring(
-                0,
-                24,
-              )}...`}</div>
-            </CopyToClipboard>
-            <div className="d-inline">
-              <Icon icon="duplicate" />
+          <div className="float-right">
+            <div className="sovryn-border small p-2">
+              Transaction detected
+              <div
+                className={`ml-2 d-inline-block circle-${
+                  props.transactionDetected ? 'green' : 'red'
+                }`}
+              ></div>
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <p>
-            <strong>Deposit requirements:</strong>
-            <ul>
-              <li>min: 0.001 BTC</li>
-              <li>max: 0.01632829 BTC</li>
-              <li>Fee 1.00 USD*</li>
-            </ul>
-          </p>
-          <p>
-            <strong>Important:</strong>
-            <ul>
-              <li>
-                Do not deposit any non - BTC assets to this address, otherwise
-                your assets will be lost permanently
-              </li>
-              <li>Please allow upto 15 mins for the transaction to process</li>
-            </ul>
-          </p>
-          <p>
-            For support please join us on{' '}
-            <a href="discord.com/invite/J22WS6z">discord.com/invite/J22WS6z</a>
-          </p>
-          <p className="small">*Fee is deducted from the transaction amount</p>
-        </div>
-      </div>
-      <div className="float-right">
-        <div className="sovryn-border small p-2">
-          Transaction detected
-          <div
-            className={`ml-2 d-inline-block circle-${
-              props.transactionDetected ? 'green' : 'red'
-            }`}
-          ></div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
