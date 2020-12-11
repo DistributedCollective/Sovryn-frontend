567cdf73b9fb7cdb41614220b0033e4b9b0d72bf
Victor Creed <rytis.grincevicius+cv@gmail.com>
2020-12-11 15:49:29 +0200

    trading events

--- src/app/containers/TradingHistory/index.tsx
+++ src/app/containers/TradingHistory/index.tsx
@@ -25,6 +25,7 @@ import { Tooltip } from '@blueprintjs/core';
 import { TradeProfit } from '../../components/TradeProfit';
 import { useSelector } from 'react-redux';
 import { selectEventsState } from '../../../store/global/events-store/selectors';
+import { useGetContractPastEvents } from '../../hooks/useGetContractPastEvents';
 
 type EventType = 'buy' | 'sell';
 
@@ -167,16 +168,12 @@ function calculateProfits(events: CustomEvent[]): CalculatedEvent | null {
 export function TradingHistory() {
   const { t } = useTranslation();
   const account = useAccount();
-  const eventsState = useSelector(selectEventsState);
 
-  const tradeStates = eventsState[account]?.['sovrynProtocol']?.Trade || {
-    events: [],
-    loading: false,
-  };
-  const closeStates = eventsState[account]?.['sovrynProtocol']
-    ?.CloseWithSwap || { events: [], loading: false };
-
-  console.log('trades:', tradeStates.events);
+  const tradeStates = useGetContractPastEvents('sovrynProtocol', 'Trade');
+  const closeStates = useGetContractPastEvents(
+    'sovrynProtocol',
+    'CloseWithSwap',
+  );
 
   const loading = tradeStates.loading || closeStates.loading;
 
@@ -187,8 +184,6 @@ export function TradingHistory() {
       const mergedEvents = [...closeEvents, ...tradeEvents].sort(
         (a, b) => b.blockNumber - a.blockNumber,
       );
-
-      console.log('merged:', mergedEvents);
 
       const items: { [key: string]: CustomEvent[] } = {};
       mergedEvents.forEach(item => {
@@ -212,7 +207,6 @@ export function TradingHistory() {
         }
       });
 
-      console.log('close entries', closeEntries);
       setEvents(closeEntries);
     },
     [],
