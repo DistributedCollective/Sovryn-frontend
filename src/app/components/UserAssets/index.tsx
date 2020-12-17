626183e52b3316bcb2ec89d9c76e26b2966ae15c
Victor Creed <rytis.grincevicius+cv@gmail.com>
2020-12-17 10:35:41 +0200

    user page

---
+++ src/app/components/UserAssets/index.tsx
@@ -0,0 +1,174 @@
+/**
+ *
+ * UserAssets
+ *
+ */
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { bignumber } from 'mathjs';
+import { Button, ButtonGroup, Text } from '@blueprintjs/core';
+import { translations } from '../../../locales/i18n';
+import { useAccount, useIsConnected } from '../../hooks/useAccount';
+import { prettyTx } from '../../../utils/helpers';
+import { AssetsDictionary } from '../../../utils/dictionaries/assets-dictionary';
+import { AssetDetails } from '../../../utils/models/asset-details';
+import { useAssetBalanceOf } from '../../hooks/useAssetBalanceOf';
+import { weiToFixed } from '../../../utils/blockchain/math-helpers';
+import { LoadableValue } from '../LoadableValue';
+import { useCachedAssetPrice } from '../../hooks/trading/useCachedAssetPrice';
+import { Asset } from '../../../types/asset';
+import { usePriceFeeds_tradingPairRates } from '../../hooks/price-feeds/usePriceFeeds_tradingPairRates';
+import { Skeleton } from '../PageSkeleton';
+import { numberToUSD } from '../../../utils/display-text/format';
+import { actions } from 'app/containers/FastBtcForm/slice';
+
+export function UserAssets() {
+  const { t } = useTranslation();
+  usePriceFeeds_tradingPairRates();
+  const connected = useIsConnected();
+  const account = useAccount();
+  const assets = AssetsDictionary.list();
+
+  return (
+    <section>
+      <div className="d-flex align-items-center justify-content-start mb-3">
+        <h2 className="flex-shrink-0 flex-grow-0 mr-3">
+          {t(translations.userAssets.meta.title)}
+        </h2>
+        {connected && account && (
+          <div>
+            <Text ellipsize>{prettyTx(account)}</Text>
+          </div>
+        )}
+      </div>
+      <div className="sovryn-border sovryn-table p-3 mb-5">
+        <table className="w-100">
+          <thead>
+            <tr>
+              <th>{t(translations.userAssets.tableHeaders.asset)}</th>
+              <th className="text-right">
+                {t(translations.userAssets.tableHeaders.totalBalance)}
+              </th>
+              <th className="text-right d-none d-md-table-cell">
+                {t(translations.userAssets.tableHeaders.dollarBalance)}
+              </th>
+              <th className="text-right d-none d-md-table-cell">
+                {t(translations.userAssets.tableHeaders.action)}
+              </th>
+            </tr>
+          </thead>
+          <tbody className="mt-5">
+            {!connected && (
+              <>
+                <tr>
+                  <td>
+                    <Skeleton />
+                  </td>
+                  <td>
+                    <Skeleton />
+                  </td>
+                  <td className="d-none d-md-table-cell">
+                    <Skeleton />
+                  </td>
+                  <td className="d-none d-md-table-cell">
+                    <Skeleton />
+                  </td>
+                </tr>
+              </>
+            )}
+            {connected &&
+              account &&
+              assets.map(item => <AssetRow key={item.asset} item={item} />)}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+}
+
+interface AssetProps {
+  item: AssetDetails;
+}
+
+function AssetRow({ item }: AssetProps) {
+  const { t } = useTranslation();
+  const tokens = useAssetBalanceOf(item.asset);
+  const dollars = useCachedAssetPrice(item.asset, Asset.USDT);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [dollarValue, setDollarValue] = useState('0');
+
+  useEffect(() => {
+    if (item.asset === Asset.USDT) {
+      setDollarValue(tokens.value);
+    } else {
+      setDollarValue(
+        bignumber(tokens.value)
+          .mul(dollars.value)
+          .div(10 ** item.decimals)
+          .toFixed(0),
+      );
+    }
+  }, [dollars.value, tokens.value, item.asset, item.decimals]);
+
+  return (
+    <tr key={item.asset}>
+      <td>
+        <img
+          className="d-inline"
+          style={{ height: '40px' }}
+          src={item.logoSvg}
+          alt={item.asset}
+        />{' '}
+        {item.symbol}
+      </td>
+      <td className="text-right">
+        <LoadableValue
+          value={weiToFixed(tokens.value, 4)}
+          loading={tokens.loading}
+        />
+      </td>
+      <td className="text-right d-none d-md-table-cell">
+        <LoadableValue
+          value={numberToUSD(Number(weiToFixed(dollarValue, 4)), 4)}
+          loading={dollars.loading}
+        />
+      </td>
+      <td className="text-right d-none d-md-table-cell">
+        <ButtonGroup>
+          {item.asset === Asset.BTC && (
+            <Button
+              minimal
+              text={t(translations.userAssets.actions.deposit)}
+              className="text-gold"
+              onClick={() => dispatch(actions.showDialog(true))}
+            />
+          )}
+          <Button
+            minimal
+            text={t(translations.userAssets.actions.trade)}
+            className="text-gold"
+            onClick={() =>
+              history.push('/', {
+                params: { asset: item.asset, action: 'trade' },
+              })
+            }
+          />
+          <Button
+            minimal
+            text={t(translations.userAssets.actions.swap)}
+            className="text-gold"
+            onClick={() =>
+              history.push('/', {
+                params: { asset: item.asset, action: 'swap' },
+              })
+            }
+          />
+        </ButtonGroup>
+      </td>
+    </tr>
+  );
+}
--- src/app/containers/FastBtcForm/saga.ts
+++ src/app/containers/FastBtcForm/saga.ts
@@ -1,34 +1,20 @@
-import Rsk from '@rsksmart/rsk3';
 import { eventChannel } from 'redux-saga';
 import { take, call, put, select, takeLatest } from 'redux-saga/effects';
 import { PayloadAction } from '@reduxjs/toolkit';
 import io from 'socket.io-client';
 import { currentChainId, fastBtcApis } from 'utils/classifiers';
 import { actions } from './slice';
-import { selectFastBtcForm } from './selectors';
 import { actions as wActions } from 'app/containers/WalletProvider/slice';
 import { selectWalletProvider } from '../WalletProvider/selectors';
 import { Sovryn } from '../../../utils/sovryn';
 
-export function* verifyReceiverWallet({ payload }: PayloadAction<string>) {
-  let valid;
-  try {
-    Rsk.utils.toChecksumAddress(payload);
-    valid = true;
-  } catch (e) {
-    valid = false;
-  }
-  yield put(actions.changeReceiverAddressValidity(valid));
-}
-
-function* callCreateWebSocketChannel({ payload }: PayloadAction<boolean>) {
+function* callCreateWebSocketChannel({ payload }: PayloadAction<string>) {
   if (!payload) {
     yield put(actions.getDepositAddressFailed());
     return;
   }
 
-  const state = yield select(selectFastBtcForm);
-  const blockChannel = yield call(createWebSocketChannel, state);
+  const blockChannel = yield call(createWebSocketChannel, payload);
   try {
     while (true) {
       const event = yield take(blockChannel);
@@ -39,7 +25,7 @@ function* callCreateWebSocketChannel({ payload }: PayloadAction<boolean>) {
   }
 }
 
-function createWebSocketChannel(state) {
+function createWebSocketChannel(receiverAddress) {
   return eventChannel(emit => {
     const { origin, pathname } = new URL(fastBtcApis[currentChainId]);
 
@@ -48,41 +34,39 @@ function createWebSocketChannel(state) {
       path: pathname && pathname !== '/' ? pathname : '',
     });
 
-    if (state.receiverAddress) {
+    console.log('sockets start', receiverAddress);
+
+    if (receiverAddress) {
       // get deposit address
-      socket.emit('getDepositAddress', state.receiverAddress, (err, res) => {
-        console.log('get deposit address', res);
+      socket.emit('getDepositAddress', receiverAddress, (err, res) => {
         if (res && res.btcadr) {
           emit(actions.getDepositAddressSuccess(res));
         } else {
           emit(actions.depositError(res.error));
         }
       });
-      getHistory(state.receiverAddress);
+      getHistory(receiverAddress);
     }
 
     socket.emit('txAmount', info => {
-      console.log('get tx amount', info);
       emit(actions.changeAmountInfo(info));
     });
 
     socket.on('txAmount', info => emit(actions.changeAmountInfo(info)));
 
     socket.on('depositTx', tx => {
-      console.log('get deposit tx', tx);
       emit(actions.changeDepositTx(tx));
-      getHistory(state.receiverAddress);
+      getHistory(receiverAddress);
     });
     socket.on('transferTx', tx => {
-      console.log('get transfer tx', tx);
       emit(actions.changeTransferTx(tx));
-      getHistory(state.receiverAddress);
+      getHistory(receiverAddress);
     });
     socket.on('depositError', errorMessage =>
       emit(actions.depositError(errorMessage)),
     );
 
-    getHistory(state.receiverAddress);
+    getHistory(receiverAddress);
 
     function getHistory(address: string) {
       socket.emit('getDepositHistory', address, info =>
@@ -115,9 +99,8 @@ function* accountChanged() {
 }
 
 export function* fastBtcFormSaga() {
-  yield takeLatest(actions.changeReceiverAddress.type, verifyReceiverWallet);
   yield takeLatest(
-    actions.changeReceiverAddressValidity.type,
+    actions.changeReceiverAddress.type,
     callCreateWebSocketChannel,
   );
   yield takeLatest(wActions.disconnected.type, resetAddresses);
