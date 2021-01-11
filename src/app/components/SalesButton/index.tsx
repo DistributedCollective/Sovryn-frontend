319057416e668d61e8f8bf71f622f8e13d2c7221
Tony Zhou <tony.zhou.vue@gmail.com>
2021-01-10 18:53:35 -0800

    updated the border style of buttons

--- src/app/components/SalesButton/index.tsx
+++ src/app/components/SalesButton/index.tsx
@@ -5,20 +5,18 @@ import { media } from '../../../styles/media';
 const StyledButton = styled.button.attrs(_ => ({
   type: 'button',
 }))`
-  border: none;
-  background: rgba(254, 192, 4, 0.05);
+  border: 1px solid var(--gold);
+  background: none;
+  border-radius: 8px;
   color: var(--gold);
   min-width: 48px;
   height: 48px;
   text-align: center;
   ${media.xl`
-    background: none;
     min-width: 208px;
     height: 50px;
     padding: 2px 20px;
-    border-radius: 8px;
     font-weight: 600;
-    border: 1px solid var(--gold);
   
     &:hover, &:active, &:focus {
       background: var(--gold) !important;
