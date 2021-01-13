4d1e257eb5d42940896e19b1b1cb186f0d103908
pietro-maximoff <pietro.maxim.official@protonmail.com>
2021-01-13 16:46:49 +0300

    [NFT] - add dynamic Limit and Tier

--- src/app/components/SovGenerationNFTS/index.tsx
+++ src/app/components/SovGenerationNFTS/index.tsx
@@ -1,21 +1,64 @@
 /**
  *
- * TopUpHistory
+ * SovGenerationNFTS
  *
  */
-import React from 'react';
-import sov_1 from '../../../assets/images/wallet/sov_1.jpg';
-import sov_2 from '../../../assets/images/wallet/sov_2.jpg';
-import sov_icon from '../../../assets/images/wallet/icon-sov.svg';
+
+import React, { useEffect, useState } from 'react';
+import sov_1 from 'assets/images/wallet/sov_1.jpg';
+import sov_2 from 'assets/images/wallet/sov_2.jpg';
+import sov_3 from 'assets/images/wallet/sov_3.jpg';
+import sov_icon from 'assets/images/wallet/icon-sov.svg';
+import { weiToNumberFormat } from 'utils/display-text/format';
+import { trimZero } from 'utils/blockchain/math-helpers';
+import { useAccount } from '../../hooks/useAccount';
+import { useCacheCallWithValue } from '../../hooks/useCacheCallWithValue';
 
 export function SovGenerationNFTS() {
+  const account = useAccount();
+  const { value: maxPurchase } = useCacheCallWithValue(
+    'CrowdSale',
+    'getMaxPurchase',
+    '0',
+    account,
+  );
+
+  const tier = trimZero(weiToNumberFormat(maxPurchase, 8));
+  const [tierLabel, setTierLabel] = useState('');
+  const [tierImage, setTierImage] = useState('');
+
+  useEffect(() => {
+    switch (tier) {
+      case '0.03':
+        setTierLabel('Community');
+        setTierImage(sov_1);
+        break;
+      case '0.1':
+        setTierLabel('Hero');
+        setTierImage(sov_2);
+        break;
+      case '2':
+        setTierLabel('Superhero');
+        setTierImage(sov_3);
+        break;
+      default:
+        setTierLabel('Community');
+        setTierImage(sov_1);
+        break;
+    }
+  }, [tier, setTierLabel, setTierImage]);
+
   return (
     <div className="sovryn-border p-3 mb-5 pb-5">
       <p className="text-center sov-title mb-5">SOV Generation 01 NFT's</p>
-      <div className="d-md-flex align-items-center justify-content-center">
-        <div className="mr-md-5 mb-md-0 mb-sm-3 mb-3 position-relative">
+      <div className="d-md-flex text-center align-items-center justify-content-center">
+        <div className="mr-md-5 mb-md-0 mb-sm-5 mb-5 ml-3 mr-3 position-relative d-inline-block">
           <div className="image-bordered">
-            <img className="w-100 h-100 image-responsive" src={sov_1} alt="" />
+            <img
+              className="w-100 h-100 image-responsive"
+              src={tierImage}
+              alt=""
+            />
           </div>
           <div className="sov-table">
             <table className="table">
@@ -30,11 +73,11 @@ export function SovGenerationNFTS() {
                 </tr>
                 <tr>
                   <td>
-                    <b>Purchase Limit: 0.03 BTC</b>
+                    <b>Purchase Limit: {tier} BTC</b>
                   </td>
                 </tr>
                 <tr>
-                  <td>Community Tier</td>
+                  <td>{tierLabel} Tier</td>
                   <td>
                     <div>SOV-OG</div>
                   </td>
@@ -43,7 +86,7 @@ export function SovGenerationNFTS() {
             </table>
           </div>
         </div>
-        <div className="ml-md-3 position-relative">
+        <div className="ml-3 mr-3 position-relative d-inline-block">
           <div className="image-bordered">
             <img className="w-100 h-100 image-responsive" src={sov_2} alt="" />
           </div>
