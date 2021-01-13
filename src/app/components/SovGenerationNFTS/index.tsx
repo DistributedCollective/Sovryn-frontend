1c46e1c4dafa4535e261ac4c9804243418f98d81
pietro-maximoff <pietro.maxim.official@protonmail.com>
2021-01-13 11:27:52 +0300

    [NFT] - add images + styles

--- src/app/components/SovGenerationNFTS/index.tsx
+++ src/app/components/SovGenerationNFTS/index.tsx
@@ -4,17 +4,19 @@
  *
  */
 import React from 'react';
-import sov_1 from '../../../assets/images/wallet/sov_1.svg';
+import sov_1 from '../../../assets/images/wallet/sov_1.jpg';
 import sov_2 from '../../../assets/images/wallet/sov_2.jpg';
 import sov_icon from '../../../assets/images/wallet/icon-sov.svg';
 
 export function SovGenerationNFTS() {
   return (
     <div className="sovryn-border p-3 mb-5 pb-5">
-      <p className="text-center sov-title">SOV Generation 01 NFT's</p>
+      <p className="text-center sov-title mb-5">SOV Generation 01 NFT's</p>
       <div className="d-md-flex align-items-center justify-content-center">
-        <div className="mr-md-3 mb-md-0 mb-sm-3 mb-3 position-relative">
-          <img className="w-100 h-100" src={sov_1} alt="" />
+        <div className="mr-md-5 mb-md-0 mb-sm-3 mb-3 position-relative">
+          <div className="image-bordered">
+            <img className="w-100 h-100 image-responsive" src={sov_1} alt="" />
+          </div>
           <div className="sov-table">
             <table className="table">
               <tbody>
@@ -42,7 +44,9 @@ export function SovGenerationNFTS() {
           </div>
         </div>
         <div className="ml-md-3 position-relative">
-          <img className="w-100 h-100" src={sov_2} alt="" />
+          <div className="image-bordered">
+            <img className="w-100 h-100 image-responsive" src={sov_2} alt="" />
+          </div>
           <div className="sov-table">
             <table className="table">
               <tbody>
