fed0688b980f64a66e97a1bd7250ef918fa0e27b
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-01-15 17:51:44 +0200

    timers updated

--- src/app/components/SaleBanner/index.tsx
+++ src/app/components/SaleBanner/index.tsx
@@ -1,87 +1,123 @@
-import React, { useEffect } from 'react';
-import './SaleBanner.scss';
-import { Button } from '@blueprintjs/core';
-import samurai from './assets/banner-samurai.svg';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Countdown from 'react-countdown';
+import { Button } from '@blueprintjs/core';
+import moment from 'moment';
+
+import './SaleBanner.scss';
+import samurai from './assets/banner-samurai.svg';
+import { useSaleEndTime } from '../../containers/SalesPage/hooks/useSaleEndTime';
+import { currentNetwork } from '../../../utils/classifiers';
+
+const startDate = moment('2021-01-19 17:00+0').utc().toDate();
+// const startDate: Date = null as any;
 
 export function SaleBanner() {
-  useEffect(() => {
-    const countDownDate = new Date('Jan 15, 2021 12:30:00+0').getTime();
-    const x = setInterval(function () {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-      let sdays,
-        shours,
-        smins,
-        ssecs = '';
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      if (days <= 9) {
-        sdays = `0${days}`;
-      } else {
-        sdays = `${days}`;
-      }
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
-      if (hours <= 9) {
-        shours = `0${hours}`;
-      } else {
-        shours = `${hours}`;
-      }
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      if (minutes <= 9) {
-        smins = `0${minutes}`;
-      } else {
-        smins = `${minutes}`;
-      }
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      if (seconds <= 9) {
-        ssecs = `0${seconds}`;
-      } else {
-        ssecs = `${seconds}`;
-      }
-      let daysElement = document.getElementById('days') as HTMLInputElement;
-      daysElement.innerText = sdays;
-      let hoursElement = document.getElementById('hours') as HTMLInputElement;
-      hoursElement.innerText = shours;
-      let minsElement = document.getElementById('mins') as HTMLInputElement;
-      minsElement.innerText = smins;
-      let secsElement = document.getElementById('secs') as HTMLInputElement;
-      secsElement.innerText = ssecs;
-
-      if (distance < 0) {
-        clearInterval(x);
-        let timerElement = document.getElementById(
-          'timer-container',
-        ) as HTMLInputElement;
-        timerElement.style.display = 'none';
-        let titleElement = document.getElementById(
-          'title-text',
-        ) as HTMLInputElement;
-        titleElement.innerText = 'Token sale is live now!';
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(x);
-    };
-  }, []);
+  const [show, setShow] = useState(true);
+  const { date: endDate, now, loading } = useSaleEndTime();
 
   const closeBanner = () => {
-    let timerElement = document.getElementById(
-      'banner-container',
-    ) as HTMLInputElement;
-    timerElement.style.display = 'none';
+    setShow(false);
   };
+
+  const countDown = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <></>;
+    } else {
+      return (
+        <div className="timer-container">
+          <div className="date-container">
+            <div>
+              <p className="text-white p-remove-padding">Days</p>
+              <h3 className="text-white">{days}</h3>
+            </div>
+            <div>
+              <p className="text-white p-remove-padding">Hours</p>
+              <h3 className="text-white">{hours}</h3>
+            </div>
+            <div>
+              <p className="text-white p-remove-padding">Mins</p>
+              <h3 className="text-white">{minutes}</h3>
+            </div>
+            <div>
+              <p className="text-white p-remove-padding">Secs</p>
+              <h3 className="text-white">{seconds}</h3>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  if (!show) {
+    return null;
+  }
+
+  //todo: temp only, until date is not decided
+  if (currentNetwork === 'mainnet') {
+    return (
+      <div className="banner-container">
+        <div className="banner py-3">
+          <div className="info-container">
+            <h2 className="text-uppercase title-container black-font">
+              <>SOV* Genesis Sale</>
+            </h2>
+            <div className="sub-info-container">
+              <p className="sub-info-text black-font">SOV Token Sale</p>
+            </div>
+            <div className="button-container">
+              <Link
+                className="button button-nav button-black button-white button-container"
+                to="/sales"
+              >
+                <span className="button-text text-nowrap">Learn More</span>
+              </Link>
+            </div>
+          </div>
+          <div className="picture-container">
+            <img
+              className="banner-samurai"
+              src={samurai}
+              alt="banner-samurai"
+            />
+          </div>
+          <div className="close-button-container">
+            <Button
+              icon="cross"
+              minimal
+              className="float-right"
+              onClick={() => {
+                closeBanner();
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="banner-container" id="banner-container">
-      <div className="banner">
+    <div className="banner-container">
+      <div className="banner py-3">
         <div className="info-container">
-          <h2
-            className="text-uppercase title-container black-font"
-            id="title-text"
-          >
-            The Countdown has begun!
+          <h2 className="text-uppercase title-container black-font">
+            {loading ? (
+              <>SOV* Genesis Sale</>
+            ) : (
+              <>
+                {endDate > now ? (
+                  <>
+                    {endDate >= now && <>Sale has begun!</>}
+                    {endDate < now && <>Sale ended!</>}
+                  </>
+                ) : (
+                  <>
+                    {startDate > now && <>Count Down has begun</>}
+                    {startDate < now && <>SOV* Genesis Sale</>}
+                  </>
+                )}
+              </>
+            )}
           </h2>
           <div className="sub-info-container">
             <p className="sub-info-text black-font">SOV Token Sale</p>
@@ -91,38 +127,19 @@ export function SaleBanner() {
               className="button button-nav button-black button-white button-container"
               to="/sales"
             >
-              <span className="button-text">Learn More</span>
+              <span className="button-text text-nowrap">Learn More</span>
             </Link>
           </div>
-
-          <div className="timer-container" id="timer-container">
-            <div className="date-container">
-              <div>
-                <p className="text-white p-remove-padding">Days</p>
-                <h3 className="text-white" id="days">
-                  00
-                </h3>
-              </div>
-              <div>
-                <p className="text-white p-remove-padding">Hours</p>
-                <h3 className="text-white" id="hours">
-                  00
-                </h3>
-              </div>
-              <div>
-                <p className="text-white p-remove-padding">Mins</p>
-                <h3 className="text-white" id="mins">
-                  00
-                </h3>
-              </div>
-              <div>
-                <p className="text-white p-remove-padding">Secs</p>
-                <h3 className="text-white" id="secs">
-                  00
-                </h3>
-              </div>
-            </div>
-          </div>
+          {!loading && (
+            <>
+              {endDate > now && (
+                <Countdown renderer={countDown} date={endDate} />
+              )}
+              {startDate && startDate > now && (
+                <Countdown renderer={countDown} date={startDate} />
+              )}
+            </>
+          )}
         </div>
         <div className="picture-container">
           <img className="banner-samurai" src={samurai} alt="banner-samurai" />
