7362ee256c60a39a6983e1c20ae49c0e122ab18a
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-01-15 19:57:58 +0200

    temp changes for mainnet

--- src/app/containers/SalesPage/hooks/useSaleEndTime.ts
+++ src/app/containers/SalesPage/hooks/useSaleEndTime.ts
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 import { useCacheCallWithValue } from '../../../hooks/useCacheCallWithValue';
+import { currentNetwork } from '../../../../utils/classifiers';
 
 // 12.30 GTM, 15th Jan
 const DATE_FORMAT = 'HH:mm [GMT], Do MMM';
 
 export function useSaleEndTime() {
   const [endDate, setEndDate] = useState(
-    moment('2021-01-15 15:30+0').utc(false).toDate(),
+    moment(
+      currentNetwork === 'testnet'
+        ? '2021-01-15 15:30+0'
+        : '2021-01-21 15:30+0',
+    )
+      .utc(false)
+      .toDate(),
   );
   const [endDateString, setEndDateString] = useState(
     moment(endDate).format(DATE_FORMAT),
