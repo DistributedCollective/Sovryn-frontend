1b1aaf34a622f79f70e01293a33ffbad94140379
Soul Bit <its.soulBit@gmail.com>
2021-01-17 01:29:30 +0000

    Remove ellipsize from CurrencyRow titles for #165

--- src/app/containers/LendBorrowSovryn/components/CurrencyContainer/CurrencyRow.tsx
+++ src/app/containers/LendBorrowSovryn/components/CurrencyContainer/CurrencyRow.tsx
@@ -41,7 +41,7 @@ const CurrencyRow: React.FC<Props> = ({
       </div>
       <div className="d-flex currency pt-0 w-lg-50 pt-lg-3">
         <div className="w-50 px-3">
-          <Text ellipsize className="text-muted">
+          <Text className="text-muted">
             {t(translations.lend.currency.lendArp)}:
           </Text>
           <NextSupplyInterestRate
@@ -50,7 +50,7 @@ const CurrencyRow: React.FC<Props> = ({
           />
         </div>
         <div className="w-50 px-3">
-          <Text ellipsize className="text-muted">
+          <Text className="text-muted">
             {t(translations.lend.currency.borrowArp)}:
           </Text>
           <NextBorrowInterestRate
