aca83c468024f05cf3e33927533239329ff2834e
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-02-05 20:50:42 +0200

    fixes pre-order banner for windows users

--- src/app/components/PreOrderBanner/index.tsx
+++ src/app/components/PreOrderBanner/index.tsx
@@ -3,15 +3,21 @@
  * PreOrderBanner
  *
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 import styles from './index.module.css';
 
 export function PreOrderBanner() {
+  const [isWindow, setIsWindows] = useState(false);
+  useEffect(() => {
+    setIsWindows(navigator?.appVersion?.indexOf('Win') !== -1);
+  }, []);
+
   return (
     <div className="row mb-5">
       <a
         href="https://token.sovryn.app"
-        className={styles.banner}
+        className={classNames(styles.banner, isWindow && styles.windows)}
         rel="noreferrer noopener"
         target="_blank"
       >
