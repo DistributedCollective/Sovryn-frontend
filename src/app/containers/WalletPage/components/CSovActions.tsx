4551f85bb5a3f9be258b4d181e7dc146ca6e515d
soulBit <its.soulBit@gmail.com>
2021-02-17 21:04:19 +0000

    Disable Claim CSOV option when none available. Closes #392 (#393)

--- src/app/containers/WalletPage/components/CSovActions.tsx
+++ src/app/containers/WalletPage/components/CSovActions.tsx
@@ -35,7 +35,7 @@ export function CSovActions(props: Props) {
         text={t(translations.userAssets.actions.claimSov)}
         className="text-gold button-round"
         onClick={() => setDialog(DialogType.CLAIM)}
-        disabled={processed}
+        disabled={processed || parseInt(props.amount) === 0}
         loading={loading}
       />
       {/*<Button*/}
