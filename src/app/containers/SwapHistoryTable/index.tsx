000b2eac4ed48dd40b1548c633039ba5552d2f60
Daiki Saito <daiki.saito92@gmail.com>
2021-02-25 13:43:15 +0900

    implement swap history

--- src/app/containers/SwapHistoryTable/index.tsx
+++ src/app/containers/SwapHistoryTable/index.tsx
@@ -1,27 +1,44 @@
 import React, { useState } from 'react';
 import { Table, TablePaginationConfig } from 'antd';
+import { EventData } from 'web3-eth-contract';
+import moment from 'moment';
+import { Tooltip } from '@blueprintjs/core';
 import { AssetsDictionary } from 'utils/dictionaries/assets-dictionary';
-import { tempTransactions } from './constants';
+import { useGetContractPastEvents } from '../../hooks/useGetContractPastEvents';
+import { useBorrowAssetPrice } from '../../hooks/trading/useBorrowAssetPrice';
+import { symbolByTokenAddress } from 'utils/blockchain/contract-helpers';
+import { weiTo2, weiTo18 } from 'utils/blockchain/math-helpers';
+import { prettyTx } from 'utils/helpers';
+import { Asset } from '../../../types/asset';
+import { weiToNumberFormat } from 'utils/display-text/format';
 
 interface Props {
   title: string;
 }
 
-interface tableDataSourceProps {
+interface TableDataSourceProps {
   id: number;
   date?: string;
   sendAsset?: string;
-  sendAmount?: number;
+  sendAmount?: string;
   receiveAsset?: string;
-  receiveAmount?: number;
-  transactionFee?: number;
+  receiveAmount?: string;
+  transactionFee?: string;
   status?: string;
   hash?: string;
+}
+interface EventReturnValuesProps {
+  _fromAmount?: string;
+  _fromToken?: string;
+  _smartToken?: string;
+  _toAmount?: string;
+  _toToken?: string;
+  _trader?: string;
 }
 
 const defaultPagination: TablePaginationConfig = {
   current: 1,
-  pageSize: 9,
+  pageSize: 10,
   total: 0,
   position: ['bottomCenter'],
 };
@@ -30,6 +47,34 @@ const assetsList = AssetsDictionary.list();
 
 export function SwapHistoryTable({ title }: Props) {
   const [pagination, setPagination] = useState(defaultPagination);
+
+  const swapStates = useGetContractPastEvents('swapNetwork', 'Conversion');
+
+  const formatTransactions = (states): TableDataSourceProps[] => {
+    const events: EventData[] = states.events.slice(0, 100);
+
+    return events.map((row: any, index: number) => {
+      const returnValues: EventReturnValuesProps = row.returnValues;
+      const _fromToken = returnValues._fromToken || '';
+      const _fromAmount = returnValues._fromAmount || '';
+      const _toToken = returnValues._toToken || '';
+      const _toAmount = returnValues._toAmount || '';
+      const fromAsset = symbolByTokenAddress(_fromToken);
+      const toAsset = symbolByTokenAddress(_toToken);
+
+      return {
+        id: index,
+        date: row.eventDate || '',
+        sendAsset: fromAsset,
+        sendAmount: _fromAmount,
+        receiveAsset: toAsset,
+        receiveAmount: _toAmount,
+        transactionFee: '0',
+        status: 'pending',
+        hash: row.transactionHash,
+      };
+    });
+  };
 
   const handleTableChange = (_pagination: TablePaginationConfig) => {
     setPagination(_pagination);
@@ -45,12 +90,17 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Date/Time',
       dataIndex: 'date',
       key: 'date',
+      sorter: (a, b) => moment(a.date).unix() - moment(b.date).unix(),
       width: '15%',
       render: (date: string) => {
+        const formatDate = new Date(date).toLocaleString('en-GB', {
+          timeZone: 'GMT',
+        });
+
         return (
           <div className="transaction-date-time">
-            <div>{date}</div>
-            <div>{`21:21 CET`}</div>
+            <div>{formatDate.split(',')[0]}</div>
+            <div>{`${formatDate.split(',')[1]} GMT`}</div>
           </div>
         );
       },
@@ -59,6 +109,8 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Sending Asset',
       dataIndex: 'sendAsset',
       key: 'sendAsset',
+      sorter: (a, b) =>
+        a.sendAsset.toLowerCase() <= b.sendAsset.toLowerCase() ? -1 : 1,
       width: '15%',
       render: (sendAsset: string) => {
         return (
@@ -75,15 +127,30 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Amount Sent',
       dataIndex: 'sendAmount',
       key: 'sendAmount',
+      sorter: (a, b) => (Number(a.sendAmount) <= Number(b.sendAmount) ? -1 : 1),
       width: '15%',
-      render: (sendAmount: number, row: tableDataSourceProps) => {
+      render: (sendAmount: string, row: TableDataSourceProps) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { value: price } = useBorrowAssetPrice(
+          Asset[(row.sendAsset || 'USDT').toUpperCase()],
+          Asset.USDT,
+        );
+        const sendAssetPrice = weiTo2(price);
+        const sendAmountInUSD = (
+          Number(weiToNumberFormat(sendAmount, 4)) * Number(sendAssetPrice)
+        ).toFixed(2);
+
         return (
           <div className="transaction-send-amount">
             <div className="transaction-send-amount__origin">
-              {`${sendAmount} ${row.sendAsset}`}
+              <Tooltip content={weiTo18(sendAmount)}>
+                <span>{`${weiToNumberFormat(sendAmount, 4)} ${
+                  row.sendAsset
+                }`}</span>
+              </Tooltip>
             </div>
             <div className="transaction-send-amount__usd">
-              {`≈ ${sendAmount} USD`}
+              {`≈ ${sendAmountInUSD} USD`}
             </div>
           </div>
         );
@@ -93,6 +160,8 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Receiving Asset',
       dataIndex: 'receiveAsset',
       key: 'receiveAsset',
+      sorter: (a, b) =>
+        a.receiveAsset.toLowerCase() <= b.receiveAsset.toLowerCase() ? -1 : 1,
       width: '15%',
       render: (receiveAsset: string) => {
         return (
@@ -111,15 +180,30 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Amount Received',
       dataIndex: 'receiveAmount',
       key: 'receiveAmount',
+      sorter: (a, b) =>
+        Number(a.receiveAmount) <= Number(b.receiveAmount) ? -1 : 1,
       width: '15%',
-      render: (receiveAmount: number, row: tableDataSourceProps) => {
+      render: (receiveAmount: number, row: TableDataSourceProps) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const { value: price } = useBorrowAssetPrice(
+          Asset[(row.receiveAsset || 'USDT').toUpperCase()],
+          Asset.USDT,
+        );
+        const receiveAssetPrice = weiTo2(price);
+        const receiveAmountInUSD = (
+          Number(weiToNumberFormat(receiveAmount, 4)) *
+          Number(receiveAssetPrice)
+        ).toFixed(2);
+
         return (
           <div className="transaction-receive-amount">
             <div className="transaction-receive-amount__origin">
-              {`${receiveAmount} ${row.receiveAsset}`}
+              <Tooltip content={weiTo18(receiveAmount)}>
+                {`${weiToNumberFormat(receiveAmount, 4)} ${row.receiveAsset}`}
+              </Tooltip>
             </div>
             <div className="transaction-receive-amount__usd">
-              {`≈ ${receiveAmount} USD`}
+              {`≈ ${receiveAmountInUSD} USD`}
             </div>
           </div>
         );
@@ -129,8 +213,10 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Transaction Fee',
       dataIndex: 'transactionFee',
       key: 'transactionFee',
+      sorter: (a, b) =>
+        Number(a.transactionFee) <= Number(b.transactionFee) ? -1 : 1,
       width: '15%',
-      render: (transactionFee: number, row: tableDataSourceProps) => {
+      render: (transactionFee: number, row: TableDataSourceProps) => {
         return (
           <div className="transaction-fee-amount">
             <div className="transaction-fee-amount__origin">
@@ -147,17 +233,25 @@ export function SwapHistoryTable({ title }: Props) {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
+      sorter: (a, b) =>
+        a.status.toLowerCase() <= b.status.toLowerCase() ? -1 : 1,
       width: '10%',
-      render: (status: string, row: tableDataSourceProps) => {
+      render: (status: string, row: TableDataSourceProps) => {
         return (
           <div>
             <div className="transaction-status">{status}</div>
-            <div className="transaction-hash">{row.hash}</div>
+            <div className="transaction-hash">
+              <Tooltip content={row.hash}>
+                {prettyTx(row.hash || '', 4, 5)}
+              </Tooltip>
+            </div>
           </div>
         );
       },
     },
   ];
+
+  const transactions: TableDataSourceProps[] = formatTransactions(swapStates);
 
   return (
     <div className="swap-history-table">
@@ -171,7 +265,7 @@ export function SwapHistoryTable({ title }: Props) {
           }
           columns={columns}
           rowKey={record => record.id}
-          dataSource={tempTransactions}
+          dataSource={transactions}
           pagination={pagination}
           onChange={handleTableChange}
         />
