10aaf9c9b19efad3e9b566e1f10951714e330e4a
Daiki Saito <daiki.saito92@gmail.com>
2021-03-10 02:33:56 +0900

    remove unnecessary codes

--- src/app/containers/LendBorrowSovryn/index.tsx
+++ src/app/containers/LendBorrowSovryn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 
