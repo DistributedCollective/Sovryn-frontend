3c882b9052ffa0fbb5e3ba57d36b0499ca580347
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2021-03-09 17:09:04 +0200

    Fix/nfty (#461)
    * prevent double submit for claiming tokens #395
    
    * Ads whitelisting for claimed sov token
    
    * nft fixes

--- src/app/components/SovGenerationNFTS/index.tsx
+++ src/app/components/SovGenerationNFTS/index.tsx
@@ -8,9 +8,12 @@ import React from 'react';
 import sov_1 from 'assets/images/wallet/sov_1.jpg';
 import sov_2 from 'assets/images/wallet/sov_2.jpg';
 import sov_3 from 'assets/images/wallet/sov_3.jpg';
+import bfntImg from 'assets/images/wallet/bnft.png';
 import sov_icon from 'assets/images/wallet/icon-sov.svg';
 import { useAccount } from '../../hooks/useAccount';
 import { useCacheCallWithValue } from '../../hooks/useCacheCallWithValue';
+
+const bdnft = '0x65299adDc002Dd792797288ee6599772d20970Da'.toLowerCase();
 
 export function SovGenerationNFTS() {
   const account = useAccount();
@@ -58,6 +61,18 @@ export function SovGenerationNFTS() {
     <div className="sovryn-border p-3 mb-5 pb-5">
       <p className="text-center sov-title mb-5">SOV Generation 01 NFT's</p>
       <div className="d-lg-flex text-center align-items-center justify-content-center">
+        {account.toLowerCase() === bdnft && (
+          <div className="mr-md-5 mb-sm-5 mb-5 ml-3 mr-3 position-relative d-inline-block">
+            <div className="image-bordered">
+              <img
+                className="w-100 h-100 image-responsive"
+                src={bfntImg}
+                alt="Staying Sovryn for 10 halvings"
+              />
+            </div>
+          </div>
+        )}
+
         {tiers.map((item, index) => {
           return (
             item.balance !== '0' && (
