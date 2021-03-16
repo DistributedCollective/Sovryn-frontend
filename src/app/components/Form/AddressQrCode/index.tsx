9fb674118476e7a08fe34374d57424967d9e4547
Daiki Saito <daiki.saito92@gmail.com>
2021-03-17 04:47:12 +0900

    implement new QrCode component

--- src/app/components/Form/AddressQrCode/index.tsx
+++ src/app/components/Form/AddressQrCode/index.tsx
@@ -5,65 +5,60 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { useTranslation } from 'react-i18next';
 import { toastSuccess } from 'utils/toaster';
 import { translations } from 'locales/i18n';
-import styles from './index.module.css';
 
 interface Props {
+  label?: string;
   address: string;
   hideClickToCopy?: boolean;
 }
 
-export function AddressQrCode({ address, hideClickToCopy }: Props) {
+export function AddressQrCode({ label, address, hideClickToCopy }: Props) {
   const { t } = useTranslation();
   return (
-    <>
-      <div className={styles.qrCodeContainer}>
-        <QRCode
-          value={address}
-          renderAs="svg"
-          bgColor="var(--white)"
-          fgColor="var(--primary)"
-          includeMargin={true}
-          className="rounded w-75 h-75"
-        />
+    <div className="tw-qrcode-container">
+      {label && <div className="tw-qrcode-label">{label}</div>}
+      <div className="tw-qrcode-wrapper">
+        <QRCode value={address} renderAs="svg" includeMargin={false} />
       </div>
       {!hideClickToCopy && (
-        <CopyToClipboard
-          text={address}
-          onCopy={() =>
-            toastSuccess(<>{t(translations.onCopy.address)}</>, 'copy')
-          }
-        >
-          <div className={styles.addressWrapper}>
-            <div className="d-flex flex-row justify-content-between align-items-center w-100">
-              <div className="flex-shrink-1 flex-grow-0 overflow-hidden">
-                <Text ellipsize>{address}</Text>
-              </div>
-              <div className="flex-shrink-0 flex-grow-0 ml-3">
-                <svg
-                  id="content_copy-24px"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    id="Path_11"
-                    data-name="Path 11"
-                    d="M0,0H20V20H0Z"
-                    fill="none"
-                  />
-                  <path
-                    id="Path_12"
-                    data-name="Path 12"
-                    d="M13.053,1H3.579A1.613,1.613,0,0,0,2,2.636V14.091H3.579V2.636h9.474Zm2.368,3.273H6.737A1.613,1.613,0,0,0,5.158,5.909V17.364A1.613,1.613,0,0,0,6.737,19h8.684A1.613,1.613,0,0,0,17,17.364V5.909A1.613,1.613,0,0,0,15.421,4.273Zm0,13.091H6.737V5.909h8.684Z"
-                    fill="#2274a5"
-                  />
-                </svg>
+        <div className="tw-qrcode-clipboard">
+          <CopyToClipboard
+            text={address}
+            onCopy={() =>
+              toastSuccess(<>{t(translations.onCopy.address)}</>, 'copy')
+            }
+          >
+            <div className="tw-qrcode-address-wrapper">
+              <div className="tw-flex tw-flex-row tw-justify-between tw-items-center tw-w-full">
+                <div className="tw-flex-shrink tw-flex-grow-0 tw-overflow-hidden tw-text-white">
+                  <Text ellipsize>{address}</Text>
+                </div>
+                <div className="tw-flex-shrink-0 tw-flex-grow-0 tw-ml-4">
+                  <svg
+                    id="content_copy-24px"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      id="Path_11"
+                      data-name="Path 11"
+                      d="M0,0H20V20H0Z"
+                      fill="none"
+                    />
+                    <path
+                      id="Path_12"
+                      data-name="Path 12"
+                      d="M13.053,1H3.579A1.613,1.613,0,0,0,2,2.636V14.091H3.579V2.636h9.474Zm2.368,3.273H6.737A1.613,1.613,0,0,0,5.158,5.909V17.364A1.613,1.613,0,0,0,6.737,19h8.684A1.613,1.613,0,0,0,17,17.364V5.909A1.613,1.613,0,0,0,15.421,4.273Zm0,13.091H6.737V5.909h8.684Z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
-        </CopyToClipboard>
+          </CopyToClipboard>
+        </div>
       )}
-    </>
+    </div>
   );
 }
