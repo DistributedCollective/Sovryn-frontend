767d46eb9342b06eaddc87d58473dadaa710d9d2
BetsyBraddock <k.shall0507+mutant@gmail.com>
2021-03-22 18:25:30 +0000

    Arbitrage opportunity banner

--- src/app/containers/SwapTradeForm/components/arbitrage.tsx
+++ src/app/containers/SwapTradeForm/components/arbitrage.tsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { symbolByTokenAddress } from 'utils/blockchain/contract-helpers';
 import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
+import { Popover, Icon } from '@blueprintjs/core';
+import { weiToFixed } from 'utils/blockchain/math-helpers';
 
 const s = translations.swapTradeForm;
 
@@ -12,6 +14,8 @@ export function Arbitrage() {
   const api = backendUrl[currentChainId];
   const [data, setData] = useState({
     USDT: {
+      oracleRate: '0',
+      negativeDelta: false,
       rateToBalance: { amount: 0, from: '', to: '', rate: 0 },
     },
   });
@@ -20,22 +24,64 @@ export function Arbitrage() {
     axios.get(api + 'amm/arbitrage').then(res => setData(res.data));
   }, [api]);
 
-  const fromAmount = data.USDT.rateToBalance.amount;
+  function calculateEarn(isNegative, oracleRate, fromAmount, toAmount) {
+    const rate = parseFloat(weiToFixed(oracleRate, 8));
+    console.log(rate);
+    const oraclePrice = isNegative
+      ? (1 / rate) * fromAmount
+      : rate * fromAmount;
+    console.log(fromAmount);
+    console.log(oraclePrice);
+    return (toAmount - oraclePrice).toFixed(4);
+  }
+
+  const fromAmount = data.USDT.rateToBalance.amount.toFixed(4);
   const fromToken = symbolByTokenAddress(data.USDT.rateToBalance.from);
-  const toAmount = data.USDT.rateToBalance.rate;
+  const toAmount = data.USDT.rateToBalance.rate.toFixed(2);
   const toToken = symbolByTokenAddress(data.USDT.rateToBalance.to);
+  const earn = calculateEarn(
+    data.USDT.negativeDelta,
+    data.USDT.oracleRate,
+    data.USDT.rateToBalance.amount,
+    data.USDT.rateToBalance.rate,
+  );
+  const tooltipText = (
+    <div className="px-5 py-4 font-weight-light">
+      <p>
+        {t(s.arbitrage.popover_p1, {
+          fromAmount: fromAmount,
+          fromToken: fromToken,
+          toAmount: toAmount,
+          toToken: toToken,
+          earn: earn,
+        })}
+      </p>
+      <p>
+        {t(s.arbitrage.popover_p2, {
+          fromToken: fromToken,
+        })}
+      </p>
+    </div>
+  );
 
   return (
     <div className="my-3">
       <div className="text-white mb-5 p-3 rounded border border-gold ">
         {t(s.arbitrage.best_rate)}{' '}
         <span className="text-gold">
-          {fromAmount.toFixed(2)} {fromToken}
+          {fromAmount} {fromToken}
         </span>{' '}
         {t(s.arbitrage.for)}{' '}
         <span className="text-gold">
-          {toAmount.toFixed(4)} {toToken}
+          {toAmount} {toToken}
         </span>
+        <Popover
+          content={tooltipText}
+          className="pl-3"
+          popoverClassName={'w-50 mx-1'}
+        >
+          <Icon icon={'info-sign'} />
+        </Popover>
       </div>
     </div>
   );
