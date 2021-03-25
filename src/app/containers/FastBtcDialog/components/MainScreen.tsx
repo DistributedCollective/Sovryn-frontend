5a4390f726f12dd2587387d09ddf023372649eba
boltsr <38220699+boltsr@users.noreply.github.com>
2021-03-25 22:22:47 +1000

    bug/512-remove0 (#518)
    * change the decimal of max amount
    
    * fixed decimal and currency position
    
    * fix warning

--- src/app/containers/FastBtcDialog/components/MainScreen.tsx
+++ src/app/containers/FastBtcDialog/components/MainScreen.tsx
@@ -6,7 +6,6 @@ import styles from '../index.module.css';
 import { translations } from '../../../../locales/i18n';
 import { AddressButton } from './AddressButton';
 import { AddressQrCode } from '../../../components/Form/AddressQrCode';
-import { toNumberFormat } from '../../../../utils/display-text/format';
 
 interface MainScreenProps {
   state: FastBtcDialogState;
@@ -27,14 +26,22 @@ export function MainScreen({ state, dispatch }: MainScreenProps) {
           {t(translations.fastBtcDialog.limits.title)}
         </div>
         <div className={styles.limitsValue}>
-          •{' '}
-          {t(translations.fastBtcDialog.limits.min, {
-            amount: toNumberFormat(state.limits.min, 4),
-          })}
-          <br />•{' '}
-          {t(translations.fastBtcDialog.limits.max, {
-            amount: toNumberFormat(state.limits.max, 4),
-          })}
+          <div>
+            •{' '}
+            {t(translations.fastBtcDialog.limits.min, {
+              amount: parseFloat(state.limits.min.toFixed(4)),
+            })}{' '}
+          </div>
+          <div> {t(translations.fastBtcDialog.limits.btc)} </div>
+        </div>
+        <div className={styles.limitsValue}>
+          <div>
+            •{' '}
+            {t(translations.fastBtcDialog.limits.max, {
+              amount: parseFloat(state.limits.max.toFixed(4)),
+            })}{' '}
+          </div>
+          <div>{t(translations.fastBtcDialog.limits.btc)} </div>
         </div>
       </div>
 
