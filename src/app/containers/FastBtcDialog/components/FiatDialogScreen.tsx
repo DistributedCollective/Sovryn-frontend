59432c5139b627be5151412a86ed248bf18f0780
Ana G <40721795+Bitcoinera@users.noreply.github.com>
2021-03-31 16:10:29 +0200

    Add fiat onramp (#481)
    * add buy section. add transak sample code
    
    * showing transak widget
    
    * lint
    
    * move transak.ts to utils
    
    * remove TRANSAK_API_K since renamed with REACT_APP
    
    * encapsulate openTransak with useEffect
    
    * change transaks back to components folder
    
    * Update src/app/containers/BuyBTCPage/index.tsx
    
    Co-authored-by: Victor Creed <69458664+creed-victor@users.noreply.github.com>
    
    * lint
    
    * change default crypto to btc
    
    * change environment to env variable
    
    * Update src/app/containers/BuyBTCPage/index.tsx
    
    * Update src/react-app-env.d.ts
    
    * add new transak env variables to .env.example
    
    * remove buyBTCPage component
    
    * move transak to FastBTC dialog
    
    * add onClose
    
    * important fix
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * add dialog before transak
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * make address look better
    
    * fix dispatching steps to state
    
    * add explanation in FiatDialog. Translations
    
    * hyper important fix
    
    * replace custom classes with bootstrap classes
    
    * fix merge errors
    
    * fix the repetitive useEffect for real
    
    * add margin top for button
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update src/locales/pt_br/translation.json
    
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    
    * Update .env.example
    
    * added public api keys
    
    * Update src/app/containers/FastBtcDialog/components/transak.tsx
    
    * Update .env.production
    
    * added production api keys
    
    Co-authored-by: Victor Creed <69458664+creed-victor@users.noreply.github.com>
    Co-authored-by: Rytis Grincevičius <rytis.grincevicius@gmail.com>
    Co-authored-by: bitcoinuser <bitcoinuser@users.noreply.github.com>
    Co-authored-by: Victor Creed <rytis.grincevicius+cv@gmail.com>

---
+++ src/app/containers/FastBtcDialog/components/FiatDialogScreen.tsx
@@ -0,0 +1,42 @@
+import React, { Dispatch } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FastBtcDialogState } from '../types';
+import { translations } from 'locales/i18n';
+import { actions } from '../slice';
+import styles from './transaction.module.css';
+import { FiatButton } from './FiatButton';
+
+interface Props {
+  state: FastBtcDialogState;
+  address: string;
+  dispatch: Dispatch<any>;
+}
+
+export function FiatDialogScreen({ address, dispatch }: Props) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className={styles.txData && 'mb-2'}>
+        <div className={styles.instructionsTitle}>
+          {t(translations.fastBtcDialog.fiatDialog.title)}
+        </div>
+        <div className="mt-2">
+          {t(translations.fastBtcDialog.fiatDialog.explanation)}
+        </div>
+        <div className="mt-2">
+          <div>
+            <strong>{t(translations.fastBtcDialog.transaction.address)}</strong>{' '}
+            {address}
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <FiatButton
+          loading={false}
+          ready={true}
+          onClick={() => dispatch(actions.openFiatOnRamp())}
+        />
+      </div>
+    </>
+  );
+}
