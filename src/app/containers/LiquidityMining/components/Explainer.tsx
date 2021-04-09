8287bf14ca4a49e01ca95a08cb61f37f82479f52
BetsyBraddock <69518422+BetsyBraddock@users.noreply.github.com>
2021-04-09 13:45:04 +0100

    Feat/liquidity mining master (#604)
    * Changed stats page to include tvl and trading volume
    
    * Refactoring and translation strings
    
    * Started adding Amm Stats to stats page
    
    * Liquidity mining with explainer
    
    * Fixed merged conflicts during cherry pick
    
    * Requested changes
    
    * Improvements to liquidity mining UI
    
    * Small style fix
    
    * Small fixes to liquidity mining UI

---
+++ src/app/containers/LiquidityMining/components/Explainer.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function Explainer() {
+  return (
+    <>
+      <div className="font-family-montserrat">
+        <p className="text-white font-weight-bold mt-3">
+          <u style={{ textUnderlinePosition: 'under' }}>
+            Earn SOV by depositing liquidity into the USDT/BTC liquidity pool
+          </u>
+        </p>
+        <p>
+          Start date: 1st April 2021, 12.00 UTC
+          <br />
+          End date: 1st May 2021, 12.00 UTC
+        </p>
+        <p>
+          Reward pool for depositing USDT: 25,000 SOV
+          <br />
+          Reward pool for depositing RBTC: 25,000 SOV
+        </p>
+        <p>
+          Your share of the pool is calculated by:
+          <div className="w-100 text-center text-white mt-2">
+            liquidity added by you * number of blocks held
+          </div>
+          <Line></Line>
+          <div className="w-100 text-center text-white">
+            total liquidity added * number of blocks held
+          </div>
+        </p>
+        <p>
+          Rewards will be deposited into liquidity providers' wallets on or
+          before 8th May 2021
+        </p>
+      </div>
+    </>
+  );
+}
+
+const Line = styled.div`
+  background-color: white;
+  width: 400px;
+  height: 1px;
+  margin: 0 auto;
+`;
