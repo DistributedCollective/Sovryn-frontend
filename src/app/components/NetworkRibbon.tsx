64c3db0ab79a6dd6b565733c0bac75bcb3e6fa03
boltsr <skrullweb.dev@gmail.com>
2021-04-14 10:32:22 +1000

    - detect network change - display dialog

--- src/app/components/NetworkRibbon.tsx
+++ src/app/components/NetworkRibbon.tsx
@@ -1,30 +1,42 @@
-import React, { useMemo } from 'react';
 import { useWalletContext } from '@sovryn/react-wallet';
 import { web3Wallets } from '@sovryn/wallet';
+import React, { useEffect, useState } from 'react';
+
 import { currentChainId, currentNetwork } from '../../utils/classifiers';
+import { Dialog } from '../containers/Dialog/Loadable';
 
 export function NetworkRibbon() {
   const { connected, wallet } = useWalletContext();
+  const getStatus = () =>
+    connected &&
+    web3Wallets.includes(wallet.providerType) &&
+    wallet.chainId !== currentChainId;
+  const [isConnect, setShow] = useState(getStatus());
 
-  const show = useMemo(() => {
-    return (
-      connected &&
-      web3Wallets.includes(wallet.providerType) &&
-      wallet.chainId !== currentChainId
-    );
+  useEffect(() => {
+    setShow(getStatus());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connected, wallet.chainId, wallet.providerType]);
-
+  const handleClose = () => {
+    setShow(false);
+  };
   return (
     <>
-      {show && (
-        <div className="bg-red py-3">
+      <Dialog
+        isOpen={isConnect}
+        onClose={handleClose}
+        canOutsideClickClose={false}
+        isCloseButtonShown={true}
+        canEscapeKeyClose={false}
+        className="fw-900 p-4"
+      >
+        <div className="py-3">
           <div className="container text-center">
             You are connected to wrong network. Please switch to RSK{' '}
             {currentNetwork}.
           </div>
         </div>
-      )}
+      </Dialog>
     </>
   );
 }
