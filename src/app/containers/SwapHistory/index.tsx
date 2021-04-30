234f41ab0b72eb1a3d65c05293d69001329e1efb
rick23p <peymannaderi3@gmail.com>
2021-04-30 16:45:47 +0430

    show pending transaction on swap history table

--- src/app/containers/SwapHistory/index.tsx
+++ src/app/containers/SwapHistory/index.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 // import ReactPaginate from 'react-paginate';
 import iconSuccess from 'assets/images/icon-success.svg';
 import iconRejected from 'assets/images/icon-rejected.svg';
-import { Sovryn } from 'utils/sovryn';
+import iconPending from 'assets/images/icon-pending.svg';
 import { bignumber } from 'mathjs';
 import { weiToFixed } from 'utils/blockchain/math-helpers';
 import { numberToUSD } from 'utils/display-text/format';
@@ -23,14 +23,19 @@ import { translations } from '../../../locales/i18n';
 import { LoadableValue } from '../../components/LoadableValue';
 import { useCachedAssetPrice } from '../../hooks/trading/useCachedAssetPrice';
 import { AssetRenderer } from '../../components/AssetRenderer';
+import { useSelector } from 'react-redux';
+import { selectTransactionArray } from 'store/global/transactions-store/selectors';
+import { TxStatus, TxType } from 'store/global/transactions-store/types';
 
 export function SwapHistory() {
+  const transactions = useSelector(selectTransactionArray);
   const account = useAccount();
   const url = backendUrl[currentChainId];
   const [history, setHistory] = useState([]) as any;
   const [currentHistory, setCurrentHistory] = useState([]) as any;
   const [loading, setLoading] = useState(false);
   const { t } = useTranslation();
+  const assets = AssetsDictionary.list();
 
   let cancelTokenSource;
   const getData = () => {
@@ -77,7 +82,45 @@ export function SwapHistory() {
     setCurrentHistory(history.slice(offset, offset + pageLimit));
   };
 
-  const assets = AssetsDictionary.list();
+  const onGoingTransactions = useMemo(() => {
+    return transactions
+      .filter(
+        tx =>
+          tx.type === TxType.CONVERT_BY_PATH &&
+          [TxStatus.FAILED, TxStatus.PENDING].includes(tx.status),
+      )
+      .map(item => {
+        const { customData } = item;
+        let assetFrom = [] as any;
+        let assetTo = [] as any;
+
+        assetFrom = assets.find(
+          currency => currency.asset === customData?.sourceToken,
+        );
+        assetTo = assets.find(
+          currency => currency.asset === customData?.targetToken,
+        );
+
+        const data = {
+          status: item.status,
+          timestamp: customData?.date,
+          transaction_hash: item.transactionHash,
+          returnVal: {
+            _fromAmount: customData?.amount,
+            _toAmount: customData?.minReturn || null,
+          },
+        };
+
+        return (
+          <AssetRow
+            key={item.transactionHash}
+            data={data}
+            itemFrom={assetFrom}
+            itemTo={assetTo}
+          />
+        );
+      });
+  }, [assets, transactions]);
 
   return (
     <section>
@@ -116,6 +159,7 @@ export function SwapHistory() {
                 </td>
               </tr>
             )}
+            {onGoingTransactions}
             {currentHistory.map(item => {
               let assetFrom = [] as any;
               let assetTo = [] as any;
@@ -168,15 +212,9 @@ interface AssetProps {
 }
 
 function AssetRow({ data, itemFrom, itemTo }: AssetProps) {
-  const txStatus = async (hash: string) => {
-    return await Sovryn.getWeb3().eth.getTransactionReceipt(hash);
-  };
-  const tx = txStatus(data.transaction_hash).then(res => {
-    return res;
-  });
-
   const dollars = useCachedAssetPrice(itemTo.asset, Asset.USDT);
   const dollarValue = useMemo(() => {
+    if (data.returnVal._toAmount === null) return '';
     return bignumber(data.returnVal._toAmount)
       .mul(dollars.value)
       .div(10 ** itemTo.decimals)
@@ -219,16 +257,24 @@ function AssetRow({ data, itemFrom, itemTo }: AssetProps) {
       <td>
         <div className="d-flex align-items-center justify-content-between col-lg-10 col-md-12 p-0">
           <div>
-            {tx && <p className="m-0">Confirmed</p>}
-            {!tx && <p className="m-0">Failed</p>}
+            {!data.status && <p className="m-0">Confirmed</p>}
+            {data.status === TxStatus.FAILED && <p className="m-0">Failed</p>}
+            {data.status === TxStatus.PENDING && <p className="m-0">Pending</p>}
             <LinkToExplorer
               txHash={data.transaction_hash}
               className="text-gold font-weight-normal text-nowrap"
             />
           </div>
           <div>
-            {tx && <img src={iconSuccess} title="Confirmed" alt="Confirmed" />}
-            {!tx && <img src={iconRejected} title="Failed" alt="Failed" />}
+            {!data.status && (
+              <img src={iconSuccess} title="Confirmed" alt="Confirmed" />
+            )}
+            {data.status === TxStatus.FAILED && (
+              <img src={iconRejected} title="Failed" alt="Failed" />
+            )}
+            {data.status === TxStatus.PENDING && (
+              <img src={iconPending} title="Pending" alt="Pending" />
+            )}
           </div>
         </div>
       </td>
