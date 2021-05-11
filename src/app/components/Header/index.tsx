b6be951b708b2b12bfb0c43d2bb1670010e99bd9
soulBit <its.soulBit@gmail.com>
2021-05-11 10:54:42 +0100

    Feat/redesign updates (#1050)
    * Add highcharts for lending APR graphs
    
    * Fix TradingChart attribution link color
    
    * Update rounded corner size on Margin Trade form, dropdown chevron padding
    
    * Restore Notification form
    
    * Update corner radius on Spot trade form
    
    * Styling updates for Lending charts
    
    * Add translation string for Loan graph legend
    
    * Margin trade pair select padding fix
    
    * Header menu dropdown position fix
    
    * Use full width for Lending APR display
    
    * Add Tailwindcss prefix, remove unused dependencies

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -12,7 +12,7 @@ import logoSvg from 'assets/images/sovryn-logo-white.svg';
 import iconNewTab from 'assets/images/iconNewTab.svg';
 import { usePageViews } from 'app/hooks/usePageViews';
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
-import { MenuItem, Popover, Menu as BPMenu } from '@blueprintjs/core';
+import { MenuItem, Popover, Menu as BPMenu, Position } from '@blueprintjs/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { translations } from 'locales/i18n';
@@ -211,6 +211,7 @@ export function Header() {
         content={content}
         hoverOpenDelay={0}
         hoverCloseDelay={0}
+        position={Position.BOTTOM_LEFT}
       >
         {children}
       </StyledPopover>
--- src/app/pages/MarginTradePage/components/TradeForm/index.tsx
+++ src/app/pages/MarginTradePage/components/TradeForm/index.tsx
@@ -73,7 +73,7 @@ export function TradeForm() {
 
   return (
     <>
-      <div className="tw-trading-form-card tw-bg-black lg:tw-rounded tw-p-12">
+      <div className="tw-trading-form-card tw-bg-black lg:tw-rounded-3xl tw-p-12">
         <div className="tw-mw-320 tw-mx-auto">
           <FormGroup
             label={t(translations.marginTradePage.tradeForm.labels.pair)}
@@ -85,6 +85,7 @@ export function TradeForm() {
               filterable={false}
               onChange={value => dispatch(actions.setPairType(value))}
               itemRenderer={renderItemNH}
+              innerClasses="tw-pr-3"
               valueRenderer={(item: Option) => (
                 <Text ellipsize className="tw-text-center">
                   {item.label}
