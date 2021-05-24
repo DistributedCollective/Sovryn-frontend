28a26f3dee2065af2ee75a018f3d441cf2ff3932
Soul Bit <its.soulBit@gmail.com>
2021-05-24 13:04:27 +0100

    [Buy SOV Page] Update promotions

--- src/app/pages/BuySovPage/components/Promotions/index.tsx
+++ src/app/pages/BuySovPage/components/Promotions/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import imgPromotion1 from 'assets/sov/promotion-1.svg';
-import imgPromotion2 from 'assets/sov/promotion-2.svg';
+// import imgPromotion1 from 'assets/sov/promotion-1.svg';
+// import imgPromotion2 from 'assets/sov/promotion-2.svg';
+import imgPromotion3 from 'assets/sov/promotion-3.svg';
 import { Promotion } from './promotion';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../../../locales/i18n';
@@ -14,7 +15,7 @@ export function Promotions() {
         {t(translations.buySovPage.promotions.title)}
       </H1>
 
-      <Promotion
+      {/* <Promotion
         title={t(translations.buySovPage.promotions.p1.title)}
         content={<>{t(translations.buySovPage.promotions.p1.text)}</>}
         cta={t(translations.buySovPage.promotions.p1.cta)}
@@ -28,6 +29,14 @@ export function Promotions() {
         cta={t(translations.buySovPage.promotions.p2.cta)}
         href="https://twitter.com/SovrynBTC/status/1381112432945459201"
         image={imgPromotion1}
+      /> */}
+
+      <Promotion
+        title={t(translations.buySovPage.promotions.p3.title)}
+        content={<>{t(translations.buySovPage.promotions.p3.text)}</>}
+        cta={t(translations.buySovPage.promotions.p3.cta)}
+        href="/liquidity"
+        image={imgPromotion3}
       />
     </article>
   );
