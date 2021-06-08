85c9388191136941f47ca942d4126798fb00fb03
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-06-08 09:34:02 +0300

    new pairs for spot trading and charts

--- src/app/pages/SpotTradingPage/types.ts
+++ src/app/pages/SpotTradingPage/types.ts
@@ -6,30 +6,42 @@ export enum SpotPairType {
   SOV_RBTC = 'SOV_RBTC',
   SOV_USDT = 'SOV_USDT',
   SOV_DOC = 'SOV_DOC',
+  SOV_ETH = 'SOV_ETH',
+  SOV_BPRO = 'SOV_BPRO',
+  SOV_MOC = 'SOV_MOC',
   RBTC_USDT = 'RBTC_USDT',
   RBTC_DOC = 'RBTC_DOC',
   USDT_DOC = 'USDT_DOC',
   MOC_RBTC = 'MOC_RBTC',
+  ETH_RBTC = 'ETH_RBTC',
 }
 
 export const pairs = {
   [SpotPairType.SOV_RBTC]: [Asset.SOV, Asset.RBTC],
   [SpotPairType.SOV_USDT]: [Asset.SOV, Asset.USDT],
   [SpotPairType.SOV_DOC]: [Asset.SOV, Asset.DOC],
+  [SpotPairType.SOV_BPRO]: [Asset.SOV, Asset.BPRO],
+  [SpotPairType.SOV_ETH]: [Asset.SOV, Asset.ETH],
+  [SpotPairType.SOV_MOC]: [Asset.SOV, Asset.MOC],
   [SpotPairType.RBTC_USDT]: [Asset.RBTC, Asset.USDT],
   [SpotPairType.RBTC_DOC]: [Asset.RBTC, Asset.DOC],
   [SpotPairType.USDT_DOC]: [Asset.USDT, Asset.DOC],
-  // [SpotPairType.MOC_RBTC]: [Asset.MOC, Asset.RBTC],
+  [SpotPairType.MOC_RBTC]: [Asset.MOC, Asset.RBTC],
+  [SpotPairType.ETH_RBTC]: [Asset.ETH, Asset.RBTC],
 };
 
 export const pairList = [
   SpotPairType.SOV_RBTC,
   SpotPairType.SOV_USDT,
+  SpotPairType.SOV_ETH,
+  SpotPairType.SOV_MOC,
   SpotPairType.SOV_DOC,
+  SpotPairType.SOV_BPRO,
   SpotPairType.RBTC_USDT,
   SpotPairType.RBTC_DOC,
   SpotPairType.USDT_DOC,
-  // SpotPairType.MOC_RBTC,
+  SpotPairType.MOC_RBTC,
+  SpotPairType.ETH_RBTC,
 ];
 
 export interface SpotTradingPageState {
