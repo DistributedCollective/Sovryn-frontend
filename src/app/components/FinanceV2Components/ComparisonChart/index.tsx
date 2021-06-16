d339a52b14e05a7c7cac390a69f2b336a2aba5b0
Soul Bit <its.soulBit@gmail.com>
2021-06-16 11:58:13 +0100

    [Yield Farm] Add history charts for pool AER

--- src/app/components/FinanceV2Components/ComparisonChart/index.tsx
+++ src/app/components/FinanceV2Components/ComparisonChart/index.tsx
@@ -36,7 +36,7 @@ const formatValues = value => {
 export default function ComparisonChart(props: ComparisonProps) {
   const options: ChartProps = {
     height: 150,
-    margin: [30, 45, 30, 40],
+    margin: [30, 45, 30, 45],
     labelColor: '#EDEDED',
     yAxisProps: {
       suffix: '%',
--- src/app/pages/LiquidityMining/components/MiningPool/PoolChart.tsx
+++ src/app/pages/LiquidityMining/components/MiningPool/PoolChart.tsx
@@ -1,312 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
+import { translations } from 'locales/i18n';
+import { currentNetwork } from 'utils/classifiers';
 import type { LiquidityPool } from 'utils/models/liquidity-pool';
 import ComparisonChart from 'app/components/FinanceV2Components/ComparisonChart';
 import { getAssetColor } from 'app/components/FinanceV2Components/utils/getAssetColor';
 
+type ChartDatum = [number, number]; //[unix timestamp, value]
+type ChartData = ChartDatum[];
+
+export type PoolData = {
+  pool_token: string;
+  activity_date: string;
+  APY_fees_pc: number;
+  APY_rewards_pc: number;
+  APY_pc: number;
+};
+
+export type PoolBalanceData = {
+  activity_date: string;
+  balance_btc: number;
+  pool: string;
+};
+
+export type AmmHistory = {
+  pool: string;
+  data: {
+    [key: string]: PoolData[];
+  };
+  balanceHistory: PoolBalanceData[];
+};
+
 interface Props {
   pool: LiquidityPool;
+  history: AmmHistory;
 }
 
-/* Sample data */
-const testData: any[] = [
-  {
-    supply_apr: '4205380178',
-    borrow_apr: '745742146',
-    timestamp: '2021-05-08T00:00:02.000Z',
-  },
-  {
-    supply_apr: '4205380178',
-    borrow_apr: '745742146',
-    timestamp: '2021-05-08T06:00:01.000Z',
-  },
-  {
-    supply_apr: '4106673965',
-    borrow_apr: '711801811',
-    timestamp: '2021-05-08T12:00:01.000Z',
-  },
-  {
-    supply_apr: '4106673965',
-    borrow_apr: '711801811',
-    timestamp: '2021-05-08T18:00:01.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-09T00:00:01.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-09T06:00:03.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-09T12:00:02.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-09T18:00:02.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-10T00:00:01.000Z',
-  },
-  {
-    supply_apr: '3720327406',
-    borrow_apr: '673757789',
-    timestamp: '2021-05-10T06:00:03.000Z',
-  },
-  {
-    supply_apr: '3721264629',
-    borrow_apr: '675145982',
-    timestamp: '2021-05-10T12:00:01.000Z',
-  },
-  {
-    supply_apr: '3721264629',
-    borrow_apr: '675145982',
-    timestamp: '2021-05-10T18:00:01.000Z',
-  },
-  {
-    supply_apr: '3721264629',
-    borrow_apr: '675145982',
-    timestamp: '2021-05-11T00:00:01.000Z',
-  },
-  {
-    supply_apr: '3721264629',
-    borrow_apr: '675145982',
-    timestamp: '2021-05-11T06:00:01.000Z',
-  },
-  {
-    supply_apr: '3720300452',
-    borrow_apr: '673753270',
-    timestamp: '2021-05-11T12:00:01.000Z',
-  },
-  {
-    supply_apr: '3719013724',
-    borrow_apr: '672172106',
-    timestamp: '2021-05-11T18:00:01.000Z',
-  },
-  {
-    supply_apr: '3719013724',
-    borrow_apr: '672172106',
-    timestamp: '2021-05-12T00:00:01.000Z',
-  },
-  {
-    supply_apr: '3719013724',
-    borrow_apr: '672172106',
-    timestamp: '2021-05-12T06:00:02.000Z',
-  },
-  {
-    supply_apr: '3724113749',
-    borrow_apr: '679863408',
-    timestamp: '2021-05-12T12:00:00.000Z',
-  },
-  {
-    supply_apr: '3720520337',
-    borrow_apr: '674621006',
-    timestamp: '2021-05-12T18:00:05.000Z',
-  },
-  {
-    supply_apr: '3721304871',
-    borrow_apr: '675932431',
-    timestamp: '2021-05-13T00:00:03.000Z',
-  },
-  {
-    supply_apr: '3721019131',
-    borrow_apr: '675884368',
-    timestamp: '2021-05-13T06:00:02.000Z',
-  },
-  {
-    supply_apr: '3721019131',
-    borrow_apr: '675884368',
-    timestamp: '2021-05-13T12:00:03.000Z',
-  },
-  {
-    supply_apr: '3524504329',
-    borrow_apr: '655796008',
-    timestamp: '2021-05-13T18:00:01.000Z',
-  },
-  {
-    supply_apr: '3524504329',
-    borrow_apr: '655796008',
-    timestamp: '2021-05-14T00:00:04.000Z',
-  },
-  {
-    supply_apr: '3524504329',
-    borrow_apr: '655796008',
-    timestamp: '2021-05-14T06:00:01.000Z',
-  },
-  {
-    supply_apr: '3524504329',
-    borrow_apr: '655796008',
-    timestamp: '2021-05-14T12:00:02.000Z',
-  },
-  {
-    supply_apr: '3524504329',
-    borrow_apr: '655796008',
-    timestamp: '2021-05-14T18:00:01.000Z',
-  },
-];
-
-const testTotal: any[] = [
-  {
-    total: '136.2222',
-    timestamp: '2021-05-08T00:00:02.000Z',
-  },
-  {
-    total: '146.1727',
-    timestamp: '2021-05-08T06:00:01.000Z',
-  },
-  {
-    total: '147.1727',
-    timestamp: '2021-05-08T12:00:01.000Z',
-  },
-  {
-    total: '143.1727',
-    timestamp: '2021-05-08T18:00:01.000Z',
-  },
-  {
-    total: '145.18',
-    timestamp: '2021-05-09T00:00:01.000Z',
-  },
-  {
-    total: '146.2222',
-    timestamp: '2021-05-09T06:00:03.000Z',
-  },
-  {
-    total: '148.5',
-    timestamp: '2021-05-09T12:00:02.000Z',
-  },
-  {
-    total: '149.78',
-    timestamp: '2021-05-09T18:00:02.000Z',
-  },
-  {
-    total: '152.555555',
-    timestamp: '2021-05-10T00:00:01.000Z',
-  },
-  {
-    total: '151.1727',
-    timestamp: '2021-05-10T06:00:03.000Z',
-  },
-  {
-    total: '153.1766',
-    timestamp: '2021-05-10T12:00:01.000Z',
-  },
-  {
-    total: '154.44',
-    timestamp: '2021-05-10T18:00:01.000Z',
-  },
-  {
-    total: '154.7',
-    timestamp: '2021-05-11T00:00:01.000Z',
-  },
-  {
-    total: '155.8888',
-    timestamp: '2021-05-11T06:00:01.000Z',
-  },
-  {
-    total: '163.1727',
-    timestamp: '2021-05-11T12:00:01.000Z',
-  },
-  {
-    total: '165.1727',
-    timestamp: '2021-05-11T18:00:01.000Z',
-  },
-  {
-    total: '166.1727',
-    timestamp: '2021-05-12T00:00:01.000Z',
-  },
-  {
-    total: '166.1727',
-    timestamp: '2021-05-12T06:00:02.000Z',
-  },
-  {
-    total: '166.1727',
-    timestamp: '2021-05-12T12:00:00.000Z',
-  },
-  {
-    total: '168.1727',
-    timestamp: '2021-05-12T18:00:05.000Z',
-  },
-  {
-    total: '167.1727',
-    timestamp: '2021-05-13T00:00:03.000Z',
-  },
-  {
-    total: '166.1727',
-    timestamp: '2021-05-13T06:00:02.000Z',
-  },
-  {
-    total: '167.727',
-    timestamp: '2021-05-13T12:00:03.000Z',
-  },
-  {
-    total: '168.1727',
-    timestamp: '2021-05-13T18:00:01.000Z',
-  },
-  {
-    total: '167.1727',
-    timestamp: '2021-05-14T00:00:04.000Z',
-  },
-  {
-    total: '167.666',
-    timestamp: '2021-05-14T06:00:01.000Z',
-  },
-  {
-    total: '169.1727',
-    timestamp: '2021-05-14T12:00:02.000Z',
-  },
-  {
-    total: '170.1727',
-    timestamp: '2021-05-14T18:00:01.000Z',
-  },
-];
-
-const btcApr: [number, number][] = testData.map(i => [
-  Date.parse(i.timestamp),
-  i.supply_apr / 1e8,
-]);
-const poolApr: [number, number][] = testData.map(i => [
-  Date.parse(i.timestamp),
-  i.borrow_apr / 1e8,
-]);
-const totalLiq: [number, number][] = testTotal.map(i => [
-  Date.parse(i.timestamp),
-  parseFloat(i.total),
-]);
-/* End sample data */
-
 export function PoolChart(props: Props) {
+  const { t } = useTranslation();
+  const [btcHistory, setBtcHistory] = useState<ChartData>([]);
+  const [assetHistory, setAssetHistory] = useState<ChartData>([]);
+  const [totalHistory, setTotalHistory] = useState<ChartData>([]);
+
+  useEffect(() => {
+    if (
+      props.history &&
+      props.history.data &&
+      props.history.balanceHistory &&
+      props.pool
+    ) {
+      const assetAddress = props.pool.supplyAssets[0].poolTokens[
+        currentNetwork
+      ].toLowerCase();
+      const btcAddress = props.pool.supplyAssets[1].poolTokens[
+        currentNetwork
+      ].toLowerCase();
+
+      const asset: ChartData = props.history.data[assetAddress]?.map(i => [
+        Date.parse(i.activity_date),
+        i.APY_pc,
+      ]);
+      setAssetHistory(asset);
+
+      //only use btc data for v2 pools, as v1 pools are 50/50 and have same AER for both sides
+      if (btcAddress !== assetAddress && props.history.data[btcAddress]) {
+        const btc: ChartData = props.history.data[btcAddress].map(i => [
+          Date.parse(i.activity_date),
+          i.APY_pc,
+        ]);
+        setBtcHistory(btc);
+      }
+      const total: ChartData = props.history.balanceHistory?.map(i => [
+        Date.parse(i.activity_date),
+        i.balance_btc,
+      ]);
+      setTotalHistory(total);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.history]);
+
   return (
     <>
-      <ComparisonChart
-        primaryData={{
-          name: 'rBTC AER',
-          color: '#FFAC3E',
-          data: btcApr,
-          numDecimals: 2,
-          suffix: '%',
-        }}
-        secondaryData={{
-          name: `${props.pool.poolAsset} AER`,
-          color: getAssetColor(props.pool.poolAsset),
-          data: poolApr,
-          numDecimals: 2,
-          suffix: '%',
-        }}
-        totalData={{
-          name: 'Total Liquidity',
-          color: '#ACACAC',
-          data: totalLiq,
-          numDecimals: 2,
-          suffix: '₿',
-        }}
-      />
+      {assetHistory &&
+        assetHistory.length > 0 &&
+        totalHistory &&
+        totalHistory.length > 0 && (
+          <ComparisonChart
+            primaryData={{
+              name: `${
+                btcHistory && btcHistory.length > 0
+                  ? props.pool.poolAsset
+                  : t(translations.liquidity.pool)
+              } ${t(translations.liquidity.aer)}`,
+              color: getAssetColor(props.pool.poolAsset),
+              data: assetHistory,
+              numDecimals: 2,
+              suffix: '%',
+            }}
+            secondaryData={
+              btcHistory && btcHistory.length > 0
+                ? {
+                    name: `rBTC ${t(translations.liquidity.aer)}`,
+                    color: '#FFAC3E',
+                    data: btcHistory,
+                    numDecimals: 2,
+                    suffix: '%',
+                  }
+                : undefined
+            }
+            totalData={{
+              name: t(translations.liquidity.totalLiquidity),
+              color: '#ACACAC',
+              data: totalHistory,
+              numDecimals: 2,
+              suffix: '₿',
+            }}
+          />
+        )}
     </>
   );
 }
