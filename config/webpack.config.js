bca87a412c26fc0e71d0ed1612c03f7c4a4fb605
ShynRou <dustin@commit.international>
2021-07-13 13:49:35 +0200

    [Webpack] fix duplicate react version when linking packages

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -301,6 +301,9 @@ module.exports = function (webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
+        // Ensure singular react version in the bundle
+        // Fixes yarn link issue with @sovryn/react-wallet
+        react: path.resolve(__dirname, '../node_modules', 'react'),
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
