cce3fcd6161f289db14cf7f88e54dbaa354b547d
Soul Bit <its.soulBit@gmail.com>
2021-07-18 20:55:44 +0100

    [Lend Page] XUSD profit display fix

--- src/app/pages/LendingPage/components/CurrencyContainer/UserLendingInfo.tsx
+++ src/app/pages/LendingPage/components/CurrencyContainer/UserLendingInfo.tsx
@@ -11,6 +11,9 @@ import { LendingPool } from 'utils/models/lending-pool';
 import { NextSupplyInterestRate } from 'app/components/NextSupplyInterestRate';
 import { useLending_profitOf } from 'app/hooks/lending/useLending_profitOf';
 import { useLending_assetBalanceOf } from 'app/hooks/lending/useLending_assetBalanceOf';
+import { useLending_balanceOf } from 'app/hooks/lending/useLending_balanceOf';
+import { useLending_checkpointPrice } from 'app/hooks/lending/useLending_checkpointPrice';
+import { useLending_tokenPrice } from 'app/hooks/lending/useLending_tokenPrice';
 import { bignumber } from 'mathjs';
 import { weiToFixed } from 'utils/blockchain/math-helpers';
 import { useAccount } from 'app/hooks/useAccount';
@@ -47,10 +50,42 @@ export const UserLendingInfo: React.FC<IUserLendingInfoProps> = ({
     account,
   );
 
+  const {
+    value: balanceOfCall,
+    loading: balanceOfLoading,
+  } = useLending_balanceOf(asset, account);
+
+  const {
+    value: checkpointPrice,
+    loading: checkpointLoading,
+  } = useLending_checkpointPrice(asset, account);
+
+  const {
+    value: tokenPrice,
+    loading: tokenPriceLoading,
+  } = useLending_tokenPrice(asset);
+
   const balance = useMemo(() => {
     return bignumber(balanceCall).minus(profitCall).toString();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [balanceCall, profitCall, asset]);
+
+  const totalProfit = useMemo(() => {
+    return bignumber(tokenPrice)
+      .sub(checkpointPrice)
+      .mul(balanceOfCall)
+      .div(10e18)
+      .add(profitCall)
+      .toString();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    balanceCall,
+    profitCall,
+    balanceOfCall,
+    checkpointPrice,
+    tokenPrice,
+    asset,
+  ]);
 
   useEffect(() => {
     if (balance !== '0') {
@@ -95,25 +130,31 @@ export const UserLendingInfo: React.FC<IUserLendingInfoProps> = ({
             })}
           </td>
         )}
-        {(balance !== '0' || pLoading || bLoading) && (
+        {(balance !== '0' || pLoading || bLoading || balanceOfLoading) && (
           <>
             <TableBodyData>
               <LoadableValue
                 loading={pLoading || bLoading}
                 value={
                   <>
-                    {weiToFixed(balance, 4)} <AssetRenderer asset={asset} />
+                    {weiToFixed(balance, 8)} <AssetRenderer asset={asset} />
                   </>
                 }
               />
             </TableBodyData>
             <TableBodyData>
               <LoadableValue
-                loading={pLoading}
+                loading={
+                  pLoading ||
+                  bLoading ||
+                  balanceOfLoading ||
+                  checkpointLoading ||
+                  tokenPriceLoading
+                }
                 value={
                   <ProfitLossRenderer
-                    isProfit={bignumber(profitCall).greaterThanOrEqualTo(0)}
-                    amount={weiToFixed(profitCall, 8)}
+                    isProfit={bignumber(totalProfit).greaterThanOrEqualTo(0)}
+                    amount={weiToFixed(totalProfit, 8)}
                     asset={asset}
                   />
                 }
