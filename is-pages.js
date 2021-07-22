67466bc0200e2f9259f8c4bc94891c0d2fb05bc2
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-07-22 09:57:51 +0300

    Reverts env.

--- is-pages.js
+++ is-pages.js
@@ -1,4 +1,3 @@
 // Pages always has `CF_PAGES` defined
-console.log('Check if Cloudflare Pages', process.env);
 if (!process.env.CF_PAGES) process.exit(1);
 console.log('Building using Cloudflare Pages');
