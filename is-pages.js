d577f57e813fdb523cd29a6ceba5a67960bac585
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-07-22 09:26:05 +0300

    working on cf pages deployer

--- is-pages.js
+++ is-pages.js
@@ -1,3 +1,4 @@
 // Pages always has `CF_PAGES` defined
+console.log('Check if Cloudflare Pages', process.env);
 if (!process.env.CF_PAGES) process.exit(1);
-console.log('Building using Cloudflare Pages', process.env);
+console.log('Building using Cloudflare Pages');
