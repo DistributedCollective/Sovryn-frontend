200607b544a6c659b04fb949081b00940fee3af3
soulBit <its.soulBit@gmail.com>
2021-07-23 12:51:26 +0100

    Update Loot Drop recalibration dates (#1542)

--- src/app/pages/LendingPage/index.tsx
+++ src/app/pages/LendingPage/index.tsx
@@ -35,7 +35,7 @@ const LendingPage: React.FC = () => {
             title="15K SOV"
             asset1={Asset.XUSD}
             message={t(translations.liquidityMining.recalibration, {
-              date: 'July 26',
+              date: 'August 2',
             })}
             linkUrl="https://www.sovryn.app/blog/sov-is-diving-into-the-lending-pools"
             linkText={t(translations.liquidityMining.lootDropLink)}
