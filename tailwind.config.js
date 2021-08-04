469dcb2a53a179b6512a818556d8c7dee0225201
ShynRou <dustin@commit.international>
2021-08-04 11:36:49 +0200

    [Styling] fix gloab styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -187,11 +187,8 @@ module.exports = {
     function ({ addComponents }) {
       addComponents({
         '.container': {
-          width: '100%',
           paddingRight: '1rem',
           paddingLeft: '1rem',
-          marginRight: 'auto',
-          marginLeft: 'auto',
           maxWidth: '100%',
           '@screen sm': {
             maxWidth: '540px',
