857c7d0676de16b97ce700a20112ce519aed06c3
boltsr <skrullweb.dev@gmail.com>
2021-08-10 22:18:46 +1000

    fix info block style

--- src/app/pages/RewardPage/components/RewardForm/index.tsx
+++ src/app/pages/RewardPage/components/RewardForm/index.tsx
@@ -64,23 +64,23 @@ export function RewardForm() {
       </Box>
       <RewardInfoBox>
         <div className="xl:tw-mx-1 tw-w-1/3 tw-bg-gray-800 tw-staking-box tw-p-4 tw-rounded-lg tw-text-sm tw-mb-4 lg:tw-mb-0">
-          <div className="tw-text-xl tw-font-semibold tw-mb-9">
+          <InfoTitle>
             {t(translations.rewardPage.topData.referralRewards)}
-          </div>
+          </InfoTitle>
           <div className="tw-flex tw-items-start">
             <div className="tw-p-2 tw-bg-gold tw-mr-5"></div>
             <div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
+                <InfoSubAvaTitle>
                   {t(translations.rewardPage.topData.availableRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">15.2976 SOV</p>
+                </InfoSubAvaTitle>
+                <InfoContent>15.2976 SOV</InfoContent>
               </div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
+                <InfoSubTotalTitle>
                   {t(translations.rewardPage.topData.totalRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">73.5927 SOV</p>
+                </InfoSubTotalTitle>
+                <InfoSubTotalContent>73.5927 SOV</InfoSubTotalContent>
               </div>
             </div>
           </div>
@@ -93,16 +93,16 @@ export function RewardForm() {
             <div className="tw-p-2 tw-bg-white tw-mr-5"></div>
             <div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
-                  {t(translations.rewardPage.topData.liquidityRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">15.2976 SOV</p>
+                <InfoSubAvaTitle>
+                  {t(translations.rewardPage.topData.availableRewards)}
+                </InfoSubAvaTitle>
+                <InfoContent>15.2976 SOV</InfoContent>
               </div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
+                <InfoSubTotalTitle>
                   {t(translations.rewardPage.topData.totalRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">73.5927 SOV</p>
+                </InfoSubTotalTitle>
+                <InfoSubTotalContent>73.5927 SOV</InfoSubTotalContent>
               </div>
             </div>
           </div>
@@ -115,16 +115,16 @@ export function RewardForm() {
             <div className="tw-p-2 tw-bg-green tw-mr-5"></div>
             <div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
+                <InfoSubAvaTitle>
                   {t(translations.rewardPage.topData.availableRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">15.2976 SOV</p>
+                </InfoSubAvaTitle>
+                <InfoContent>15.2976 SOV</InfoContent>
               </div>
               <div className="mb-3">
-                <p className="tw-text-xs mb-1">
+                <InfoSubTotalTitle>
                   {t(translations.rewardPage.topData.totalRewards)}
-                </p>
-                <p className="tw-text-xl tw-font-semibold">73.5927 SOV</p>
+                </InfoSubTotalTitle>
+                <InfoSubTotalContent>73.5927 SOV</InfoSubTotalContent>
               </div>
             </div>
           </div>
@@ -149,6 +149,35 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
 `;
+const InfoTitle = styled.div`
+  color: '#E9EAE9';
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 2.25rem;
+`;
+const InfoContent = styled.div`
+  color: '#E9EAE9';
+  font-size: 20px;
+  font-weight: 600;
+`;
+const InfoSubAvaTitle = styled.div`
+  color: '#E9EAE9';
+  font-size: 12px;
+  font-weight: 500;
+  margin-bottom: 0.25rem;
+`;
+const InfoSubTotalTitle = styled.div`
+  color: 'rgba(237,237,237,0.65)';
+  font-size: 10px;
+  font-weight: 500;
+  margin-bottom: 0.25rem;
+`;
+const InfoSubTotalContent = styled.div`
+  color: 'rgba(237,237,237,0.65)';
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 0.25rem;
+`;
 const RewardInfoBox = styled.div`
   /* background-color: #222222; */
   width: 100%;
@@ -157,6 +186,7 @@ const RewardInfoBox = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 2rem;
+  font-family: 'Montserrat';
 `;
 const Divider = styled.div`
   width: 0px;
