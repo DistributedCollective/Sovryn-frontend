4ecda14d14e0715403b788fd37b196ef3f1c45b9
ShynRou <dustin@commit.international>
2021-08-16 12:43:42 +0200

    [Styling] remove and replace css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,8 +39,6 @@ module.exports = {
       '4xl': '2.25rem',
       '4-5xl': '2.5rem',
       '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '5rem',
     },
     colors: {
       transparent: 'transparent',
