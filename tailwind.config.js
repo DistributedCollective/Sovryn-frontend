4c7f9d56bf645f50a42519f9c9cdb7850fe253b4
ShynRou <dustin@commit.international>
2021-08-19 16:14:48 +0200

    [Styling] change container widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -172,19 +172,7 @@ module.exports = {
           marginRight: 'auto',
           marginLeft: 'auto',
           maxWidth: '100%',
-          '@screen sm': {
-            maxWidth: '540px',
-          },
-          '@screen md': {
-            maxWidth: '720px',
-          },
-          '@screen lg': {
-            maxWidth: '960px',
-          },
           '@screen xl': {
-            maxWidth: '1920px',
-          },
-          '@screen 2xl': {
             maxWidth: '1920px',
           },
         },
