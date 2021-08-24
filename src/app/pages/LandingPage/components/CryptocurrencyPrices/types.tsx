55e9b7d206786d2028a8883d0ba76b12ee75243d
Rick <81699526+rick23p@users.noreply.github.com>
2021-08-24 20:31:26 +0430

    Feat/market cap landing (#1640)
    * add landingPage container.
    
    * Add Welcome section
    
    * Add Total value locked section
    
    * Promotions section - initial commit (PromotionsCarousel)
    
    * add landing page ArbitrageOpportunity.
    
    * add LendingAssets to LandingPage.
    
    * PromotionsCard initial version
    
    * add top amm pools.
    
    * fix landing ui issue.
    
    * Promotions - PromotionCard adjustments
    
    * Promotions section adjustments
    
    * Initial rework of TradingVolume section
    
    * TradingVolume section and welcome title adjustments
    
    * fix arbitrage assets logo.
    
    * Implement promotion links to specific modals
    
    * update tokens icons.
    
    * fix Arbitrage list issue.
    
    * update isNullOrUndefined.
    
    * add isValidArbitrage function.
    
    * Promotion cards adjustments
    
    * fix isNullOrUndefined null check.
    
    * add validation to ArbitrageOpportunity data.
    
    * Promotion cards - asset logos
    
    * Fix margin trade promotion links
    
    * update assets.
    
    * update lending assets design.
    
    * update amm balance table design.
    
    * add new landing footer.
    
    * update lock.
    
    * update lock.
    
    * finish TVL and Lend borrow table.
    
    * improve landing page.
    
    * disable stats page.
    
    * fix responsive issues.
    
    * improve landing page responsive and styles.
    
    * add landing banner origin fish sale.
    
    * finish landing page banner.
    
    * fix landing page issues.
    
    * add apy to amm pools.
    
    * fix amm pool apy.
    
    * fix apy issue in amm balance.
    
    * fix ArbitrageOpportunity image.
    
    * fix linkAsset issue in promotions.
    
    * update origin fish banner title.
    
    * add CryptocurrencyPrices component.
    
    * fix TradingVolume data.
    
    * fix landing page responsive issues.
    
    * fix PR comments.
    
    * fix landing page promotion overflow.
    
    * fix promotion center.
    
    * [Landing] Update copy and links for promos
    
    * cross chain responsive design improvements.
    
    * add Market Cap to landing page.
    
    * add market cap and supply to CryptocurrencyPrices table
    
    * update Market Cap Tooltip text.
    
    * chore: fix spelling on market cap "tooltip"
    
    Co-authored-by: tiltom <tilnak.tomas@gmail.com>
    Co-authored-by: Soul Bit <its.soulBit@gmail.com>

--- src/app/pages/LandingPage/components/CryptocurrencyPrices/types.tsx
+++ src/app/pages/LandingPage/components/CryptocurrencyPrices/types.tsx
@@ -15,7 +15,6 @@ export interface IPairData {
   price_change_week: number;
   price_change_week_usd: number;
 }
-
 export interface IPairs {
   [key: string]: IPairData;
 }
@@ -26,3 +25,17 @@ export type IPairsData = {
   total_volume_usd: number;
   updated_at: string;
 };
+
+export interface IAssetData {
+  symbol: string;
+  name: string;
+  id: string;
+  trading_fee: number;
+  unified_cryptoasset_id: string;
+  circulating_supply: number;
+  updated: string;
+}
+
+export interface IAssets {
+  [key: string]: IAssetData;
+}
