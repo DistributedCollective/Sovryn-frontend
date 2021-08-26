ef168eb5e984997cbf5cd12858ffcf2eba1d0eb7
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-08-26 12:59:19 +0300

    fix: bring back saking rewards proxy contract address (testnet)

--- src/utils/blockchain/contracts.testnet.ts
+++ src/utils/blockchain/contracts.testnet.ts
@@ -280,8 +280,7 @@ export const contracts = {
     abi: feeSharingProxyAbi,
   },
   stakingRewards: {
-    address: '0x4fb9BCd9e83BA2E52C091D5082Fc5D4A3B4a825E',
-    //address: '0x18eF0ff12f1b4D30104B4680D485D026C26D164D', // TODO: bring it back after testing is done
+    address: '0x18eF0ff12f1b4D30104B4680D485D026C26D164D',
     abi: stakingRewardsProxyAbi,
   },
   FISH_token: {
