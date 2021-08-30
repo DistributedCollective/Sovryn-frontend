de767cad6f928f393570ef71c361d19a5a15b370
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-08-30 14:33:07 +0300

    feat: bypass maintenance mode checks while on staging env, update gas price for each block

--- src/utils/blockchain/gas-price.ts
+++ src/utils/blockchain/gas-price.ts
@@ -2,9 +2,11 @@ import { toWei } from 'web3-utils';
 
 class GasPrice {
   private gasPrice: string = toWei('0.066', 'gwei');
-  public set(amountInGwei: string): void {
-    this.gasPrice = amountInGwei;
+
+  public set(weiAmount: string): void {
+    this.gasPrice = weiAmount;
   }
+
   public get(): string {
     return this.gasPrice;
   }
