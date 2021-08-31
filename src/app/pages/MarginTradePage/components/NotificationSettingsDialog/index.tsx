a358233bed27bcbb8f6bba1d73cae19cb4fcd15f
tiltom <tilnak.tomas@gmail.com>
2021-08-31 12:29:30 +0200

    Dialog styles adjustments

--- src/app/pages/MarginTradePage/components/NotificationSettingsDialog/index.tsx
+++ src/app/pages/MarginTradePage/components/NotificationSettingsDialog/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Dialog } from 'app/containers/Dialog/Loadable';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../../../locales/i18n';
@@ -6,6 +6,8 @@ import { Switch } from '@blueprintjs/core/lib/esm/components/forms/controls';
 import { Input } from 'app/components/Form/Input';
 import { FormGroup } from 'app/components/Form/FormGroup';
 import { DialogButton } from 'app/components/Form/DialogButton';
+import imgTelegram from 'assets/images/marginTrade/Telegram_logo.svg';
+import imgDiscord from 'assets/images/marginTrade/Discord-Logo-Color.svg';
 
 interface INotificationSettingsDialogProps {
   isOpen: boolean;
@@ -23,6 +25,16 @@ export const NotificationSettingsDialog: React.FC<INotificationSettingsDialogPro
   const [isTelegramActive, setIsTelegramActive] = useState(false);
   const [isDiscordActive, setIsDiscordActive] = useState(false);
 
+  const onChangeTelegramSwitch = useCallback(
+    () => setIsTelegramActive(prevValue => !prevValue),
+    [],
+  );
+
+  const onChangeDiscordSwitch = useCallback(
+    () => setIsDiscordActive(prevValue => !prevValue),
+    [],
+  );
+
   return (
     <Dialog isOpen={isOpen} onClose={onClose}>
       <div className="tw-mw-340 tw-mx-auto">
@@ -36,12 +48,20 @@ export const NotificationSettingsDialog: React.FC<INotificationSettingsDialogPro
         </div>
 
         <div className="tw-mb-10">
-          <div className="tw-flex tw-justify-between tw-mb-1.5">
-            <div>Telegram</div>
+          <div className="tw-flex tw-justify-between tw-mb-4">
+            <div className="tw-flex tw-items-center">
+              <img
+                src={imgTelegram}
+                alt="Telegram logo"
+                className="tw-mr-1.5"
+              />
+              Telegram
+            </div>
             <Switch
               checked={isTelegramActive}
-              onChange={() => setIsTelegramActive(prevValue => !prevValue)}
+              onChange={onChangeTelegramSwitch}
               large
+              className="tw-mb-0"
             />
           </div>
 
@@ -71,12 +91,16 @@ export const NotificationSettingsDialog: React.FC<INotificationSettingsDialogPro
         </div>
 
         <div className="tw-mb-6">
-          <div className="tw-flex tw-justify-between tw-mb-1.5">
-            <div>Discord</div>
+          <div className="tw-flex tw-justify-between tw-mb-4">
+            <div className="tw-flex tw-items-center">
+              <img src={imgDiscord} alt="Discord logo" className="tw-mr-1.5" />
+              Discord
+            </div>
             <Switch
               checked={isDiscordActive}
-              onChange={() => setIsDiscordActive(prevValue => !prevValue)}
+              onChange={onChangeDiscordSwitch}
               large
+              className="tw-mb-0"
             />
           </div>
 
@@ -139,7 +163,6 @@ export const NotificationSettingsDialog: React.FC<INotificationSettingsDialogPro
               `Telegram handle: ${telegramUsername} , Discord handle: ${discordUsername}`,
             )
           }
-          disabled={!telegramUsername && !discordUsername}
           className="tw-rounded-lg tw-mt-10"
         />
       </div>
--- src/app/pages/MarginTradePage/index.tsx
+++ src/app/pages/MarginTradePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet-async';
@@ -23,6 +23,7 @@ import { useHistory, useLocation } from 'react-router-dom';
 import { IPromotionLinkState } from '../LandingPage/components/Promotions/components/PromotionCard/types';
 import styles from './index.module.scss';
 import { NotificationSettingsDialog } from './components/NotificationSettingsDialog';
+import imgNotificationBell from 'assets/images/marginTrade/notifications.svg';
 
 export function MarginTradePage() {
   useInjectReducer({ key: sliceKey, reducer: reducer });
@@ -56,6 +57,11 @@ export function MarginTradePage() {
 
   const connected = useIsConnected();
 
+  const onNotificationSettingsClick = useCallback(
+    () => setShowNotificationSettingsModal(true),
+    [],
+  );
+
   return (
     <>
       <Helmet>
@@ -69,8 +75,16 @@ export function MarginTradePage() {
       <div className={styles.overview}>
         <div>Overview</div>
         <div>
-          <button onClick={() => setShowNotificationSettingsModal(true)}>
-            Enable notifications
+          <button
+            onClick={onNotificationSettingsClick}
+            className="tw-text-sm tw-text-primary tw-tracking-normal tw-flex tw-items-center"
+          >
+            <img
+              src={imgNotificationBell}
+              alt="Notification bell"
+              className="tw-mr-1.5"
+            />{' '}
+            {t(translations.marginTradePage.notificationsButton.enable)}
           </button>
         </div>
       </div>
