0f171ffe8e47c2afe13cc83626e96365cbc1028c
ShynRou <dustin@commit.international>
2021-09-01 13:43:30 +0200

    [Storybook] add webpack.config for storybook and import app styles

--- src/app/components/Button/index.stories.tsx
+++ src/app/components/Button/index.stories.tsx
@@ -6,6 +6,12 @@ export const Secondary = () => (
   <Button text="Button" loading disabled onClick={() => {}} />
 );
 
+export const TailwindTest = () => (
+  <div className="tw-h-16 tw-w-1/2 tw-bg-green tw-text-black tw-skeleton">
+    TAILWIND DOES <span className="tw-opacity-0">NOT</span> WORK
+  </div>
+);
+
 export default {
   title: 'Form Elements',
 };
