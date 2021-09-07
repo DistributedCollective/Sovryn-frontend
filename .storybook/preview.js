bbb9ae889804c8bee6600151cd689f981335f7bc
Dustin Hagemeier <dustin@commit.international>
2021-09-07 16:11:04 +0200

    [Storybook] tailwind for storybook (#1678)
    * [Storybook] add webpack.config for storybook and import app styles
    
    * [Storybook] add descriptive comment
    
    * [Storybook] remove console.log
    
    * [Storybook] add router wrapper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import '../src/styles/sass/styles.scss';
+import '../src/styles/tailwindcss/index.css';
+import '@sovryn/react-wallet/index.css';
+
+export const decorators = [
+  Story => (
+    <MemoryRouter>
+      <Story />
+    </MemoryRouter>
+  ),
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
