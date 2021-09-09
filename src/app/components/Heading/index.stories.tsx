04cbdf716075c799acc46b02c35167b3d3bebfc2
ShynRou <dustin@commit.international>
2021-09-09 11:28:20 +0200

    [Heading] add all in one story

--- src/app/components/Heading/index.stories.tsx
+++ src/app/components/Heading/index.stories.tsx
@@ -1,12 +1,24 @@
 import React, { ComponentProps } from 'react';
 import { Story } from '@storybook/react';
 
-import { H1, H2, H3, H4, H5, H6, Label as LabelHeading } from './index';
+import { H1, H2, H3, H4, H5, H6, Label } from './index';
 
 export default {
   title: 'Atoms/Headings',
   component: H1,
 };
+
+export const All = () => (
+  <>
+    <H1>Heading 1</H1>
+    <H2>Heading 2</H2>
+    <H3>Heading 3</H3>
+    <H4>Heading 4</H4>
+    <H5>Heading 5</H5>
+    <H6>Heading 6</H6>
+    <Label>Label</Label>
+  </>
+);
 
 const TemplateH1: Story<ComponentProps<typeof H1>> = args => <H1 {...args} />;
 export const Heading1 = TemplateH1.bind({});
@@ -44,10 +56,10 @@ Heading6.args = {
   children: 'Heading 6',
 };
 
-const TemplateLabel: Story<ComponentProps<typeof LabelHeading>> = args => (
-  <LabelHeading {...args} />
+const TemplateLabel: Story<ComponentProps<typeof Label>> = args => (
+  <Label {...args} />
 );
-export const Label = TemplateLabel.bind({});
-Label.args = {
+export const _Label = TemplateLabel.bind({});
+_Label.args = {
   children: 'Label',
 };
