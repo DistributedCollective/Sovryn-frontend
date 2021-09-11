c7b4b1a8d7cf87f311a920aa47a1c8ab2d247018
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2021-09-11 09:26:43 +0300

    Feat/1342 swaps upgrade (#1709)
    * Uses swaps external smart contract for swaps.
    
    * Tests for price diff when opening new position
    
    * removed unused imports
    
    * swap network changed by swap protocol (swaps external)
    
    * Swaps fix
    
    * Set destTokenAmount to 0 for swaps
    
    * chore: remove deprecated convertsionPath hook
    
    * Merge remote-tracking branch 'origin/development' into feat/1342-swaps-upgrade
    
    # Conflicts:
    #	src/app/containers/SwapFormContainer/index.tsx
    #	src/app/hooks/swap-network/useSwapNetwork_resolveRate.ts
    #	src/app/pages/MarginTradePage/components/TradeDialog/index.tsx
    #	src/app/pages/SpotTradingPage/components/TradeForm/index.tsx
    #	src/utils/blockchain/contracts.ts
    #	src/utils/classifiers.ts
    
    * fix: marigin trading price
    
    Co-authored-by: tiltom <tilnak.tomas@gmail.com>

--- src/utils/providers/rate-provider.ts
+++ src/utils/providers/rate-provider.ts
@@ -1,44 +1,27 @@
-import { Asset } from '../../types/asset';
+import { bignumber } from 'mathjs';
+import { Asset } from '../../types';
 import { contractReader } from '../sovryn/contract-reader';
 import { getTokenContract } from '../blockchain/contract-helpers';
-import { bignumber } from 'mathjs';
-
-interface Paths {
-  [key: string]: string[];
-}
 
 export class RateProvider {
-  private cachedPaths: Paths = {};
-
-  public async getPath(source: Asset, target: Asset) {
-    const key = `${source}_${target}`;
-    if (
-      !this.cachedPaths.hasOwnProperty(key) ||
-      !this.cachedPaths[key].length
-    ) {
-      this.cachedPaths[key] = await contractReader.call(
-        'swapNetwork',
-        'conversionPath',
-        [getTokenContract(source).address, getTokenContract(target).address],
-      );
-    }
-    return this.cachedPaths[key];
-  }
-
-  public async priceByPath(path: string[], weiAmount: string) {
-    return await contractReader.call('swapNetwork', 'rateByPath', [
-      path,
-      weiAmount,
-    ]);
-  }
-
   public async getPrice(source: Asset, target: Asset, weiAmount: string) {
-    const path = await this.getPath(source, target);
-    const price = await this.priceByPath(path, weiAmount);
+    const price = await contractReader.call(
+      'sovrynProtocol',
+      'getSwapExpectedReturn',
+      [
+        getTokenContract(source).address,
+        getTokenContract(target).address,
+        weiAmount,
+      ],
+    );
     return bignumber(price as string).toFixed();
   }
 
-  public async getRate(source: Asset, target: Asset, weiAmount: string) {
+  public async getDisagreementRate(
+    source: Asset,
+    target: Asset,
+    weiAmount: string,
+  ) {
     const price = await this.getPrice(source, target, weiAmount);
     return await contractReader.call('priceFeed', 'checkPriceDisagreement', [
       getTokenContract(source).address,
