04d821c6e8deca3b1c95b092c4d53ec4ac549167
ShynRou <dustin@commit.international>
2021-09-16 11:32:32 +0200

    [Perpetual] style contract details

--- src/app/pages/PerpetualPage/components/ContractDetails/index.tsx
+++ src/app/pages/PerpetualPage/components/ContractDetails/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { translations } from '../../../../../locales/i18n';
 import { PerpetualPair } from '../../../../../utils/models/perpetual-pair';
 import { usePerpetual_ContractDetails } from '../../hooks/usePerpetual_ContractDetails';
+import {
+  numberToPercent,
+  toNumberFormat,
+} from '../../../../../utils/display-text/format';
 
 type IContractDetailsProps = {
   pair: PerpetualPair;
@@ -14,66 +19,73 @@ export const ContractDetails: React.FC<IContractDetailsProps> = ({ pair }) => {
 
   return (
     <div className="tw-w-full tw-bg-black tw-py-2">
-      <div className="tw-container tw-flex tw-flex-row tw-flex-wrap tw-justify-between tw-items-center ">
-        <h3 className="tw-mr-2.5 tw-text-sm tw-font-semibold tw-normal-case">
+      <div className="tw-container tw-flex tw-flex-col sm:tw-flex-row tw-flex-wrap tw-items-start sm:tw-items-center">
+        <h3 className="tw-mt-0.5 sm:tw-mr-5 tw-text-sm tw-font-semibold tw-normal-case">
           {t(translations.perpetualPage.contractDetails.title)}
         </h3>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs tw-font-medium">
-            {t(translations.perpetualPage.contractDetails.indexPrice)}
-          </span>
-          <span className="tw-text-sm tw-text-primary tw-font-semibold">
-            {data?.indexPrice?.toFixed(2)}
-          </span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.volume24h)}
-          </span>
-          <span className="tw-text-sm tw-font-medium">{data?.volume24h}</span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.openInterest)}
-          </span>
-          <span className="tw-text-sm tw-font-medium">
-            {data?.openInterest}
-          </span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.fundingRate)}
-          </span>
-          <span className="tw-text-sm tw-text-trade-short tw-font-medium">
-            {data?.fundingRate4h && (data.fundingRate4h * 100).toFixed(4) + '%'}
-          </span>{' '}
-          <span className="tw-text-sm tw-font-medium">
-            {t(translations.perpetualPage.contractDetails.fundingRate4hr)}
-          </span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.contractValue)}
-          </span>
-          <span className="tw-text-sm tw-font-medium">
-            {data?.contractValue}
-          </span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.lotSize)}
-          </span>
-          <span className="tw-text-sm tw-font-medium">{data?.lotSize}</span>
-        </div>
-        <div className="tw-mr-4">
-          <span className="tw-mr-2.5 tw-text-xs">
-            {t(translations.perpetualPage.contractDetails.minTradeAmount)}
-          </span>
-          <span className="tw-text-sm tw-font-medium">
-            {data?.minTradeAmount}
-          </span>
-        </div>
+        <ContractDetailEntry
+          titleClassName="tw-font-medium"
+          valueClassName="tw-text-primary tw-font-semibold"
+          title={t(translations.perpetualPage.contractDetails.indexPrice)}
+          value={data?.indexPrice && toNumberFormat(data.indexPrice, 2)}
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.volume24h)}
+          value={data?.volume24h && toNumberFormat(data.volume24h, 0)}
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.openInterest)}
+          value={data?.openInterest && toNumberFormat(data.openInterest, 0)}
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.fundingRate)}
+          value={
+            <>
+              <span className="tw-text-sm tw-text-trade-short tw-font-medium">
+                {data?.fundingRate4h && numberToPercent(data.fundingRate4h, 4)}
+              </span>{' '}
+              {t(translations.perpetualPage.contractDetails.fundingRate4hr)}
+            </>
+          }
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.contractValue)}
+          value={data?.contractValue && toNumberFormat(data.contractValue, 0)}
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.lotSize)}
+          value={data?.lotSize && toNumberFormat(data.lotSize, 0)}
+        />
+        <ContractDetailEntry
+          title={t(translations.perpetualPage.contractDetails.minTradeAmount)}
+          value={data?.minTradeAmount && toNumberFormat(data.minTradeAmount, 0)}
+        />
       </div>
     </div>
   );
 };
+
+type IContractDetailEntryProps = {
+  className?: string;
+  titleClassName?: string;
+  valueClassName?: string;
+  title: React.ReactNode;
+  value: React.ReactNode;
+};
+
+const ContractDetailEntry: React.FC<IContractDetailEntryProps> = ({
+  className,
+  titleClassName,
+  valueClassName,
+  title,
+  value,
+}) => (
+  <div className={classNames('sm:tw-mr-8', className)}>
+    <span className={classNames('tw-mr-2.5 tw-text-xs', titleClassName)}>
+      {title}
+    </span>
+    <span className={classNames('tw-text-sm tw-font-medium', valueClassName)}>
+      {value}
+    </span>
+  </div>
+);
