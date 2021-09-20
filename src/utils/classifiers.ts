4833e53ce6a517c54702f2d06d448ed68536ead9
Victor Creed <rytis.grincevicius+cv@gmail.com>
2021-09-20 14:52:32 +0300

    chore: increase gas limit for closing trading position

--- src/utils/classifiers.ts
+++ src/utils/classifiers.ts
@@ -50,7 +50,7 @@ export const chartStorageKey = 'sovryn.charts';
 
 export const gasLimit = {
   [TxType.TRADE]: 3750000,
-  [TxType.CLOSE_WITH_SWAP]: 1500000,
+  [TxType.CLOSE_WITH_SWAP]: 2300000,
   [TxType.ADD_LIQUIDITY]: 500000,
   [TxType.REMOVE_LIQUIDITY]: 650000,
   [TxType.BORROW]: 1500000,
