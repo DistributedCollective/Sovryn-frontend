83e123d648ea96997f892d2bdedc76ae6169360f
soulBit <its.soulBit@gmail.com>
2021-09-23 11:37:42 +0100

    chore: remove charting_library dependency

--- src/app/components/TradingChart/index.tsx
+++ src/app/components/TradingChart/index.tsx
@@ -6,17 +6,18 @@
  * and make any relevant changes before updating the library version:
  * https://github.com/tradingview/charting_library/wiki/Breaking-Changes
  */
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+// import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
-import {
-  widget,
-  IChartingLibraryWidget,
-} from '@distributedcollective/charting-library/src/charting_library/charting_library.min';
+// import {
+//   widget,
+//   IChartingLibraryWidget,
+// } from '@distributedcollective/charting-library/src/charting_library/charting_library.min';
 
 import { Skeleton } from '../PageSkeleton';
-import Datafeed from './datafeed';
-import Storage from './storage';
-import { noop } from '../../constants';
+// import Datafeed from './datafeed';
+// import Storage from './storage';
+// import { noop } from '../../constants';
 
 export enum Theme {
   LIGHT = 'Light',
@@ -29,77 +30,77 @@ export interface ChartContainerProps {
 }
 
 export function TradingChart(props: ChartContainerProps) {
-  const [hasCharts, setHasCharts] = useState<boolean>(false);
-  const [chart, setChart] = useState<IChartingLibraryWidget | null>(null);
+  const [hasCharts] = useState<boolean>(false);
+  // const [chart, setChart] = useState<IChartingLibraryWidget | null>(null);
 
-  useEffect(() => {
-    try {
-      // full list of widget config options here: https://github.com/tradingview/charting_library/wiki/Widget-Constructor/cf26598509d8bba6dc95c5fe8208caa5e8474827
-      const widgetOptions: any = {
-        debug: false,
-        symbol: props.symbol,
-        datafeed: Datafeed,
-        save_load_adapter: Storage,
-        study_count_limit: 15, //max number of indicators that can be added to charts
-        interval: '30', //default time interval
-        timeframe: '3D', //default range
-        container_id: 'tv_chart_container', //id of DOM container
-        library_path: '/charting_library/', //relative path of library in /public folder
-        locale: 'en',
-        load_last_chart: true, //last chart layout (if present)
-        enabled_features: [
-          'study_templates', //remove to disable storing of indicators
-          'side_toolbar_in_fullscreen_mode',
-        ], //full list of features here: https://github.com/tradingview/charting_library/wiki/Featuresets/c2ec34605fa84781048d32b87a2b08ef1466639a
-        disabled_features: [
-          'header_symbol_search',
-          //'header_saveload', //uncomment to disable storing of drawings
-          'header_compare',
-        ],
-        autosize: true,
-        // toolbar_bg: '#a3a3a3',
-        theme: props.theme,
-        time_frames: [
-          { text: '1d', resolution: '10', description: '1d', title: '1d' },
-          { text: '3d', resolution: '30', description: '3d', title: '3d' },
-          { text: '7d', resolution: '60', description: '7d', title: '7d' },
-          { text: '3m', resolution: '120', description: '3m', title: '3m' },
-          // { text: '6m', resolution: '240', description: '6m', title: '6m' },
-          // { text: '1y', resolution: '1W', description: '1y', title: '1y' },
-          { text: '5y', resolution: '1W', description: '5y', title: '5y' },
-        ],
-      };
+  // useEffect(() => {
+  //   try {
+  //     // full list of widget config options here: https://github.com/tradingview/charting_library/wiki/Widget-Constructor/cf26598509d8bba6dc95c5fe8208caa5e8474827
+  //     const widgetOptions: any = {
+  //       debug: false,
+  //       symbol: props.symbol,
+  //       datafeed: Datafeed,
+  //       save_load_adapter: Storage,
+  //       study_count_limit: 15, //max number of indicators that can be added to charts
+  //       interval: '30', //default time interval
+  //       timeframe: '3D', //default range
+  //       container_id: 'tv_chart_container', //id of DOM container
+  //       library_path: '/charting_library/', //relative path of library in /public folder
+  //       locale: 'en',
+  //       load_last_chart: true, //last chart layout (if present)
+  //       enabled_features: [
+  //         'study_templates', //remove to disable storing of indicators
+  //         'side_toolbar_in_fullscreen_mode',
+  //       ], //full list of features here: https://github.com/tradingview/charting_library/wiki/Featuresets/c2ec34605fa84781048d32b87a2b08ef1466639a
+  //       disabled_features: [
+  //         'header_symbol_search',
+  //         //'header_saveload', //uncomment to disable storing of drawings
+  //         'header_compare',
+  //       ],
+  //       autosize: true,
+  //       // toolbar_bg: '#a3a3a3',
+  //       theme: props.theme,
+  //       time_frames: [
+  //         { text: '1d', resolution: '10', description: '1d', title: '1d' },
+  //         { text: '3d', resolution: '30', description: '3d', title: '3d' },
+  //         { text: '7d', resolution: '60', description: '7d', title: '7d' },
+  //         { text: '3m', resolution: '120', description: '3m', title: '3m' },
+  //         // { text: '6m', resolution: '240', description: '6m', title: '6m' },
+  //         // { text: '1y', resolution: '1W', description: '1y', title: '1y' },
+  //         { text: '5y', resolution: '1W', description: '5y', title: '5y' },
+  //       ],
+  //     };
 
-      const myChart = new widget(widgetOptions);
-      setChart(myChart);
-      myChart.onChartReady(() => {
-        setHasCharts(true);
-      });
+  //     const myChart = new widget(widgetOptions);
+  //     setChart(myChart);
+  //     myChart.onChartReady(() => {
+  //       setHasCharts(true);
+  //     });
 
-      return () => {
-        myChart.remove();
-        setHasCharts(false);
-        setChart(null);
-      };
-    } catch (e) {
-      console.error(e);
-      setHasCharts(false);
-    }
+  //     return () => {
+  //       myChart.remove();
+  //       setHasCharts(false);
+  //       setChart(null);
+  //     };
+  //   } catch (e) {
+  //     console.error(e);
+  //     setHasCharts(false);
+  //   }
 
-    // run only once after mount
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  //   // run only once after mount
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  useLayoutEffect(() => {
-    if (chart && hasCharts) {
-      chart.chart().setSymbol(props.symbol, noop);
-    }
-  }, [chart, hasCharts, props.symbol]);
+  // useLayoutEffect(() => {
+  //   if (chart && hasCharts) {
+  //     chart.chart().setSymbol(props.symbol, noop);
+  //   }
+  // }, [chart, hasCharts, props.symbol]);
 
   return (
     <div
       className={cn(
-        'tw-w-full tw-h-full tw-flex tw-rounded tw-overflow-hidden',
+        'tw-w-full tw-h-full tw-flex tw-rounded tw-items-center tw-justify-center tw-overflow-hidden tw-rounded',
         hasCharts && 'tw-border',
       )}
       style={{ minWidth: 270, minHeight: 500 }}
@@ -111,7 +112,9 @@ export function TradingChart(props: ChartContainerProps) {
             'tv-chart-container tw-flex-grow',
             !hasCharts && 'tw-hidden',
           )}
-        />
+        >
+          TradingView Charts disabled for external contributors
+        </div>
         <div
           className={cn(
             'tw-w-full tw-h-full tw-content-end tw-gap-4',
