ad275ab2287460bd25b1865b901cb634bbced423
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2021-09-27 18:49:10 +0300

    Revert "hotfix: disable binance chain bridging (#1768)" (#1771)
    This reverts commit ed6cc17ed67c23f063ba980b8696c87f421f2276.

--- src/app/pages/BridgeDepositPage/dictionaries/bridges/mainnet.ts
+++ src/app/pages/BridgeDepositPage/dictionaries/bridges/mainnet.ts
@@ -2,30 +2,30 @@ import { AppMode, Chain, ChainId } from 'types';
 import { BridgeModel } from '../../types/bridge-model';
 
 import ethToRskAssets from '../assets/eth-to-rsk.mainnet';
-// import bscToRskAssets from '../assets/bsc-to-rsk.mainnet';
+import bscToRskAssets from '../assets/bsc-to-rsk.mainnet';
 import rskToEthAssets from '../assets/rsk-to-eth.mainnet';
-// import rskToBscAssets from '../assets/rsk-to-bsc.mainnet';
+import rskToBscAssets from '../assets/rsk-to-bsc.mainnet';
 
 const mainnetBridges = [
   // RSK / BSC
-  // new BridgeModel(
-  //   Chain.RSK,
-  //   ChainId.RSK_MAINNET,
-  //   ChainId.BSC_MAINNET,
-  //   '0x971b97c8cc82e7d27bc467c2dc3f219c6ee2e350',
-  //   '0xa2f50a2c699c1aa3b9089f6b565d4999d45d8983',
-  //   rskToBscAssets,
-  //   AppMode.MAINNET,
-  // ),
-  // new BridgeModel(
-  //   Chain.BSC,
-  //   ChainId.BSC_MAINNET,
-  //   ChainId.RSK_MAINNET,
-  //   '0xdfc7127593c8af1a17146893f10e08528f4c2aa7',
-  //   '0x05b68e70168e876b2025f837bc8e0b2312d5327d',
-  //   bscToRskAssets,
-  //   AppMode.MAINNET,
-  // ),
+  new BridgeModel(
+    Chain.RSK,
+    ChainId.RSK_MAINNET,
+    ChainId.BSC_MAINNET,
+    '0x971b97c8cc82e7d27bc467c2dc3f219c6ee2e350',
+    '0xa2f50a2c699c1aa3b9089f6b565d4999d45d8983',
+    rskToBscAssets,
+    AppMode.MAINNET,
+  ),
+  new BridgeModel(
+    Chain.BSC,
+    ChainId.BSC_MAINNET,
+    ChainId.RSK_MAINNET,
+    '0xdfc7127593c8af1a17146893f10e08528f4c2aa7',
+    '0x05b68e70168e876b2025f837bc8e0b2312d5327d',
+    bscToRskAssets,
+    AppMode.MAINNET,
+  ),
 
   // RSK / ETH
   new BridgeModel(
