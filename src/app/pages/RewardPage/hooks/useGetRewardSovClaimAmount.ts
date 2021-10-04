4564891542d6b733ac3fb6dc569c2729d842d830
soulBit <its.soulBit@gmail.com>
2021-10-04 13:49:22 +0100

    fix: reward sov claim amount sum

--- src/app/pages/RewardPage/hooks/useGetRewardSovClaimAmount.ts
+++ src/app/pages/RewardPage/hooks/useGetRewardSovClaimAmount.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { bignumber } from 'mathjs';
 import { weiTo18 } from 'utils/blockchain/math-helpers';
 import { useGetAvailableLendingRewards } from '../components/RewardTab/hooks/useGetAvailableLendingRewards';
 import { useGetAvailableLiquidityRewards } from '../components/RewardTab/hooks/useGetAvailableLiquidityRewards';
@@ -15,21 +16,22 @@ export const useGetRewardSovClaimAmount = (): RewardSovClaimData => {
   const availableLendingRewards = useGetAvailableLendingRewards();
   const availableTradingRewards = useGetAvailableTradingRewards();
   const availableLiquidityRewards = useGetAvailableLiquidityRewards();
-
+  // bignumber(useGetAvailableLiquidityRewards())
+  //   .mul(10 ** 4)
+  //   .toFixed();
   const amountToClaim = useMemo(
     () =>
-      weiTo18(
-        availableLiquidityRewards +
-          availableLendingRewards +
-          availableTradingRewards,
-      ),
+      (
+        Number(availableLendingRewards) +
+        Number(availableLiquidityRewards) +
+        Number(availableTradingRewards)
+      ).toString(),
     [
       availableLendingRewards,
       availableLiquidityRewards,
       availableTradingRewards,
     ],
   );
-
   return {
     availableLendingRewards: availableLendingRewards,
     availableTradingRewards: availableTradingRewards,
