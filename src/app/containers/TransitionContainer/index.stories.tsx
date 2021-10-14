0077c59ecb0bfd771eacf74439851eaa677c56ba
ShynRou <dustin@commit.international>
2021-10-14 12:43:50 +0200

    [TransitionContainer] implement animation types

--- src/app/containers/TransitionContainer/index.stories.tsx
+++ src/app/containers/TransitionContainer/index.stories.tsx
@@ -1,40 +1,70 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
-import { TransitionContainer } from './index';
+import { TransitionContainer, TransitionContainerAnimation } from './index';
 
 export default {
   title: 'Atoms/TransitionContainer',
   component: TransitionContainer,
 };
 
+const Transitions = [
+  TransitionContainerAnimation.slideLeft,
+  TransitionContainerAnimation.slideUp,
+  TransitionContainerAnimation.slideRight,
+  TransitionContainerAnimation.slideDown,
+];
+
 export const Basic = ({ animateHeight = true }) => {
   const [active, setActive] = useState(0);
+  const [animation, setAnimation] = useState(
+    TransitionContainerAnimation.slideLeft,
+  );
+
+  const onAnimationStarted = useCallback(() => {
+    setAnimation(Transitions[active]);
+  }, [active]);
+
   return (
-    <TransitionContainer active={active} animateHeight={animateHeight}>
-      {active === 0 && (
-        <button
-          className="tw-p-8 tw-text-black tw-bg-primary"
-          onClick={() => setActive(1)}
-        >
-          Alpha
-        </button>
-      )}
-      {active === 1 && (
-        <button
-          className="tw-p-16 tw-text-black tw-bg-secondary"
-          onClick={() => setActive(2)}
-        >
-          Beta
-        </button>
-      )}
-      {active === 2 && (
-        <button
-          className="tw-p-24 tw-text-black tw-bg-sov-white"
-          onClick={() => setActive(0)}
-        >
-          Gamma
-        </button>
-      )}
-    </TransitionContainer>
+    <div className="tw-w-80 tw-p-4 tw-mx-auto tw-bg-gray-3 tw-rounded-xl">
+      <TransitionContainer
+        active={active}
+        animateHeight={animateHeight}
+        animation={animation}
+        onAnimationStarted={onAnimationStarted}
+      >
+        {active === 0 && (
+          <button
+            className="tw-p-8 tw-text-black tw-bg-primary"
+            onClick={() => setActive(1)}
+          >
+            Alpha
+          </button>
+        )}
+        {active === 1 && (
+          <button
+            className="tw-p-16 tw-text-black tw-bg-secondary"
+            onClick={() => setActive(2)}
+          >
+            Beta
+          </button>
+        )}
+        {active === 2 && (
+          <button
+            className="tw-p-24 tw-text-black tw-bg-sov-white"
+            onClick={() => setActive(3)}
+          >
+            Gamma
+          </button>
+        )}
+        {active === 3 && (
+          <button
+            className="tw-p-28 tw-text-black tw-bg-trade-long"
+            onClick={() => setActive(0)}
+          >
+            Delta
+          </button>
+        )}
+      </TransitionContainer>
+    </div>
   );
 };
--- src/app/containers/TransitionContainer/index.tsx
+++ src/app/containers/TransitionContainer/index.tsx
@@ -1,14 +1,62 @@
 import classNames from 'classnames';
-import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
+import React, {
+  useCallback,
+  useState,
+  useRef,
+  useLayoutEffect,
+  useEffect,
+} from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { useResizeObserver } from '../../hooks/useResizeObserver';
 import styles from './index.module.scss';
+
+export enum TransitionContainerAnimation {
+  fade = 'fade',
+  slideLeft = 'slideLeft',
+  slideRight = 'slideRight',
+  slideUp = 'slideUp',
+  slideDown = 'slideDown',
+}
+
+const classNamesForAnimation = (animation: string) => ({
+  enter: styles[`${animation}Enter`],
+  enterActive: styles[`${animation}EnterActive`],
+  enterDone: styles[`${animation}EnterDone`],
+  exit: styles[`${animation}Exit`],
+  exitActive: styles[`${animation}ExitActive`],
+  exitDone: styles[`${animation}ExitDone`],
+});
+
+const AnimationConfig: {} = {
+  fade: {
+    classNames: classNamesForAnimation('fade'),
+    transitionProperties: ['opacity'],
+  },
+  slideLeft: {
+    classNames: classNamesForAnimation('slideLeft'),
+    transitionProperties: ['transform'],
+  },
+  slideRight: {
+    classNames: classNamesForAnimation('slideRight'),
+    transitionProperties: ['transform'],
+  },
+  slideUp: {
+    classNames: classNamesForAnimation('slideUp'),
+    transitionProperties: ['transform', 'bottom'],
+  },
+  slideDown: {
+    classNames: classNamesForAnimation('slideDown'),
+    transitionProperties: ['transform', 'top'],
+  },
+};
 
 type ITransitionContainerProps = {
   active: number | string;
   children: React.ReactNode;
   animateHeight?: boolean;
   duration?: number;
+  animation?: TransitionContainerAnimation;
+  onAnimationStarted?: () => void;
 };
 
 export const TransitionContainer: React.FC<ITransitionContainerProps> = ({
@@ -16,36 +64,46 @@ export const TransitionContainer: React.FC<ITransitionContainerProps> = ({
   children,
   animateHeight = true,
   duration = 1000,
+  animation = TransitionContainerAnimation.fade,
+  onAnimationStarted,
 }) => {
   const { ref, dimensions } = useResizeObserver<HTMLDivElement>();
 
+  const animationConfig = AnimationConfig[animation];
+
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={active}
-        classNames={{
-          enter: styles.fadeEnter,
-          enterActive: styles.fadeEnterActive,
-          enterDone: styles.fadeEnterDone,
-          exit: styles.fadeExit,
-          exitActive: styles.fadeExitActive,
-          exitDone: styles.fadeExitDone,
-        }}
-        timeout={{ enter: duration, exit: duration }}
-      >
-        <div
-          className={classNames(
-            'tw-absolute tw-w-full tw-bg-gray-3',
-            animateHeight && 'tw-overflow-hidden',
-          )}
-          style={{
-            height: animateHeight ? dimensions?.height : undefined,
-            transition: `all ease-in-out ${duration}ms`,
-          }}
+    <div
+      className={classNames(
+        'tw-relative tw-w-full',
+        animateHeight && 'tw-overflow-hidden',
+      )}
+      style={{
+        height: animateHeight ? dimensions?.height : undefined,
+        transition: `height ease-in-out ${duration}ms`,
+      }}
+    >
+      <TransitionGroup>
+        <CSSTransition
+          key={active}
+          classNames={animationConfig.classNames}
+          timeout={{ enter: duration, exit: duration }}
+          onEntering={onAnimationStarted}
         >
-          <div ref={ref}>{children}</div>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
+          <div
+            className={classNames(
+              'tw-absolute tw-w-full',
+              animateHeight && 'tw-overflow-hidden',
+            )}
+            style={{
+              transition: animationConfig.transitionProperties
+                .map(prop => `${prop} ease-in-out ${duration}ms`)
+                .join(', '),
+            }}
+          >
+            <div ref={ref}>{children}</div>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
   );
 };
