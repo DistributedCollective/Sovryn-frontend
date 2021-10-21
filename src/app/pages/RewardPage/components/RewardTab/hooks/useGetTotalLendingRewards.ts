54e2e2fd34bbc7b2e299a94217d21705abe1f6a3
soulBit <its.soulBit@gmail.com>
2021-10-21 12:00:57 +0100

    fix: use filtered RewardClaimed events

--- src/app/pages/RewardPage/components/RewardTab/hooks/useGetTotalLendingRewards.ts
+++ src/app/pages/RewardPage/components/RewardTab/hooks/useGetTotalLendingRewards.ts
@@ -1,17 +1,24 @@
-import { useGetContractPastEvents } from 'app/hooks/useGetContractPastEvents';
-import { bignumber } from 'mathjs';
 import { useMemo } from 'react';
+import { bignumber } from 'mathjs';
+
+import { useGetContractPastEvents } from 'app/hooks/useGetContractPastEvents';
+import { LendingPoolDictionary } from 'utils/dictionaries/lending-pool-dictionary';
+
+const lendPools = LendingPoolDictionary.list().map(
+  value => value.getAssetDetails().lendingContract.address,
+);
 
 export const useGetTotalLendingRewards = (): string => {
   const { events: lendingRewardEvents } = useGetContractPastEvents(
-    'lockedSov',
-    'Deposited',
+    'liquidityMiningProxy',
+    'RewardClaimed',
   );
 
   const totalLendingRewards = useMemo(
     () =>
       lendingRewardEvents
-        .map(item => item.returnValues.sovAmount)
+        .filter(item => lendPools.includes(item.returnValues.poolToken))
+        .map(item => item.returnValues.amount)
         .reduce((prevValue, curValue) => prevValue.add(curValue), bignumber(0)),
     [lendingRewardEvents],
   );
--- src/app/pages/RewardPage/components/RewardTab/hooks/useGetTotalLiquidityRewards.ts
+++ src/app/pages/RewardPage/components/RewardTab/hooks/useGetTotalLiquidityRewards.ts
@@ -1,6 +1,13 @@
-import { useGetContractPastEvents } from 'app/hooks/useGetContractPastEvents';
-import { bignumber } from 'mathjs';
 import { useMemo } from 'react';
+import { bignumber } from 'mathjs';
+
+import { useGetContractPastEvents } from 'app/hooks/useGetContractPastEvents';
+import { LiquidityPoolDictionary } from 'utils/dictionaries/liquidity-pool-dictionary';
+import { currentNetwork } from 'utils/classifiers';
+
+const liquidityPools = LiquidityPoolDictionary.list().map(
+  pool => pool.supplyAssets[0].poolTokens[currentNetwork],
+);
 
 export const useGetTotalLiquidityRewards = (): string => {
   const { events: liquidityRewardsEvents } = useGetContractPastEvents(
@@ -11,6 +18,7 @@ export const useGetTotalLiquidityRewards = (): string => {
   const totalLiquidityRewards = useMemo(
     () =>
       liquidityRewardsEvents
+        .filter(item => liquidityPools.includes(item.returnValues.poolToken))
         .map(item => item.returnValues.amount)
         .reduce((prevValue, curValue) => prevValue.add(curValue), bignumber(0)),
     [liquidityRewardsEvents],
