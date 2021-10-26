fc4aba4a1595a07192f01e6e5ec5f14d24c6df88
ShynRou <dustin@commit.international>
2021-10-26 10:16:22 +0200

    [Perpetual] fix minor issues

--- src/app/pages/PerpetualPage/index.tsx
+++ src/app/pages/PerpetualPage/index.tsx
@@ -12,7 +12,6 @@ import { reducer, sliceKey, actions } from './slice';
 import { HeaderLabs } from '../../components/HeaderLabs';
 import { Footer } from '../../components/Footer';
 import { PerpetualPairDictionary } from '../../../utils/dictionaries/perpetual-pair-dictionary';
-import { TradeForm } from './components/TradeForm';
 import { Theme, TradingChart } from '../../components/TradingChart';
 import { OpenPositionsTable } from './components/OpenPositionsTable';
 import { useIsConnected } from '../../hooks/useAccount';
