bf7fd2711a4571e3a02b9d429cbcfb1380b14dc9
tiltom <tilnak.tomas@gmail.com>
2021-11-05 15:32:51 +0100

    Initial version with hardcoded values

--- src/app/pages/PerpetualPage/components/TradeReviewDialog/index.tsx
+++ src/app/pages/PerpetualPage/components/TradeReviewDialog/index.tsx
@@ -8,6 +8,8 @@ import { actions } from '../../slice';
 import { isPerpetualTrade, PerpetualPageModals } from '../../types';
 import { usePerpetual_openTrade } from '../../hooks/usePerpetual_openTrade';
 import { getTradeDirection } from '../../utils/contractUtils';
+import styles from './index.module.scss';
+import classNames from 'classnames';
 
 export const TradeReviewDialog: React.FC = () => {
   const dispatch = useDispatch();
@@ -45,9 +47,60 @@ export const TradeReviewDialog: React.FC = () => {
       onClose={onClose}
     >
       <h1>{t(translations.perpetualPage.reviewTrade.title)}</h1>
-      {/* TODO: implement Review Trade Dialog */}
-      <pre>{JSON.stringify(trade, null, 2)}</pre>
-      <button onClick={onSubmit}>Open trade</button>
+      <div className={styles.contentWrapper}>
+        <div className={styles.importantInfo}>
+          <div className={styles.orderAction}>5x Market Buy</div>
+          <div className={styles.orderActionAdditionalInfo}>
+            0.002 BTC @ ≥ 46415.50 USD
+          </div>
+        </div>
+
+        <div className={classNames(styles.importantInfo, 'tw-mt-4')}>
+          <div className={styles.tradingFeeWrapper}>
+            <span className="tw-text-gray-10 tw-font-light">Trading fee:</span>
+            <span className="tradingFeeValue">0.0004 BTC</span>
+          </div>
+        </div>
+
+        <div className={styles.positionDetailsTitle}>New Position Details</div>
+
+        <div className={styles.positionInfo}>
+          <div className={styles.positionInfoRow}>
+            <span className="tw-text-gray-10 tw-font-light">
+              Position Size:
+            </span>
+            <span>+0.006 BTC</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">Margin:</span>
+            <span>0.006 BTC</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">Leverage:</span>
+            <span>5x</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">Entry Price:</span>
+            <span>≥ 46415.50</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">
+              Liquidation Price:
+            </span>
+            <span>91 000 USD</span>
+          </div>
+        </div>
+
+        <div className="tw-flex tw-justify-center">
+          <button className={styles.confirmButton} onClick={onSubmit}>
+            Confirm
+          </button>
+        </div>
+      </div>
     </Dialog>
   );
 };
