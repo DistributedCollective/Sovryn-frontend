7154f2cd42921d7596d629ac5d410f14c97682ed
soulBit <its.soulBit@gmail.com>
2021-11-15 18:23:23 +0100

    Master to development (#1882)
    * feat: add deposit button to header, xusd asset icons to portfolio, re… (#1870)
    
    * feat: add deposit button to header, xusd asset icons to portfolio, remove action button borders on portfolio
    
    * remove unused className attributes
    
    * chore: review styling update
    
    * feat: close mynt sale, add to previous sale dashboard (#1878)
    
    * hotfix: use wrbtc proxy instead of swaps external module for swaps (#1880)
    
    * hotfix: use wrbtc proxy instead of swaps external module for swaps
    
    * hotfix: buy sov page
    
    Co-authored-by: Rytis Grincevičius <rytis.grincevicius@gmail.com>
    
    Co-authored-by: Victor Creed <69458664+creed-victor@users.noreply.github.com>
    Co-authored-by: Rytis Grincevičius <rytis.grincevicius@gmail.com>

--- src/app/pages/OriginsLaunchpad/index.tsx
+++ src/app/pages/OriginsLaunchpad/index.tsx
@@ -4,7 +4,8 @@ import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
 import { Header } from 'app/components/Header';
 import { Footer } from 'app/components/Footer';
-import { SalesDay } from './pages/SalesDay';
+//import { SalesDay } from './pages/SalesDay';
+import { Dashboard } from './pages/Dashboard';
 
 export const OriginsLaunchpad: React.FC = () => {
   const { t } = useTranslation();
@@ -27,7 +28,8 @@ export const OriginsLaunchpad: React.FC = () => {
       <Header />
 
       <div className="tw-container tw-pt-11 tw-font-body">
-        <SalesDay saleName="MYNT" />
+        {/* <SalesDay saleName="MYNT" /> */}
+        <Dashboard />
       </div>
       <Footer />
     </>
