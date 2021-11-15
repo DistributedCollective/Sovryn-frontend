78cee6f49976c6c288b4198b9cd32746a634bdc0
tiltom <tilnak.tomas@gmail.com>
2021-11-15 13:13:27 +0100

    Use localized labels and values from trade state

--- src/app/pages/PerpetualPage/components/TradeReviewDialog/index.tsx
+++ src/app/pages/PerpetualPage/components/TradeReviewDialog/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { translations } from '../../../../../locales/i18n';
 import { Dialog } from '../../../../containers/Dialog';
@@ -7,14 +7,29 @@ import { selectPerpetualPage } from '../../selectors';
 import { actions } from '../../slice';
 import { isPerpetualTrade, PerpetualPageModals } from '../../types';
 import { usePerpetual_openTrade } from '../../hooks/usePerpetual_openTrade';
-import { getTradeDirection } from '../../utils/contractUtils';
 import styles from './index.module.scss';
 import classNames from 'classnames';
+import { TradingPosition } from 'types/trading-position';
+import { fromWei } from 'web3-utils';
+import { usePerpetual_queryAmmState } from '../../hooks/usePerpetual_queryAmmState';
+import { usePerpetual_queryPerpParameters } from '../../hooks/usePerpetual_queryPerpParameters';
+import { getMidPrice } from '../../utils/perpUtils';
+import { toNumberFormat } from 'utils/display-text/format';
+
+const getTradePosition = (tradePosition: TradingPosition) =>
+  tradePosition === TradingPosition.LONG ? (
+    <Trans i18nKey={translations.perpetualPage.reviewTrade.buy} />
+  ) : (
+    <Trans i18nKey={translations.perpetualPage.reviewTrade.sell} />
+  );
 
 export const TradeReviewDialog: React.FC = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
   const { modal, modalOptions } = useSelector(selectPerpetualPage);
+
+  const ammState = usePerpetual_queryAmmState();
+  const perpParameters = usePerpetual_queryPerpParameters();
 
   const { trade: openTrade } = usePerpetual_openTrade();
 
@@ -41,6 +56,15 @@ export const TradeReviewDialog: React.FC = () => {
     [openTrade, trade],
   );
 
+  const midPrice = useMemo(() => getMidPrice(perpParameters, ammState), [
+    perpParameters,
+    ammState,
+  ]);
+
+  if (!trade) {
+    return null;
+  }
+
   return (
     <Dialog
       isOpen={modal === PerpetualPageModals.TRADE_REVIEW}
@@ -49,15 +73,21 @@ export const TradeReviewDialog: React.FC = () => {
       <h1>{t(translations.perpetualPage.reviewTrade.title)}</h1>
       <div className={styles.contentWrapper}>
         <div className={styles.importantInfo}>
-          <div className={styles.orderAction}>5x Market Buy</div>
+          <div className={styles.orderAction}>
+            {trade?.leverage}x{' '}
+            {t(translations.perpetualPage.reviewTrade.market)}{' '}
+            {getTradePosition(trade?.position)}
+          </div>
           <div className={styles.orderActionAdditionalInfo}>
-            0.002 BTC @ ≥ 46415.50 USD
+            {fromWei(trade?.amount)} BTC @ ≥ {toNumberFormat(midPrice, 2)} USD
           </div>
         </div>
 
         <div className={classNames(styles.importantInfo, 'tw-mt-4')}>
           <div className={styles.tradingFeeWrapper}>
-            <span className="tw-text-gray-10 tw-font-light">Trading fee:</span>
+            <span className="tw-text-gray-10 tw-font-light">
+              {t(translations.perpetualPage.tradeForm.labels.tradingFee)}
+            </span>
             <span className="tradingFeeValue">0.0004 BTC</span>
           </div>
         </div>
@@ -67,29 +97,42 @@ export const TradeReviewDialog: React.FC = () => {
         <div className={styles.positionInfo}>
           <div className={styles.positionInfoRow}>
             <span className="tw-text-gray-10 tw-font-light">
-              Position Size:
+              {t(translations.perpetualPage.reviewTrade.labels.positionSize)}
             </span>
-            <span>+0.006 BTC</span>
-          </div>
-
-          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
-            <span className="tw-text-gray-10 tw-font-light">Margin:</span>
-            <span>0.006 BTC</span>
-          </div>
-
-          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
-            <span className="tw-text-gray-10 tw-font-light">Leverage:</span>
-            <span>5x</span>
-          </div>
-
-          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
-            <span className="tw-text-gray-10 tw-font-light">Entry Price:</span>
-            <span>≥ 46415.50</span>
+            <span className={styles.positionSize}>
+              +{fromWei(trade?.amount)} BTC
+            </span>
           </div>
 
           <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
             <span className="tw-text-gray-10 tw-font-light">
-              Liquidation Price:
+              {t(translations.perpetualPage.reviewTrade.labels.margin)}
+            </span>
+            <span>0.006 BTC</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">
+              {t(translations.perpetualPage.reviewTrade.labels.leverage)}
+            </span>
+            <span>{trade.leverage}x</span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">
+              {t(translations.perpetualPage.reviewTrade.labels.entryPrice)}
+            </span>
+            <span>
+              {trade.position === TradingPosition.LONG ? '≥' : '≤'}{' '}
+              {toNumberFormat(midPrice, 2)}
+            </span>
+          </div>
+
+          <div className={classNames(styles.positionInfoRow, 'tw-mt-2')}>
+            <span className="tw-text-gray-10 tw-font-light">
+              {t(
+                translations.perpetualPage.reviewTrade.labels.liquidationPrice,
+              )}
             </span>
             <span>91 000 USD</span>
           </div>
