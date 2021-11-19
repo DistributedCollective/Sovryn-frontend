8dbfe6b1ac5dd101bf3d29983aabbf30a0e16ab2
tiltom <tilnak.tomas@gmail.com>
2021-11-19 13:37:07 +0100

    Implement account balance

--- src/app/pages/PerpetualPage/hooks/usePerpetual_accountBalance.ts
+++ src/app/pages/PerpetualPage/hooks/usePerpetual_accountBalance.ts
@@ -1,7 +1,16 @@
-import { useBlockSync } from 'app/hooks/useAccount';
-import { useEffect, useState } from 'react';
+import { useAccount, useBlockSync } from 'app/hooks/useAccount';
+import { bridgeNetwork } from 'app/pages/BridgeDepositPage/utils/bridge-network';
+import { useEffect, useMemo, useState } from 'react';
+import { Chain } from 'types';
+import { getContract } from 'utils/blockchain/contract-helpers';
 import { toWei } from '../../../../utils/blockchain/math-helpers';
 import { PerpetualPairType } from '../../../../utils/dictionaries/perpetual-pair-dictionary';
+import { usePerpetual_queryTraderState } from './usePerpetual_queryTraderState';
+import marginTokenAbi from 'utils/blockchain/abi/MarginToken.json';
+import { usePerpetual_marginAccountBalance } from './usePerpetual_marginAccountBalance';
+import { usePerpetual_queryAmmState } from './usePerpetual_queryAmmState';
+import { getTraderPnL } from '../utils/perpUtils';
+import { bignumber } from 'mathjs';
 
 type AccountBalance = {
   total: string;
@@ -12,6 +21,7 @@ type AccountBalance = {
 
 export const usePerpetual_accountBalance = (pairType: PerpetualPairType) => {
   const blockId = useBlockSync();
+  const account = useAccount();
   const [data, setData] = useState<AccountBalance>({
     total: '',
     available: '',
@@ -19,17 +29,38 @@ export const usePerpetual_accountBalance = (pairType: PerpetualPairType) => {
     unrealized: '',
   });
 
-  // TODO: implement perpetual account Data fetching
+  const [availableBalance, setAvailableBalance] = useState('0');
+
+  const traderState = usePerpetual_queryTraderState();
+  const ammState = usePerpetual_queryAmmState();
+  const marginBalance = usePerpetual_marginAccountBalance();
 
   useEffect(() => {
-    const unrealized = blockId % 2 === 0 ? 37.7331 : -37.7331;
+    bridgeNetwork
+      .call(
+        Chain.BSC,
+        getContract('PERPETUALS_token').address,
+        marginTokenAbi,
+        'balanceOf',
+        [account],
+      )
+      .catch(e => console.log(e))
+      .then(result => result && setAvailableBalance(String(result)));
+  }, [account]);
+
+  const unrealizedPnl = useMemo(() => getTraderPnL(traderState, ammState), [
+    ammState,
+    traderState,
+  ]);
+
+  useEffect(() => {
     setData({
-      total: toWei(1300 + unrealized),
-      available: toWei(500),
-      inPositions: toWei(700),
-      unrealized: toWei(unrealized),
+      total: bignumber(availableBalance).add(marginBalance.fCashCC).toString(),
+      available: availableBalance,
+      inPositions: toWei(marginBalance.fCashCC),
+      unrealized: toWei(unrealizedPnl),
     });
-  }, [blockId]);
+  }, [availableBalance, blockId, marginBalance.fCashCC, unrealizedPnl]);
 
   return data;
 };
