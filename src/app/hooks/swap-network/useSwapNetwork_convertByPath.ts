35a591ace0d277ee398e2f68f86fff09b6c22381
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2021-11-25 15:56:08 +0200

    fix: remove additional params

--- src/app/hooks/swap-network/useSwapNetwork_convertByPath.ts
+++ src/app/hooks/swap-network/useSwapNetwork_convertByPath.ts
@@ -1,8 +1,8 @@
 import { useSendContractTx } from '../useSendContractTx';
 import { useAccount } from '../useAccount';
-import { Asset, AppMode } from '../../../types';
+import { Asset } from '../../../types';
 import { TxType } from '../../../store/global/transactions-store/types';
-import { gasLimit, currentNetwork } from '../../../utils/classifiers';
+import { gasLimit } from '../../../utils/classifiers';
 
 export function useSwapNetwork_convertByPath(
   sourceToken: Asset,
@@ -38,12 +38,6 @@ export function useSwapNetwork_convertByPath(
           '0x0000000000000000000000000000000000000000',
           '0',
         ];
-
-        // add affiliate params on testnet
-        // todo make sure to move it to mainnet once contract deployed
-        if (currentNetwork === AppMode.TESTNET) {
-          args = [...args, ['0x0000000000000000000000000000000000000000', '0']];
-        }
 
         config = {
           from: account,
