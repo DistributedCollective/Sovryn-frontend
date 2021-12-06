570336ba97bba8662c933d89a540c23beaf599dc
tiltom <tilnak.tomas@gmail.com>
2021-12-06 15:05:15 +0100

    Adjust usePerpetual_getLatestTradeId hook

--- src/app/pages/PerpetualPage/hooks/usePerpetual_getLatestTradeId.ts
+++ src/app/pages/PerpetualPage/hooks/usePerpetual_getLatestTradeId.ts
@@ -1,8 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { Nullable } from 'types';
 import { RecentTradesContext } from '../components/RecentTradesTable/context';
+import { useDebouncedEffect } from '../../../hooks/useDebouncedEffect';
+
+const BSC_AVERAGE_BLOCK_TIME = 3500; // 3.5s
 
 export const usePerpetual_getLatestTradeId = () => {
+  const [latestTradeId, setLatestTradeId] = useState<Nullable<string>>();
   const { trades } = useContext(RecentTradesContext);
 
-  return trades[0]?.id;
+  useDebouncedEffect(
+    () => setLatestTradeId(trades[0]?.id),
+    [trades],
+    BSC_AVERAGE_BLOCK_TIME,
+  );
+
+  return latestTradeId;
 };
