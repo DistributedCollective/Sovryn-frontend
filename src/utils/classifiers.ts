c40b4d7fdf6f8c26ff7b5a87d2e878e99149422c
soulBit <its.soulBit@gmail.com>
2021-12-08 11:37:04 +0000

    chore: bump deposit collateral tx gas limit

--- src/utils/classifiers.ts
+++ src/utils/classifiers.ts
@@ -76,7 +76,7 @@ export const gasLimit = {
   [TxType.STAKING_WITHDRAW]: 650000,
   [TxType.STAKING_REWARDS_CLAIM]: 3250000, //limit should be reduced once contract issue with claiming 0 values is resolved
   [TxType.STAKING_LIQUID_SOV_CLAIM]: 1500000,
-  [TxType.DEPOSIT_COLLATERAL]: 150000,
+  [TxType.DEPOSIT_COLLATERAL]: 250000,
   [TxType.CLAIM_VESTED_SOV_REWARDS]: 6000000,
   [TxType.SOV_WITHDRAW_VESTING]: 1900000,
   [TxType.SIMULATOR_REQUEST]: 6800000,
