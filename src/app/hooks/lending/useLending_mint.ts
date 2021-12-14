cecc0fe5349c14e22838b896ad287e2fd69a6ce4
soulBit <its.soulBit@gmail.com>
2021-12-14 17:45:40 +0000

    fix: bump gas limit for unlend, ensure lend/unlend respects gas limits

--- src/app/hooks/lending/useLending_mint.ts
+++ src/app/hooks/lending/useLending_mint.ts
@@ -3,6 +3,7 @@ import { getLendingContractName } from 'utils/blockchain/contract-helpers';
 import { TxType } from 'store/global/transactions-store/types';
 import { useSendContractTx } from '../useSendContractTx';
 import { useAccount } from '../useAccount';
+import { gasLimit } from 'utils/classifiers';
 import { LendingPoolDictionary } from '../../../utils/dictionaries/lending-pool-dictionary';
 
 export function useLending_mint(asset: Asset, weiAmount: string) {
@@ -18,7 +19,12 @@ export function useLending_mint(asset: Asset, weiAmount: string) {
       asset === Asset.RBTC
         ? send(
             [account, useLM],
-            { from: account, value: weiAmount, nonce },
+            {
+              from: account,
+              value: weiAmount,
+              nonce,
+              gas: gasLimit[TxType.LEND],
+            },
             {
               approveTransactionHash: approveTx,
               type: TxType.LEND,
@@ -26,7 +32,7 @@ export function useLending_mint(asset: Asset, weiAmount: string) {
           )
         : send(
             [account, weiAmount, useLM],
-            { from: account, nonce },
+            { from: account, nonce, gas: gasLimit[TxType.LEND] },
             {
               approveTransactionHash: approveTx,
               type: TxType.LEND,
