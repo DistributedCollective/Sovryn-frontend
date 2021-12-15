55f0f2aa85968cb7db1e7d222e32c68fe79c9b24
Victor Creed <victor@sovryn.app>
2021-12-15 10:16:02 +0200

    feat(mynt): add amm testnet contracts

--- src/utils/dictionaries/liquidity-pool-dictionary.ts
+++ src/utils/dictionaries/liquidity-pool-dictionary.ts
@@ -156,6 +156,19 @@ export class LiquidityPoolDictionary {
         }),
       ]).setVersion(1),
     ],
+    [
+      Asset.MYNT,
+      new LiquidityPool(Asset.MYNT, [
+        new LiquidityPoolSupplyAsset(Asset.MYNT, {
+          mainnet: '0xAE66117C8105a65D914fB47d37a127E879244319', // todo
+          testnet: '0xB12FA09a50c56e9a0C826b98e76DA7645017AB4D',
+        }),
+        new LiquidityPoolSupplyAsset(Asset.RBTC, {
+          mainnet: '0x7Fef930ebaA90B2f8619722adc55e3f1D965B79b',
+          testnet: '0x6e03DEFD0ae9091Be74f64c8CB9BE319994E5deB',
+        }),
+      ]).setVersion(1),
+    ],
   ]);
 
   public static get(asset: Asset): LiquidityPool {
