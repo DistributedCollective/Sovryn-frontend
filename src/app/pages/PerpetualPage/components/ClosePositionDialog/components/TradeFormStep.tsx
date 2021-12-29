5f49f2468cc4b8a54ac8525f0e56cf3d16830cc2
tiltom <tilnak.tomas@gmail.com>
2021-12-29 13:23:36 +0100

    Unify action dialogs submit buttons

--- src/app/pages/PerpetualPage/components/ClosePositionDialog/components/TradeFormStep.tsx
+++ src/app/pages/PerpetualPage/components/ClosePositionDialog/components/TradeFormStep.tsx
@@ -38,8 +38,7 @@ import { PerpetualTxMethods, PerpetualTx } from '../../TradeDialog/types';
 import { PerpetualQueriesContext } from 'app/pages/PerpetualPage/contexts/PerpetualQueriesContext';
 import { roundToLot } from '../../../utils/perpMath';
 import { useMaintenance } from 'app/hooks/useMaintenance';
-import { ErrorBadge } from 'app/components/Form/ErrorBadge';
-import { discordInvite } from 'utils/classifiers';
+import { ActionDialogSubmitButton } from '../../ActionDialogSubmitButton';
 
 export const TradeFormStep: TransitionStep<ClosePositionDialogStep> = ({
   changeTo,
@@ -319,39 +318,15 @@ export const TradeFormStep: TransitionStep<ClosePositionDialogStep> = ({
         </div>
       )}
 
-      {!inMaintenance ? (
-        <button
-          className={classNames(
-            'tw-absolute tw-bottom-0 tw-w-full tw-min-h-10 tw-p-2 tw-mt-4 tw-text-lg tw-text-primary tw-font-medium tw-border tw-border-primary tw-bg-primary-10 tw-rounded-lg tw-transition-colors tw-transition-opacity tw-duration-300',
-            isButtonDisabled
-              ? 'tw-opacity-25 tw-cursor-not-allowed'
-              : 'tw-opacity-100 hover:tw-bg-primary-25',
-          )}
-          disabled={isButtonDisabled}
-          onClick={onSubmit}
-        >
-          {t(translations.perpetualPage.closePosition.button)}
-        </button>
-      ) : (
-        <ErrorBadge
-          content={
-            <Trans
-              i18nKey={translations.maintenance.perpetualsTrade}
-              components={[
-                <a
-                  href={discordInvite}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  className="tw-text-warning tw-text-xs tw-underline hover:tw-no-underline"
-                >
-                  x
-                </a>,
-              ]}
-            />
-          }
-          className="tw-mb-0 tw-pb-0"
-        />
-      )}
+      <ActionDialogSubmitButton
+        inMaintenance={inMaintenance}
+        isDisabled={isButtonDisabled}
+        onClick={onSubmit}
+        className="tw-absolute tw-bottom-0"
+        maintenanceClassName="tw-absolute tw-bottom-0"
+      >
+        {t(translations.perpetualPage.closePosition.button)}
+      </ActionDialogSubmitButton>
     </div>
   );
 };
