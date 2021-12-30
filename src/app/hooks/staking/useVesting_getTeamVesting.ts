18401e351d8c6f8293ebffe7d2faca5885e8e616
tiltom <tilnak.tomas@gmail.com>
2021-12-30 10:17:56 +0100

    Fix useVesting_getTeamVesting hook

--- src/app/hooks/staking/useVesting_getTeamVesting.ts
+++ src/app/hooks/staking/useVesting_getTeamVesting.ts
@@ -7,5 +7,8 @@ export function useVesting_getTeamVesting(address: string) {
     'getTeamVesting',
     !!address && address !== ethGenesisAddress,
     address || ethGenesisAddress,
+    2419200, // 28 days in seconds
+    62899200, // 24 months in seconds
+    1,
   );
 }
