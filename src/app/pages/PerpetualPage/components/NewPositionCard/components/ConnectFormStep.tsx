a7d0bd07376040cef90784c695665de785087ba3
tiltom <tilnak.tomas@gmail.com>
2022-02-02 11:11:52 +0100

    Revert "Don't show text in TradeForm if wallet is not connected"
    This reverts commit d0ead1b9a075bccf4aea6a13ccd52b0c7652c2ab.

--- src/app/pages/PerpetualPage/components/NewPositionCard/components/ConnectFormStep.tsx
+++ src/app/pages/PerpetualPage/components/NewPositionCard/components/ConnectFormStep.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useWalletContext } from '@sovryn/react-wallet';
 import { translations } from '../../../../../../locales/i18n';
 import { TransitionStep } from '../../../../../containers/TransitionSteps';
@@ -12,7 +12,33 @@ export const ConnectFormStep: TransitionStep<NewPositionCardStep> = ({
   const { connect } = useWalletContext();
 
   return (
-    <div className="tw-absolute tw-w-full tw-top-1/2 tw-h-auto tw-p-4">
+    <div className="tw-flex-grow tw-p-4 tw-text-xs">
+      <p className="tw-mt-4">
+        {t(translations.perpetualPage.tradeForm.text.welcome1)}
+      </p>
+      <p className="tw-mb-2">
+        {t(translations.perpetualPage.tradeForm.text.welcome2)}
+      </p>
+      <ul className="tw-ml-4 tw-mb-8 tw-list-disc">
+        <Trans
+          i18nKey={translations.perpetualPage.tradeForm.text.welcome3}
+          components={[<li className="tw-mb-1" />]}
+        />
+      </ul>
+      <p className="tw-mb-11">
+        {/* TODO: add href to quickstart guide */}
+        <Trans
+          i18nKey={translations.perpetualPage.tradeForm.text.welcome4}
+          components={[
+            <a
+              className="tw-text-secondary tw-underline"
+              href="https://wiki.sovryn.app/"
+            >
+              Quickstart Guide
+            </a>,
+          ]}
+        />
+      </p>
       <button
         className="tw-w-full tw-min-h-10 tw-p-2 tw-text-base tw-text-primary tw-border tw-border-primary tw-bg-primary-05 tw-rounded-lg tw-transition-colors tw-duration-300 hover:tw-bg-primary-25"
         onClick={connect}
