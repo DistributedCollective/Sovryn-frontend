69821f8965402166fc66984a13037d7e2930ce1c
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2022-02-21 10:33:29 +0200

    fix: review insights

--- src/app/hooks/useDetectWeb3ChainId.ts
+++ src/app/hooks/useDetectWeb3ChainId.ts
@@ -2,8 +2,9 @@ import { useContext, useMemo } from 'react';
 import { WalletContext } from '@sovryn/react-wallet';
 import { InjectedWalletProvider, isWeb3Wallet } from '@sovryn/wallet';
 
-export function useDetectWeb3ChainId(expectedChainId: number) {
+export const useDetectWeb3ChainId = (expectedChainId: number) => {
   const { wallet } = useContext(WalletContext);
+
   return useMemo(() => {
     if (isWeb3Wallet(wallet.providerType!)) {
       const provider = InjectedWalletProvider.getProvider(expectedChainId);
