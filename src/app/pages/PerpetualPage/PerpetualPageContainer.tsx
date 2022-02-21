48c8edeb0706744f8a567c95f7d17d8ca45c4198
ShynRou <dustin@commit.international>
2022-02-21 21:30:54 +0100

    [PerpetualsCompetition] implement competition closed text

--- src/app/pages/PerpetualPage/PerpetualPageContainer.tsx
+++ src/app/pages/PerpetualPage/PerpetualPageContainer.tsx
@@ -28,7 +28,7 @@ import { DataCard } from './components/DataCard';
 import { AmmDepthChart } from './components/AmmDepthChart';
 import { RecentTradesTable } from './components/RecentTradesTable';
 import { ContractDetails } from './components/ContractDetails';
-import { isMainnet } from '../../../utils/classifiers';
+import { isMainnet, discordInvite } from '../../../utils/classifiers';
 import { ChainId } from '../../../types';
 import { useWalletContext } from '@sovryn/react-wallet';
 import { ProviderType } from '@sovryn/wallet';
@@ -47,6 +47,8 @@ import { FundingPaymentsTable } from './components/FundingPaymentsTable/index';
 import { PerpetualQueriesContextProvider } from './contexts/PerpetualQueriesContext';
 import { PairSelector } from './components/PairSelector';
 import { ToastsWatcher } from './components/ToastsWatcher';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 export const PerpetualPageContainer: React.FC = () => {
   useInjectReducer({ key: sliceKey, reducer });
@@ -127,12 +129,65 @@ export const PerpetualPageContainer: React.FC = () => {
           }
         />
         <div className="tw-fixed tw-inset-0 tw-z-50 tw-flex tw-items-center tw-justify-center tw-bg-gray-5 tw-bg-opacity-50 tw-blur">
-          <div className="tw-w-full tw-max-w-lg tw-bg-black tw-text-sov-white tw-p-8 tw-rounded-xl">
-            <h1 className="tw-mb-6">Competition Closed</h1>
+          <div className="tw-w-full tw-max-w-3xl tw-bg-black tw-text-sov-white tw-p-8 tw-rounded-xl">
+            <h2 className="tw-mb-4">
+              The Sovryn Perpetual Futures Trading Competition has ended!
+            </h2>
             <p>
-              Competition Closed test aakjshd laksjd aksjd lakjsd laslj dalks
-              jdlaksj dlkajs d
+              A special thanks to everyone that participated! We hope you had
+              fun while helping us stress-test the perpetual futures interface
+              and backend. Your participation and feedback has provided us with
+              invaluable information and improvement suggestions, all of which
+              will be used to refine the functionality and interface prior to
+              launch on mainnet.
             </p>
+            <p className="tw-font-semibold tw-text-primary tw-bg-gray-2 tw-p-4 tw-rounded-lg">
+              Winners of the trading competition will be announced this Friday
+              the 25th February, through the official Sovryn channels!
+            </p>
+            <p className="tw-mt-6 tw-mb-1">Total Prize Pool = $10k in SOV</p>
+            <ul className="tw-list-disc tw-ml-4 tw-mb-4">
+              <li>
+                SOV price based on previous 30 day average on the Sovryn dapp.
+              </li>
+              <li>$7.5k SOV to be shared among the top three traders.</li>
+              <li>$2.5k SOV as a bug bounty or improvement suggestion.</li>
+              <li>Prizes vest monthly for a period of 10 months.</li>
+            </ul>
+            <p>
+              Sovryn will distribute prizes directly to the winning wallet
+              addresses used during the competition. If you are one of the named
+              winners and would prefer that payment be made to a different
+              address, please contact us on <a href={discordInvite}>Discord</a>.
+              In this event we will authenticate ownership of the winning
+              testnet wallets via a specific transaction - so don’t waste your
+              time impersonating a winner!
+            </p>
+            <p>
+              Bug bounty / improvement suggestion prize winners will be
+              announced at a later date after a thorough analysis of all
+              feedback, along with a report on findings and next steps.
+            </p>
+            <p className="tw-my-8 tw-font-semibold tw-bg-gray-2 tw-p-4 tw-rounded-lg">
+              <FontAwesomeIcon
+                icon={faExclamationTriangle}
+                size="2x"
+                className="tw-inline-block tw-float-left tw-text-primary tw-my-1 tw-mr-2"
+              />
+              Under no circumstance will any member of the Sovryn Team ask for
+              your personal, wallet, or any other sensitive information. At all
+              times, be aware of potential phishing scams and report anyone
+              attempting to directly contact you representing themselves as part
+              of the Sovryn Team.
+            </p>
+            <p>
+              Follow us and subscribe to our{' '}
+              <a href="https://wiki.sovryn.app/en/technical-documents/official-channels">
+                official channels
+              </a>{' '}
+              to stay tuned for more events!
+            </p>
+            <p className="tw-text-xl tw-font-bold">Stay Sovryn!</p>
           </div>
         </div>
         <div className="tw-relative tw--top-2.5 tw-w-full">
