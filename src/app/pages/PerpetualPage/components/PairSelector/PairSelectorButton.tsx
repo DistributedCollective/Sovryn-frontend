9efba02fa7293845ef1e36602e0c88280ca3240b
tiltom <tilnak.tomas@gmail.com>
2022-03-04 08:23:34 +0100

    Add label to PairSelectorButton

--- src/app/pages/PerpetualPage/components/PairSelector/PairSelectorButton.tsx
+++ src/app/pages/PerpetualPage/components/PairSelector/PairSelectorButton.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
+import { translations } from 'locales/i18n';
 import React, { useCallback, useContext, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { PerpetualPairType } from 'utils/dictionaries/perpetual-pair-dictionary';
 import { toNumberFormat } from 'utils/display-text/format';
 import { PerpetualPair } from 'utils/models/perpetual-pair';
@@ -20,6 +22,7 @@ export const PairSelectorButton: React.FC<PairSelectorButtonProps> = ({
   isSelected,
   onSelect,
 }) => {
+  const { t } = useTranslation();
   const { trades } = useContext(RecentTradesContext);
   const { ammState } = useContext(PerpetualQueriesContext);
 
@@ -37,7 +40,7 @@ export const PairSelectorButton: React.FC<PairSelectorButtonProps> = ({
   return (
     <div
       className={classNames(
-        'tw-flex tw-flex-row tw-items-center tw-min-w-56 tw-px-3 tw-py-1.5 tw-mr-2 tw-rounded-lg tw-select-none tw-transition-colors tw-duration-300',
+        'tw-flex tw-flex-row tw-items-center tw-min-w-56 tw-px-3 tw-py-1 tw-mr-2 tw-rounded-lg tw-select-none tw-transition-colors tw-duration-300',
         isSelected
           ? 'tw-bg-gray-5'
           : 'tw-bg-gray-4 tw-cursor-pointer hover:tw-bg-gray-5',
@@ -45,14 +48,14 @@ export const PairSelectorButton: React.FC<PairSelectorButtonProps> = ({
       onClick={onClick}
     >
       <span className="tw-font-medium tw-mr-2 tw-text-xs">{pair.name}</span>
-      <span
-        className={classNames(
-          'tw-flex-auto tw-text-right tw-font-medium tw-text-base',
-          color,
-        )}
-      >
-        {toNumberFormat(markPrice, 2)}
-      </span>
+      <div className="tw-flex-auto tw-text-right">
+        <div className="tw-text-tiny">
+          {t(translations.perpetualPage.pairSelector.markPrice)}
+        </div>
+        <span className={classNames('tw-font-medium tw-text-base', color)}>
+          {toNumberFormat(markPrice, 2)}
+        </span>
+      </div>
     </div>
   );
 };
