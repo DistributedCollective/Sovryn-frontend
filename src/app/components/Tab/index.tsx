74a06d9326a57bacbcd16c9a10b7bd5253e61af6
rick23p <peymannaderi3@gmail.com>
2022-03-08 12:50:14 +0330

    feat: update Tab component to match coding guideline

--- src/app/components/Tab/index.tsx
+++ src/app/components/Tab/index.tsx
@@ -1,41 +1,33 @@
 import React from 'react';
-import styled, { css } from 'styled-components/macro';
-import { Text } from '@blueprintjs/core';
+import classNames from 'classnames';
 
-interface Props {
-  text: string;
+type TabProps = {
+  text: React.ReactNode;
   active: boolean;
+  disabled?: boolean;
   onClick: () => void;
-}
+  className?: string;
+};
 
-export function Tab(props: Props) {
-  return (
-    <StyledTab active={props.active} onClick={() => props.onClick()}>
-      <Text ellipsize>{props.text}</Text>
-    </StyledTab>
-  );
-}
-
-interface StyledProps {
-  active: boolean;
-}
-const StyledTab = styled.button.attrs(_ => ({
-  type: 'button',
-  className: 'btn hover:tw-text-gray-9',
-}))`
-  color: var(--sov-white);
-  padding: 5px 10px;
-  background: transparent;
-  font-size: 1.125rem;
-  font-weight: 100;
-  font-family: Montserrat;
-  text-transform: none;
-  ${(props: StyledProps) =>
-    props.active &&
-    css`
-      font-weight: 400;
-      &:hover {
-        color: var(--white);
-      }
-    `}
-`;
+export const Tab: React.FC<TabProps> = ({
+  text,
+  active,
+  onClick,
+  disabled,
+  className,
+}) => (
+  <span
+    className={classNames(
+      'tw-text-sov-white tw-px-2.5 tw-py-2 tw-text-lg tw-font-thin tw-whitespace-nowrap',
+      {
+        'tw-text-white tw-font-normal': active,
+        'tw-cursor-pointer hover:tw-text-gray-9': !disabled,
+        'tw-opacity-50': disabled,
+      },
+      className,
+    )}
+    onClick={onClick}
+  >
+    {text}
+  </span>
+);
