175bbb273c3512a0d401046f253a5b62fa7d3fcc
ShynRou <dustin@commit.international>
2022-03-16 15:34:37 +0100

    [Heading] fix code style issues

--- src/app/components/Heading/index.tsx
+++ src/app/components/Heading/index.tsx
@@ -2,46 +2,42 @@ import classNames from 'classnames';
 import React from 'react';
 import styles from './index.module.scss';
 
-export enum HeadingStyles {
-  'h1' = 'h1',
-  'h2' = 'h2',
-  'h3' = 'h3',
-  'h4' = 'h4',
-  'h5' = 'h5',
-  'h6' = 'h6',
-  'label' = 'label',
+export enum HeadingStyle {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6',
+  label = 'label',
 }
 
-type IHeadingProps = {
-  style?: HeadingStyles;
+type HeadingProps = {
+  style?: HeadingStyle;
   children: React.ReactNode;
   className?: string;
   id?: string;
 };
 
-const Heading: (tag: string) => React.FC<IHeadingProps> = tag => ({
+const Heading: (tag: HeadingStyle) => React.FC<HeadingProps> = tag => ({
   style,
   className,
   id,
   children,
-}) => {
-  return React.createElement(
+}) =>
+  React.createElement(
     tag,
     {
-      className: classNames(
-        style ? styles[style.toString()] : styles[tag],
-        className,
-      ),
+      className: classNames(style ? styles[style] : styles[tag], className),
       id,
     },
     children,
   );
-};
 
-export const H1 = Heading('h1');
-export const H2 = Heading('h2');
-export const H3 = Heading('h3');
-export const H4 = Heading('h4');
-export const H5 = Heading('h5');
-export const H6 = Heading('h6');
-export const Label = Heading('label');
+export const H1 = Heading(HeadingStyle.h1);
+export const H2 = Heading(HeadingStyle.h2);
+export const H3 = Heading(HeadingStyle.h3);
+export const H4 = Heading(HeadingStyle.h4);
+export const H5 = Heading(HeadingStyle.h5);
+export const H6 = Heading(HeadingStyle.h6);
+export const Label = Heading(HeadingStyle.label);
