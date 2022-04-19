0ea23be42a5154d61fbbdfe2abca09a45a91405e
tiltom <tilnak.tomas@gmail.com>
2022-04-19 16:47:42 +0200

    Do not disable TradeForm when users have open positions

--- src/app/pages/PerpetualPage/components/NewPositionCard/components/TradeFormStep.tsx
+++ src/app/pages/PerpetualPage/components/NewPositionCard/components/TradeFormStep.tsx
@@ -11,13 +11,9 @@ export const TradeFormStep: TransitionStep<NewPositionCardStep> = ({
 }) => {
   const { t } = useTranslation();
 
-  const {
-    hasEmptyBalance,
-    hasOpenPosition,
-    trade,
-    onSubmit,
-    onChangeTrade,
-  } = useContext(NewPositionCardContext);
+  const { hasEmptyBalance, trade, onSubmit, onChangeTrade } = useContext(
+    NewPositionCardContext,
+  );
 
   const onOpenSlippage = useCallback(
     () => changeTo(NewPositionCardStep.slippage),
@@ -26,37 +22,19 @@ export const TradeFormStep: TransitionStep<NewPositionCardStep> = ({
 
   return (
     <div className="tw-relative tw-p-4">
-      {(hasOpenPosition || hasEmptyBalance) && (
+      {hasEmptyBalance && (
         <div className="tw-absolute tw-left-0 tw-top-0 tw-bg-black tw-h-full tw-w-full tw-px-10 tw-z-10 tw-bg-opacity-90 tw-flex tw-items-center tw-justify-center tw-flex-col tw-text-center tw-text-sm tw-font-semibold">
-          {hasOpenPosition ? (
-            <>
-              <div>
-                {t(
-                  translations.perpetualPage.tradeForm.disabledState
-                    .explanation1,
-                )}
-              </div>
-              <div className="tw-mt-4">
-                {t(
-                  translations.perpetualPage.tradeForm.disabledState
-                    .explanation2,
-                )}
-              </div>
-            </>
-          ) : (
-            <div>
-              {t(
-                translations.perpetualPage.tradeForm.disabledState
-                  .emptyBalanceExplanation,
-              )}
-            </div>
-          )}
+          <div>
+            {t(
+              translations.perpetualPage.tradeForm.disabledState
+                .emptyBalanceExplanation,
+            )}
+          </div>
         </div>
       )}
       <TradeForm
         trade={trade}
-        isNewTrade
-        disabled={hasEmptyBalance || hasOpenPosition}
+        disabled={hasEmptyBalance}
         onOpenSlippage={onOpenSlippage}
         onSubmit={onSubmit}
         onChange={onChangeTrade}
