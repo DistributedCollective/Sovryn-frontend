da32de2b8dcbcc0b65cf00b5b3350c606419e5cf
Victor Creed <victor@sovryn.app>
2022-04-20 13:49:31 +0300

    fix: unnessesary number cast

--- src/app/hooks/staking/useStaking_timestampToLockDate.ts
+++ src/app/hooks/staking/useStaking_timestampToLockDate.ts
@@ -1,9 +1,4 @@
 import { useCacheCallWithValue } from '../useCacheCallWithValue';
 
 export const useStaking_timestampToLockDate = (timestamp: number) =>
-  useCacheCallWithValue(
-    'staking',
-    'timestampToLockDate',
-    '0',
-    Number(timestamp),
-  );
+  useCacheCallWithValue('staking', 'timestampToLockDate', '0', timestamp);
