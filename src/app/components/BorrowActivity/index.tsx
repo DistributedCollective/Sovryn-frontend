8a759dfb9750b93cfd194b159df78df055e4aa13
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2022-05-05 10:03:54 +0300

    feat: enforce tab component (#2150)
    * feat: enforce tab component
    
    * fix: split limit order tables to different components

--- src/app/components/BorrowActivity/index.tsx
+++ src/app/components/BorrowActivity/index.tsx
@@ -1,58 +1,40 @@
-/**
- *
- * BorrowActivity
- *
- */
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { translations } from 'locales/i18n';
 import { useIsConnected } from 'app/hooks/useAccount';
 import { ActiveUserBorrows } from 'app/containers/ActiveUserBorrows';
 import { BorrowHistory } from 'app/containers/BorrowHistory/Loadable';
+import { Tabs } from '../Tabs';
 
-import { Tab } from '../Tab';
-import { SkeletonRow } from '../Skeleton/SkeletonRow';
-
-interface Props {}
-
-export function BorrowActivity(props: Props) {
+export const BorrowActivity: React.FC = () => {
   const { t } = useTranslation();
   const isConnected = useIsConnected();
-  const [activeBorrows, setActiveBorrows] = useState(true);
-  return (
-    <div>
-      <div className="tw-mt-12 tw-mb-6 tw-flex tw-flex-col md:tw-flex-row md:tw-justify-between md:tw-items-center">
-        <h3 className="tw-mt-0 tw-mb-4 md:tw-mb-0 tw-text-sov-white">
-          {t(translations.borrowActivity.title)}
-        </h3>
 
-        <div className="tw-flex tw-flex-row tw-items-center tw-justify-start md:justify-content-end">
-          <Tab
-            text={t(translations.borrowActivity.tabs.active)}
-            active={activeBorrows}
-            onClick={() => setActiveBorrows(true)}
-            className="tw-mr-4"
-          />
-          <Tab
-            text={t(translations.borrowActivity.tabs.history)}
-            active={!activeBorrows}
-            onClick={() => setActiveBorrows(false)}
-          />
-        </div>
-      </div>
-      <div className="tw-grid tw-gap-8 tw-grid-cols-12">
-        <div className="tw-col-span-12">
-          {!isConnected ? (
-            <SkeletonRow
-              loadingText={t(translations.borrowActivity.walletNote)}
-            />
-          ) : activeBorrows ? (
-            <ActiveUserBorrows />
-          ) : (
-            <BorrowHistory />
-          )}
-        </div>
-      </div>
-    </div>
+  const tabs = useMemo(
+    () => [
+      {
+        id: 'active',
+        label: t(translations.borrowActivity.tabs.active),
+        content: <ActiveUserBorrows />,
+      },
+      {
+        id: 'history',
+        label: t(translations.borrowActivity.tabs.history),
+        content: <BorrowHistory />,
+      },
+    ],
+    [t],
   );
-}
+
+  return (
+    <>
+      {isConnected && (
+        <Tabs
+          items={tabs}
+          initial={tabs[0].id}
+          dataActionId="borrow-activity"
+        />
+      )}
+    </>
+  );
+};
