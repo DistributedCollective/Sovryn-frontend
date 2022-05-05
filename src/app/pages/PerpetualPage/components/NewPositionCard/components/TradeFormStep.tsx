2f6ca062ee50465ddeed3e46597474663119ae4a
tiltom <tilnak.tomas@gmail.com>
2022-05-05 16:20:07 +0200

    Disable TradeForm and view account balance if user is not whitelisted

--- src/app/pages/PerpetualPage/components/NewPositionCard/components/TradeFormStep.tsx
+++ src/app/pages/PerpetualPage/components/NewPositionCard/components/TradeFormStep.tsx
@@ -1,15 +1,19 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
 import { NewPositionCardContext } from '..';
 import { TransitionStep } from '../../../../../containers/TransitionSteps';
 import { TradeForm } from '../../TradeForm';
 import { NewPositionCardStep } from '../types';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { translations } from '../../../../../../locales/i18n';
+import { selectPerpetualPage } from 'app/pages/PerpetualPage/selectors';
+import { useSelector } from 'react-redux';
 
 export const TradeFormStep: TransitionStep<NewPositionCardStep> = ({
   changeTo,
 }) => {
   const { t } = useTranslation();
+
+  const { isAddressWhitelisted } = useSelector(selectPerpetualPage);
 
   const { hasEmptyBalance, trade, onSubmit, onChangeTrade } = useContext(
     NewPositionCardContext,
@@ -20,21 +24,53 @@ export const TradeFormStep: TransitionStep<NewPositionCardStep> = ({
     [changeTo],
   );
 
+  const isDisabled = useMemo(() => !isAddressWhitelisted || hasEmptyBalance, [
+    hasEmptyBalance,
+    isAddressWhitelisted,
+  ]);
+
   return (
     <div className="tw-relative tw-p-4">
-      {hasEmptyBalance && (
+      {isDisabled && (
         <div className="tw-absolute tw-left-0 tw-top-0 tw-bg-black tw-h-full tw-w-full tw-px-10 tw-z-10 tw-bg-opacity-90 tw-flex tw-items-center tw-justify-center tw-flex-col tw-text-center tw-text-sm tw-font-semibold">
           <div>
-            {t(
-              translations.perpetualPage.tradeForm.disabledState
-                .emptyBalanceExplanation,
+            {isAddressWhitelisted ? (
+              <Trans
+                i18nKey={
+                  translations.perpetualPage.tradeForm.disabledState
+                    .whitelistExplanation
+                }
+                components={[
+                  <a
+                    className="tw-underline"
+                    href="https://live.sovryn.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    sovryn.app
+                  </a>,
+                  <a
+                    className="tw-underline"
+                    href="https://twitter.com/SovrynBTC"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Twitter
+                  </a>,
+                ]}
+              />
+            ) : (
+              t(
+                translations.perpetualPage.tradeForm.disabledState
+                  .emptyBalanceExplanation,
+              )
             )}
           </div>
         </div>
       )}
       <TradeForm
         trade={trade}
-        disabled={hasEmptyBalance}
+        disabled={isDisabled}
         onOpenSlippage={onOpenSlippage}
         onSubmit={onSubmit}
         onChange={onChangeTrade}
