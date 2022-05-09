8e4ac99c9c98c2bfd59e83a86edc8a1cd3b479a5
Victor Creed <victor@sovryn.app>
2022-05-09 10:46:38 +0300

    fix(dialog): add missing dialog sizes

--- src/app/components/Dialog/types.ts
+++ src/app/components/Dialog/types.ts
@@ -12,6 +12,10 @@ export enum DialogSize {
   xl = 'xl',
   xl2 = 'xl2',
   xl3 = 'xl3',
+  xl4 = 'xl4',
+  xl5 = 'xl5',
+  xl6 = 'xl6',
+  xl7 = 'xl7',
   full = 'full',
 }
 
@@ -23,5 +27,9 @@ export const dialogSizeMap: Record<DialogSize, string> = {
   [DialogSize.xl]: 'tw-max-w-xl',
   [DialogSize.xl2]: 'tw-max-w-2xl',
   [DialogSize.xl3]: 'tw-max-w-3xl',
+  [DialogSize.xl4]: 'tw-max-w-4xl',
+  [DialogSize.xl5]: 'tw-max-w-5xl',
+  [DialogSize.xl6]: 'tw-max-w-6xl',
+  [DialogSize.xl7]: 'tw-max-w-7xl',
   [DialogSize.full]: 'tw-w-full',
 };
