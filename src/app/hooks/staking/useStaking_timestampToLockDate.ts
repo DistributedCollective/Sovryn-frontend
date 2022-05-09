9b5574cfca4c019bef2d2d4f407585483905585a
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2022-05-09 18:28:41 +0300

    fix: use timestampToLockDate value instead of current timestamp to calculate voting power (#2139)
    * fix: use timestampToLockDate value instead of current timestamp to calculate voting power
    
    * fix: unnessesary number cast

---
+++ src/app/hooks/staking/useStaking_timestampToLockDate.ts
@@ -0,0 +1,4 @@
+import { useCacheCallWithValue } from '../useCacheCallWithValue';
+
+export const useStaking_timestampToLockDate = (timestamp: number) =>
+  useCacheCallWithValue('staking', 'timestampToLockDate', '0', timestamp);
