a796fedbe3068021b57c5baf4b23865a545f6e12
Victor Creed <victor@sovryn.app>
2022-05-12 15:55:27 +0300

    fix: prevent repayment modal from reloading each block

--- src/app/containers/RepayPositionHandler/index.tsx
+++ src/app/containers/RepayPositionHandler/index.tsx
@@ -1,9 +1,3 @@
-/**
- *
- * ClosePositionDialog
- *
- */
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dialog } from '../Dialog/Loadable';
@@ -12,14 +6,10 @@ import { actions } from '../../pages/BorrowPage/slice';
 import { useGetLoan } from '../../hooks/trading/useGetLoan';
 import { SkeletonRow } from '../../components/Skeleton/SkeletonRow';
 import { RepayPositionForm } from './RepayPositionForm';
-import { useBlockSync } from 'app/hooks/useAccount';
 
-interface Props {}
-
-export function RepayPositionHandler(props: Props) {
+export const RepayPositionHandler = () => {
   const { repayItem, repayModalOpen } = useSelector(selectLendBorrowSovryn);
   const dispatch = useDispatch();
-  const blockSync = useBlockSync();
 
   const { value: loan, loading: loanLoading, getLoan } = useGetLoan();
 
@@ -27,7 +17,7 @@ export function RepayPositionHandler(props: Props) {
     if (repayItem) {
       getLoan(repayItem);
     }
-  }, [repayItem, getLoan, blockSync]);
+  }, [repayItem, getLoan]);
 
   return (
     <Dialog
@@ -38,4 +28,4 @@ export function RepayPositionHandler(props: Props) {
       {!loanLoading && !!loan && <RepayPositionForm loan={loan} />}
     </Dialog>
   );
-}
+};
