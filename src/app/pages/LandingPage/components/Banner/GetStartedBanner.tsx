24ff263da7a5682e1f468c35110b1c89bc7c125b
pietro-maximoff <pietro.maxim.official@protonmail.com>
2022-06-02 11:12:30 +0300

    [Banner] - fix mob view on 567px and add url to zero

--- src/app/pages/LandingPage/components/Banner/GetStartedBanner.tsx
+++ src/app/pages/LandingPage/components/Banner/GetStartedBanner.tsx
@@ -6,6 +6,7 @@ import getStartedBannerMobile from 'assets/images/banner/banner_zero_mobile.svg'
 import { Button, ButtonSize, ButtonStyle } from 'app/components/Button';
 import { Picture } from 'app/components/Picture';
 import { Breakpoint } from 'types';
+import { zeroUrl } from 'utils/classifiers';
 
 export const GetStartedBanner: React.FC = () => {
   const { t } = useTranslation();
@@ -19,9 +20,9 @@ export const GetStartedBanner: React.FC = () => {
     <div className="tw-flex tw-flex-col tw-justify-center sm:tw-flex-row tw-items-end tw-relative tw-w-full tw-h-full">
       <Picture src={getStartedBanner} srcSet={pictureSrcSet} />
       <Button
-        className="tw-absolute tw-self-center tw-min-h-8 sm:tw-min-h-12 sm:tw-self-auto tw-bottom-3 sm:tw-bottom-6 tw-text-sm sm:tw-text-xl tw-border-2 tw-py-1 sm:tw-py-3"
+        className="tw-absolute tw-self-center tw-min-h-8 md:tw-min-h-12 sm:tw-self-auto tw-bottom-3 md:tw-bottom-6 tw-text-sm md:tw-text-xl tw-border-2 tw-py-1 md:tw-py-3"
         text={t(translations.landingPage.banner.getStarted)}
-        href="/zero"
+        href={zeroUrl}
         size={ButtonSize.lg}
         style={ButtonStyle.transparent}
         dataActionId="landing-getstarted-button"
