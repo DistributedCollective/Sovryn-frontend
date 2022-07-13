bd9b055b7b3800de38b5ff035d2c13d68ac4d6b9
soulBit <its.soulBit@gmail.com>
2022-07-13 11:18:50 +0100

    fix: lend charts not loading when wallet is disconnected

--- src/app/hooks/lending/useGetLendingHistory.ts
+++ src/app/hooks/lending/useGetLendingHistory.ts
@@ -1,27 +1,22 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import axios from 'axios';
-import { selectWalletProvider } from 'app/containers/WalletProvider/selectors';
-import { subgraphWrapperUrl } from 'utils/classifiers';
+import { subgraphWrapperUrl, currentChainId } from 'utils/classifiers';
 import { LendingPool } from 'utils/models/lending-pool';
 import { getLendingContract } from 'utils/blockchain/contract-helpers';
 import { LoanTokenGraphItem } from 'app/components/LoanTokenGraphs/types';
 
 export function useGetLendingHistory(pool: LendingPool) {
   const [data, setData] = useState<LoanTokenGraphItem[]>([]);
-  const { chainId } = useSelector(selectWalletProvider);
   const poolAddress = getLendingContract(pool.getAsset()).address;
 
   useEffect(() => {
-    if (chainId !== undefined) {
+    if (currentChainId !== undefined) {
       axios
-        .get(`${subgraphWrapperUrl[chainId]}/lendingApy/${poolAddress}`)
-        .then(res => {
-          setData(res.data.slice(-36)); //last 6 days of data in 4hr chunks
-        })
+        .get(`${subgraphWrapperUrl[currentChainId]}/lendingApy/${poolAddress}`)
+        .then(res => setData(res.data.slice(-36))) //last 6 days of data in 4hr chunks
         .catch(() => setData([]));
     }
-  }, [chainId, poolAddress]);
+  }, [poolAddress]);
 
   return data;
 }
