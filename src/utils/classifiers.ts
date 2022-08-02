c1552daf74f4492c26411cdbc05c1daa29699b12
tiltom <tilnak.tomas@gmail.com>
2022-08-02 16:01:08 +0200

    RPC and WS configuration

--- src/utils/classifiers.ts
+++ src/utils/classifiers.ts
@@ -33,8 +33,8 @@ export const rpcNodes = {
     'https://public-node.rsk.co/',
   ],
   31: ['https://testnet.sovryn.app/rpc', 'https://public-node.testnet.rsk.co/'],
-  56: 'wss://bsctestnet.sovryn.app/mainnet/websocket',
-  97: 'wss://bsctestnet.sovryn.app/testnet/websocket',
+  56: 'wss://bsc.sovryn.app/mainnet/websocket',
+  97: 'wss://bsc.sovryn.app/testnet/websocket',
 };
 
 export const bitocracyUrl =
