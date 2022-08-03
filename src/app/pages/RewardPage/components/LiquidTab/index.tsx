a4e97739af28eda8abc2150ad70dd128bdd07a9c
soulBit <its.soulBit@gmail.com>
2022-08-03 13:22:46 +0100

    fix: available Liquid rewards amount formatting

--- src/app/pages/RewardPage/components/LiquidTab/index.tsx
+++ src/app/pages/RewardPage/components/LiquidTab/index.tsx
@@ -10,6 +10,7 @@ import { PieChart } from '../../styled';
 import imgNoClaim from 'assets/images/reward/ARMANDO__LENDING.svg';
 import { NoRewardInfo } from '../NoRewardInfo';
 import { sumRewards } from '../../helpers';
+import { weiTo18 } from 'utils/blockchain/math-helpers';
 
 interface ILiquidTabProps {
   amountToClaim: string;
@@ -70,7 +71,7 @@ export const LiquidTab: React.FC<ILiquidTabProps> = ({
         <RewardsDetail
           color={RewardsDetailColor.Yellow}
           title={t(translations.rewardPage.stakingReward)}
-          availableAmount={amountToClaim}
+          availableAmount={weiTo18(amountToClaim)}
           totalEarnedAmount={totalRewardsEarned.toString()}
         />
         <RewardsDetail
