9f2af8d2c26f2a28898e90f3bd2419abdb5f2dee
tiltom <tiltom.defi@protonmail.com>
2022-08-18 12:39:08 +0200

    Perpetuals - Ranking adjustments (#2356)
    * Queries adjustments
    
    * Ranking utils - initial version
    
    * Refactor utils
    
    * Add new tabs with placeholders and prepare the folder structure
    
    * Implement HighestVolumeTable
    
    * Refactor table row
    
    * Separate data fetching and presentation layer
    
    * Create new universal Table component
    
    * Add the rest of the tables
    
    * Small adjustments
    
    * Fix HighestProfit table data fetching
    
    * Fix tables loading
    
    * Add date reset info
    
    * Align reset date with rank
    
    * Add comments regarding potentialPrize
    
    * Adjust ranking reset date + fix a typo
    
    * Fix timestamp to block conversion for mainnet
    
    * Update HighestVolume and MostTrades tables
    
    * Fix review remarks
    
    Co-authored-by: soulBit <its.soulBit@gmail.com>

---
+++ src/app/pages/PerpetualPage/hooks/graphql/useGetRealizedPnlData.ts
@@ -0,0 +1,41 @@
+import { useMemo } from 'react';
+import { useQuery, gql } from '@apollo/client';
+import {
+  PerpetualPairType,
+  PerpetualPairDictionary,
+} from '../../../../../utils/dictionaries/perpetual-pair-dictionary';
+
+/**
+ * Hook to retrieve realized PnL data for a list of trader IDs (wallet addresses)
+ */
+
+export function useGetRealizedPnlData(
+  pairType: PerpetualPairType,
+  traderIDs: string[],
+  tradesTimestamp: string,
+) {
+  const pair = useMemo(() => PerpetualPairDictionary.get(pairType), [pairType]);
+  const QUERY = gql`
+    {
+      realizedPnLs(
+        where: {
+          blockTimestamp_gt: ${tradesTimestamp}
+          perpetual: ${JSON.stringify(pair.id)}
+          trader_in: ${JSON.stringify(traderIDs)}
+        }
+      ) {
+        id
+        perpetual {
+          id
+        }
+        trader {
+          id
+        }
+        pnlCC
+        blockTimestamp
+      }
+    }
+  `;
+  const realizedPnlDataQuery = useQuery(QUERY);
+  return realizedPnlDataQuery;
+}
