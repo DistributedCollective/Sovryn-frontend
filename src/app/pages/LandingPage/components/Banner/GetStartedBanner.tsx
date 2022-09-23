5907b35a4278f0feee339e708ee4b5315de82e3a
tiltom <tiltom.defi@protonmail.com>
2022-09-23 15:12:14 +0200

    Hide perps banner on homepage (#2413)

--- src/app/pages/LandingPage/components/Banner/GetStartedBanner.tsx
+++ src/app/pages/LandingPage/components/Banner/GetStartedBanner.tsx
@@ -4,9 +4,6 @@ import getStartedBannerMobile from 'assets/images/banner/Zero_banner_mobile.svg'
 import { Picture } from 'app/components/Picture';
 import { Breakpoint } from 'types';
 import { zeroUrl } from 'utils/classifiers';
-import perpetualsBanner from 'assets/images/banner/Decentralized_Perpetual.svg';
-import perpetualsBannerMobile from 'assets/images/banner/Decentralized_Perpetual_Mobile.svg';
-import { BannersCarousel } from './components/BannersCarousel';
 
 const zeroSrcSet = [
   {
@@ -15,12 +12,12 @@ const zeroSrcSet = [
   },
 ];
 
-const perpetualsSrcSet = [
-  {
-    src: perpetualsBannerMobile,
-    media: Breakpoint.sm,
-  },
-];
+// const perpetualsSrcSet = [
+//   {
+//     src: perpetualsBannerMobile,
+//     media: Breakpoint.sm,
+//   },
+// ];
 
 export const GetStartedBanner: React.FC = () => {
   const handleZeroBannerClick = useCallback(
@@ -28,29 +25,29 @@ export const GetStartedBanner: React.FC = () => {
     [],
   );
 
-  const handlePerpetualsClick = useCallback(
-    () => window.open('https://www.sovryn.app/perp-futures', '_blank'),
-    [],
-  );
+  // const handlePerpetualsClick = useCallback(
+  //   () => window.open('https://www.sovryn.app/perp-futures', '_blank'),
+  //   [],
+  // );
 
   return (
     <div className="tw-relative tw-w-full">
-      <BannersCarousel>
-        <div
+      {/* <BannersCarousel> */}
+      {/* <div
           onClick={handlePerpetualsClick}
           data-action-id="landing-perpetuals-button"
           className="tw-cursor-pointer"
         >
           <Picture src={perpetualsBanner} srcSet={perpetualsSrcSet} />
-        </div>
-        <div
-          onClick={handleZeroBannerClick}
-          data-action-id="landing-getstarted-button"
-          className="tw-cursor-pointer"
-        >
-          <Picture src={getStartedBanner} srcSet={zeroSrcSet} />
-        </div>
-      </BannersCarousel>
+        </div> */}
+      <div
+        onClick={handleZeroBannerClick}
+        data-action-id="landing-getstarted-button"
+        className="tw-cursor-pointer"
+      >
+        <Picture src={getStartedBanner} srcSet={zeroSrcSet} />
+      </div>
+      {/* </BannersCarousel> */}
     </div>
   );
 };
