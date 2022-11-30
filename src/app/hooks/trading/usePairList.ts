252a15c4a870f09dc846ea64e7b0e6f05a62978a
soulBit <its.soulBit@gmail.com>
2022-11-30 10:24:34 +0000

    Release 2022-11-30 (#2458)
    * Hide XUSD_legacy from margin trade (#2446)
    
    * Fix RBTC missing on Swap page (#2447)
    
    * Remove Perpetuals page from navigation (#2456)
    
    Co-authored-by: tiltom <tiltom.defi@protonmail.com>

--- src/app/hooks/trading/usePairList.ts
+++ src/app/hooks/trading/usePairList.ts
@@ -1,13 +1,25 @@
 import { IPairData } from 'types/trading-pairs';
+import { isMainnet, isStaging } from 'utils/classifiers';
+
+// XUSD_legacy, cannot use getContract because we don't have a mainnet contract for it
+const deprecatedPair = '0x74858FE37d391f81F89472e1D8BC8Ef9CF67B3b1'.toLowerCase();
 
 export const usePairList = (pairs: IPairData[]) => {
   if (!pairs) {
     return [];
   }
 
-  return Object.keys(pairs)
+  const pairList = Object.keys(pairs)
     .map(key => pairs[key])
     .filter((pair: IPairData) => {
       return pair;
     });
+
+  return isMainnet || isStaging
+    ? pairList
+    : pairList.filter(
+        pair =>
+          pair.base_id.toLowerCase() !== deprecatedPair &&
+          pair.quote_id.toLowerCase() !== deprecatedPair,
+      );
 };
