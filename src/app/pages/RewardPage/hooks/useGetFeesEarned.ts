27806e035ffd0d88a93539ab8cb865a3fc643dd2
Victor Creed <victor@sovryn.app>
2023-09-20 11:22:02 +0300

    feat: optimize calculations

--- src/app/pages/RewardPage/hooks/useGetFeesEarned.ts
+++ src/app/pages/RewardPage/hooks/useGetFeesEarned.ts
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
 import { useAccount, useBlockSync } from 'app/hooks/useAccount';
-import { Asset } from 'types';
-import { getTokenContract } from 'utils/blockchain/contract-helpers';
+import { Asset, Chain } from 'types';
+import {
+  getContract,
+  getTokenContract,
+} from 'utils/blockchain/contract-helpers';
 import { contractReader } from 'utils/sovryn/contract-reader';
 import { IEarnedFee } from './useGetFeesEarnedClaimAmount';
 import { BigNumber } from 'ethers';
+import {
+  MultiCallData,
+  bridgeNetwork,
+} from 'app/pages/BridgeDepositPage/utils/bridge-network';
+import { useIsMounted } from 'app/hooks/useIsMounted';
+import { AssetsDictionary } from 'utils/dictionaries/assets-dictionary';
 
 const MAX_CHECKPONTS = 50;
 
-const ASSETS = [Asset.RBTC, Asset.SOV, Asset.MYNT, Asset.ZUSD];
+const ASSETS = AssetsDictionary.assetList();
+// const ASSETS = [Asset.RBTC, Asset.SOV, Asset.MYNT, Asset.ZUSD];
 
 let btcDummyAddress: string;
 const getRbtcDummyAddress = async () => {
@@ -33,33 +43,11 @@ const getTokenAddresses = async () => {
   return result;
 };
 
-const getProcessedCheckpoints = (token: string, owner: string) =>
-  contractReader
-    .call<string>('feeSharingProxy', 'processedCheckpoints', [owner, token])
-    .then(Number);
-
-const getAllUserFeesPerMaxCheckpoints = (
-  token: string,
-  owner: string,
-  from: number,
-  max: number,
-) =>
-  contractReader
-    .call<BigNumber[]>('feeSharingProxy', 'getAllUserFeesPerMaxCheckpoints', [
-      owner,
-      token,
-      from,
-      max,
-    ])
-    .then(result =>
-      result.reduce((prev, cur) => prev.add(cur), BigNumber.from(0)),
-    )
-    .then(result => result.toString());
-
 export const useGetFeesEarned = (): {
   loading: boolean;
   earnedFees: IEarnedFee[];
 } => {
+  const isMounted = useIsMounted();
   const address = useAccount();
   const blockSync = useBlockSync();
   const [loading, setLoading] = useState(false);
@@ -67,6 +55,10 @@ export const useGetFeesEarned = (): {
 
   useEffect(() => {
     const run = async () => {
+      if (!isMounted()) {
+        return;
+      }
+
       if (!address) {
         setLoading(false);
         setEarnedFees([]);
@@ -74,39 +66,80 @@ export const useGetFeesEarned = (): {
       }
 
       setLoading(true);
+
+      const feeSharingProxy = getContract('feeSharingProxy');
       const tokens = await getTokenAddresses();
 
-      const checkpoints = await Promise.all(
-        tokens.map(contractAddress =>
-          getProcessedCheckpoints(contractAddress, address),
+      const checkpoints = await bridgeNetwork.multiCall(
+        Chain.RSK,
+        tokens.flatMap(
+          token =>
+            [
+              {
+                address: feeSharingProxy.address,
+                abi: feeSharingProxy.abi,
+                fnName: 'totalTokenCheckpoints',
+                args: [token],
+                key: `${token}/totalTokenCheckpoints`,
+                parser: (value: string) => Number(value),
+              },
+              {
+                address: feeSharingProxy.address,
+                abi: feeSharingProxy.abi,
+                fnName: 'processedCheckpoints',
+                args: [address, token],
+                key: `${token}/processedCheckpoints`,
+                parser: (value: string) => Number(value),
+              },
+            ] as MultiCallData[],
         ),
       );
 
-      const results = await Promise.all(
-        tokens.map((token, index) =>
-          getAllUserFeesPerMaxCheckpoints(
-            token,
-            address,
-            Math.max(checkpoints[index], 0),
-            MAX_CHECKPONTS,
-          ).then(
-            amount =>
-              ({
-                asset: ASSETS[index],
-                contractAddress: token,
-                value: amount,
-                rbtcValue: 0,
-              } as IEarnedFee),
-          ),
+      const amounts = await bridgeNetwork.multiCall(
+        Chain.RSK,
+        tokens.map(
+          token =>
+            ({
+              address: feeSharingProxy.address,
+              abi: feeSharingProxy.abi,
+              fnName: 'getAllUserFeesPerMaxCheckpoints',
+              args: [
+                address,
+                token,
+                Math.max(
+                  checkpoints.returnData[`${token}/processedCheckpoints`],
+                  0,
+                ),
+                MAX_CHECKPONTS,
+              ],
+              key: token,
+              parser: ({ fees }: { fees: string[] }) =>
+                fees.reduce((prev, cur) => prev.add(cur), BigNumber.from(0)),
+            } as MultiCallData),
         ),
       );
 
-      setEarnedFees(results);
-      setLoading(false);
+      const results = tokens.map(
+        (token, index) =>
+          ({
+            asset: ASSETS[index],
+            contractAddress: token,
+            value: amounts.returnData[token].toString(),
+            rbtcValue: 0,
+            startFrom: checkpoints.returnData[`${token}/processedCheckpoints`],
+            maxCheckpoints:
+              checkpoints.returnData[`${token}/totalTokenCheckpoints`],
+          } as IEarnedFee),
+      );
+
+      if (isMounted()) {
+        setEarnedFees(results);
+        setLoading(false);
+      }
     };
 
     run().catch(console.error);
-  }, [address, blockSync]);
+  }, [address, blockSync, isMounted]);
 
   return {
     loading,
