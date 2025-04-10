cebf1a4a77ab6e2110df96cebac011a716d40dc9
tiltom <tiltom.defi@protonmail.com>
2025-04-10 16:49:17 +0200

    SOV-4870: Hide fee estimations (#2621)
    Hide fee estimations

--- src/app/pages/MarginTradePage/components/TxFeeCalculator/index.tsx
+++ src/app/pages/MarginTradePage/components/TxFeeCalculator/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import type { TransactionConfig } from 'web3-core';
-import { translations } from 'locales/i18n';
+// import { translations } from 'locales/i18n';
 import { ContractName } from '../../../../../utils/types/contracts';
-import classNames from 'classnames';
-import { TransactionFee } from './TransactionFee';
+// import classNames from 'classnames';
+// import { TransactionFee } from './TransactionFee';
 import { Asset } from 'types';
 
 interface ITxFeeCalculatorProps {
@@ -21,49 +21,53 @@ interface ITxFeeCalculatorProps {
   customLoading?: boolean;
 }
 
-export const TxFeeCalculator: React.FC<ITxFeeCalculatorProps> = ({
-  asset = Asset.RBTC,
-  contractName,
-  methodName,
-  args,
-  txConfig = {},
-  condition = true,
-  className = 'tw-mb-1',
-  textClassName,
-  onFeeUpdated,
-  customLimit,
-  customLoading,
-}) => {
-  const { t } = useTranslation();
-  return (
-    <div
-      className={classNames(
-        'tw-flex tw-flex-row tw-justify-between tw-text-sov-white tw-items-center',
-        className,
-      )}
-    >
-      <div className="tw-w-1/2 tw-text-gray-10">
-        {t(translations.marginTradePage.tradeForm.labels.estimatedNetworkFee)}
-      </div>
-      <div
-        className={classNames(
-          'tw-w-1/2 tw-font-medium tw-pl-2 tw-text-right',
-          textClassName,
-        )}
-        data-action-id="margin-reviewTransaction-txFeeCalculator"
-      >
-        <TransactionFee
-          asset={asset}
-          contractName={contractName}
-          methodName={methodName}
-          args={args}
-          txConfig={txConfig}
-          condition={condition}
-          onFeeUpdated={onFeeUpdated}
-          customLimit={customLimit}
-          customLoading={customLoading}
-        />
-      </div>
-    </div>
-  );
-};
+export const TxFeeCalculator: React.FC<ITxFeeCalculatorProps> = () => null;
+
+// Temporary fix based on this bug report: https://sovryn.atlassian.net/browse/SOV-4870
+
+// export const TxFeeCalculator: React.FC<ITxFeeCalculatorProps> = ({
+//   asset = Asset.RBTC,
+//   contractName,
+//   methodName,
+//   args,
+//   txConfig = {},
+//   condition = true,
+//   className = 'tw-mb-1',
+//   textClassName,
+//   onFeeUpdated,
+//   customLimit,
+//   customLoading,
+// }) => {
+//   const { t } = useTranslation();
+//   return (
+//     <div
+//       className={classNames(
+//         'tw-flex tw-flex-row tw-justify-between tw-text-sov-white tw-items-center',
+//         className,
+//       )}
+//     >
+//       <div className="tw-w-1/2 tw-text-gray-10">
+//         {t(translations.marginTradePage.tradeForm.labels.estimatedNetworkFee)}
+//       </div>
+//       <div
+//         className={classNames(
+//           'tw-w-1/2 tw-font-medium tw-pl-2 tw-text-right',
+//           textClassName,
+//         )}
+//         data-action-id="margin-reviewTransaction-txFeeCalculator"
+//       >
+//         <TransactionFee
+//           asset={asset}
+//           contractName={contractName}
+//           methodName={methodName}
+//           args={args}
+//           txConfig={txConfig}
+//           condition={condition}
+//           onFeeUpdated={onFeeUpdated}
+//           customLimit={customLimit}
+//           customLoading={customLoading}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
